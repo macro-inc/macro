81c85ba017aec37f395aff767eb2d26645b418ad
ehayes <eqhayes@protonmail.com>
2025-11-04 11:42:50 -0500

    rename, types, example

---
+++ rust/cloud-storage/anthropic/src/client/chat.rs
@@ -0,0 +1,39 @@
+use futures::Stream;
+use serde::{Serialize, de::DeserializeOwned};
+use std::pin::Pin;
+
+use super::Client;
+use crate::error::AnthropicError;
+use crate::types::request::CreateMessageRequestBody;
+use crate::types::stream_response::StreamEvent;
+
+pub struct Chat<'c> {
+    inner: &'c Client,
+}
+
+impl Client {
+    pub fn chat(&'_ self) -> Chat<'_> {
+        Chat { inner: self }
+    }
+}
+
+impl<'c> Chat<'c> {
+    pub async fn create_stream(
+        &self,
+        mut request: CreateMessageRequestBody,
+    ) -> Pin<Box<dyn Stream<Item = Result<StreamEvent, AnthropicError>> + Send>> {
+        request.stream = Some(true);
+        self.create_stream_unchecked(request).await
+    }
+
+    pub async fn create_stream_unchecked<I, O>(
+        &self,
+        request: I,
+    ) -> Pin<Box<dyn Stream<Item = Result<O, AnthropicError>> + Send>>
+    where
+        I: Serialize,
+        O: DeserializeOwned + Send + Sync + 'static,
+    {
+        self.inner.post_stream("/v1/messages", request).await
+    }
+}
--- rust/cloud-storage/anthropic/src/types/stream_response.rs
+++ rust/cloud-storage/anthropic/src/types/stream_response.rs
@@ -1,13 +1,4 @@
 use super::response::MessageResponse;
-/// https://docs.claude.com/en/docs/build-with-claude/streaming#content-block-delta-types
-/// stream response is comprised of:
-/// A message_start event
-/// Potentially multiple content blocks, each of which contains:
-/// A content_block_start event
-/// Potentially multiple content_block_delta events
-/// A content_block_stop event
-/// A message_delta event
-/// A message_stop event
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
@@ -15,7 +6,7 @@ use serde::{Deserialize, Serialize};
 pub enum StreamEvent {
     MessageStart {
         message: MessageResponse,
-    }, // contains message type with empty content
+    },
     ContentBlockStart {
         index: u32,
         content_block: ContentDeltaEvent,
@@ -30,6 +21,7 @@ pub enum StreamEvent {
     MessageDelta {
         delta: MessageResponse,
     }, // changes to top level message
+
     MessageStop,
     Ping,
 }
@@ -61,10 +53,22 @@ impl Into<Result<StreamEvent, StreamError>> for StreamResult {
 #[derive(Clone, Debug, Serialize, Deserialize, PartialEq)]
 #[serde(tag = "type", rename_all = "snake_case")]
 pub enum ContentDeltaEvent {
-    TextDelta { text: String },
-    InputJsonDelta { partial_json: String },
-    ThinkingDelta { thinking: String },
-    SignatureDelta { signature: String },
+    #[serde(rename = "text")]
+    StartTextDelta {
+        text: String,
+    },
+    TextDelta {
+        text: String,
+    },
+    InputJsonDelta {
+        partial_json: String,
+    },
+    ThinkingDelta {
+        thinking: String,
+    },
+    SignatureDelta {
+        signature: String,
+    },
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize, PartialEq)]
@@ -98,8 +102,14 @@ mod test {
         json!({
             "type":"message_start",
             "message":{
-                "id":"abc","type":"message","role":"assistant","model":"claude-sonnet-4-5-20250929","stop_sequence":null,
-                "usage":{"input_tokens":472,"output_tokens":2},"content":[],"stop_reason":null}})
+                "id":"abc",
+                "type":"message",
+                "role":"assistant",
+                "model":"claude-sonnet-4-5-20250929",
+                "stop_sequence":null,
+                "usage": {"input_tokens":472,"output_tokens":2},
+                "content":[],
+                "stop_reason":null}})
     }
 
     fn content_block_start_text() -> Value {
@@ -123,7 +133,21 @@ mod test {
     }
 
     fn message_delta() -> Value {
-        json!({"type":"message_delta","delta":{"id":"abc","type":"message","role":"assistant","model":"claude-sonnet-4-5-20250929","stop_sequence":null,"usage":{"input_tokens":472,"output_tokens":2},"content":[],"stop_reason":null}})
+        json!({
+            "type":"message_delta",
+            "delta": {
+                "id":"abc",
+                "type":"message",
+                "role":"assistant",
+                "model":"claude-sonnet-4-5-20250929",
+                "stop_sequence":null,
+                "usage": {
+                    "input_tokens":472,
+                    "output_tokens":2},
+                "content":[],
+                "stop_reason":null
+            }
+        })
     }
 
     fn message_stop() -> Value {
@@ -138,16 +162,16 @@ mod test {
     fn test_se_start() {
         let se = serde_json::to_value(&StreamEvent::MessageStart {
             message: MessageResponse {
-                id: "abc".into(),
-                r#type: "message".into(),
+                r#type: Some("message".into()),
+                id: Some("abc".into()),
                 container: None,
-                content: Content::Array(vec![]),
-                role: Role::Assistant,
+                content: Some(Content::Array(vec![])),
+                role: Some(Role::Assistant),
                 context_management: None,
-                model: "claude-sonnet-4-5-20250929".into(),
+                model: Some("claude-sonnet-4-5-20250929".into()),
                 stop_sequence: None,
                 stop_reason: None,
-                usage: Usage {
+                usage: Some(Usage {
                     cache_creation: None,
                     cache_creation_input_tokens: None,
                     cache_read_input_tokens: None,
@@ -155,7 +179,7 @@ mod test {
                     output_tokens: 2,
                     server_tool_use: None,
                     service_tier: None,
-                },
+                }),
             },
         })
         .expect("se start");
@@ -338,16 +362,16 @@ mod test {
     fn test_se_message_delta() {
         let se = serde_json::to_value(&StreamEvent::MessageDelta {
             delta: MessageResponse {
-                id: "abc".into(),
-                r#type: "message".into(),
+                id: Some("abc".into()),
+                r#type: Some("message".into()),
                 container: None,
-                content: Content::Array(vec![]),
-                role: Role::Assistant,
+                content: Some(Content::Array(vec![])),
+                role: Some(Role::Assistant),
                 context_management: None,
-                model: "claude-sonnet-4-5-20250929".into(),
+                model: Some("claude-sonnet-4-5-20250929".into()),
                 stop_sequence: None,
                 stop_reason: None,
-                usage: Usage {
+                usage: Some(Usage {
                     cache_creation: None,
                     cache_creation_input_tokens: None,
                     cache_read_input_tokens: None,
@@ -355,7 +379,7 @@ mod test {
                     output_tokens: 2,
                     server_tool_use: None,
                     service_tier: None,
-                },
+                }),
             },
         })
         .expect("se message_delta");
