e0212de8d0d539ed4c2427a1a9dc142e7b22449a
seamus <seamus@macro.com>
2025-11-09 15:34:00 -0500

    make the active element setter more flexible

--- js/app/packages/app/signal/focus.ts
+++ js/app/packages/app/signal/focus.ts
@@ -1,5 +1,46 @@
-import { createSignal } from 'solid-js';
+import {
+  createEffect,
+  createSignal,
+  getOwner,
+  on,
+  onCleanup,
+  onMount,
+} from 'solid-js';
 
-export const [activeElement, setActiveElement] = createSignal<Element | null>(
+const [rawActiveElement, setRawActiveElement] = createSignal<Element | null>(
   document.activeElement
 );
+
+const [dirty, setDirty] = createSignal(undefined, { equals: () => false });
+
+export function mountGlobalFocusListener(withDebug?: boolean) {
+  const owner = getOwner();
+  if (!owner) {
+    console.error(
+      'No owner. mountGlobalFocusListener should be called once - from within the component tree.'
+    );
+    return;
+  }
+  onMount(() => {
+    document.addEventListener('focusin', setDirty);
+    document.addEventListener('focusout', setDirty);
+    onCleanup(() => {
+      document.removeEventListener('focusin', setDirty);
+      document.removeEventListener('focusout', setDirty);
+    });
+  });
+
+  createEffect(
+    on(dirty, () => {
+      setRawActiveElement(document.activeElement);
+    })
+  );
+  if (withDebug) {
+    createEffect(() => {
+      console.log('### ACTIVE ELEMENT', rawActiveElement());
+    });
+  }
+}
+
+export const activeElement = rawActiveElement;
+export const recheckFocus = () => setDirty();
