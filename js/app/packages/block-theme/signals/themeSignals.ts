c5f2b9c2cfb254bd583427de15b5d251de4aa68c
Peter Chinman <56948273+peterchinman@users.noreply.github.com>
2025-11-11 15:10:59 -0500

    fix: make themes responsive to system theme (#55)

--- js/app/packages/block-theme/signals/themeSignals.ts
+++ js/app/packages/block-theme/signals/themeSignals.ts
@@ -2,7 +2,7 @@ import { createEffect, createMemo, createSignal } from 'solid-js';
 import { convertThemev0v1 } from '../utils/themeMigrations';
 import type { ThemeV0, ThemeV1 } from '../types/themeTypes';
 import { makePersisted } from '@solid-primitives/storage';
-import { DEFAULT_THEMES } from '../constants';
+import { DEFAULT_DARK_THEME, DEFAULT_LIGHT_THEME, DEFAULT_THEMES } from '../constants';
 
 export const [isThemeSaved, setIsThemeSaved] = createSignal<boolean>(true);
 
@@ -26,10 +26,19 @@ let convertedDefaultThemes = DEFAULT_THEMES.map((theme) => {
   else { return theme }
 });
 
-export const [currentThemeId, setCurrentThemeId] = makePersisted(
-  createSignal<string>('Macro'),
+export const [currentThemeId, setCurrentThemeId_] = makePersisted(
+  createSignal<string>(DEFAULT_DARK_THEME),
   { name: 'macro-selected-theme' }
 );
+
+// If theme should match system, when we set current theme, we also set the corresponding mode's theme
+// This avoids the issue where a user sets a theme, and then refreshes, and gets reverted to their preferred mode's theme.
+export const setCurrentThemeId = (...args: Parameters<typeof setCurrentThemeId_>) => {
+  setCurrentThemeId_(...args);
+  if (themeShouldMatchSystem()) {
+    systemMode() === 'dark' ? setDarkModeTheme(...args) : setLightModeTheme(...args);
+  }
+}
 
 export const themes = createMemo(() => [
   ...convertedDefaultThemes,
@@ -37,19 +46,26 @@ export const themes = createMemo(() => [
 ]);
 
 export const [lightModeTheme, setLightModeTheme] = makePersisted(
-  createSignal<string>('micro'),
+  createSignal<string>(DEFAULT_LIGHT_THEME),
   { name: 'macro-light-mode-theme' }
 );
 
 export const [darkModeTheme, setDarkModeTheme] = makePersisted(
-  createSignal<string>('macro'),
+  createSignal<string>(DEFAULT_DARK_THEME),
   { name: 'macro-dark-mode-theme' }
 );
 
 export const [themeShouldMatchSystem, setThemeShouldMatchSystem] = makePersisted(
-  createSignal<boolean>(false),
+  createSignal<boolean>(true),
   {name: 'macro-theme-should-match-system'}
 );
+
+export const [systemMode, setSystemMode] = createSignal<'dark' | 'light'>(window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+
+const darkModeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+darkModeQuery.addEventListener('change', (e: MediaQueryListEvent) => {
+  setSystemMode(e.matches ? 'dark' : 'light');
+});
 
 export const [monochromeIcons, setMonochromeIcons] = makePersisted(
   createSignal<boolean>(false),
