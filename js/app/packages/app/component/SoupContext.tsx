1424b816d4b2daeb9d8dfb755811837124f5ca02
gbirman <25272206+gbirman@users.noreply.github.com>
2025-11-12 16:32:58 -0500

    feat: support project block as a unified list (#89)
    * remove todo
    
    * rename ultimate view to view with state
    
    * more rename
    
    * no export
    
    * nit cleanup
    
    * support project as view
    
    * search toolbar styling
    
    * remove getSelectedViewStore
    
    * fix search since its not using selected view data store
    
    * remove set selected view store in unified list view
    
    * remove set selected view data store in email
    
    * remove setselectedviewstore completely
    
    * use selectedview for email view in soup
    
    * save previuos view
    
    * remove viewid prop from unifiedlistview
    
    * specify project type filters correctly
    
    * add project id filters
    
    * remove incorrect comment
    
    * remove unused
    
    * fix project display for nested

--- js/app/packages/app/component/SoupContext.tsx
+++ js/app/packages/app/component/SoupContext.tsx
@@ -46,8 +46,6 @@ import {
 export type UnifiedListContext = {
   viewsDataStore: Store<ViewDataMap>;
   setViewDataStore: SetStoreFunction<ViewDataMap>;
-  getSelectedViewStore: Accessor<Store<ViewData>>;
-  setSelectedViewStore: SetStoreFunction<ViewData>;
   selectedView: Accessor<ViewId>;
   setSelectedView: Setter<ViewId>;
   virtualizerHandleSignal: Signal<VirtualizerHandle | undefined>;
@@ -65,9 +63,6 @@ export function createSoupContext(): UnifiedListContext {
   const [viewsDataStore, setViewDataStore_] = useAllViews({
     selectedViewSignal: [selectedView, setSelectedView],
   });
-  const getSelectedViewStore = createLazyMemo(
-    () => viewsDataStore[selectedView()]
-  );
   const virtualizerHandleSignal = createSignal<VirtualizerHandle>();
   const entityListRefSignal = createSignal<HTMLDivElement>();
   const entitiesSignal = createSignal<EntityData[]>();
@@ -92,24 +87,10 @@ export function createSoupContext(): UnifiedListContext {
     // @ts-ignore narrowing set store function is annoying due to function overloading
     setViewDataStore_(...args);
   };
-  const setSelectedViewStore: SetStoreFunction<ViewData> = (...args: any[]) => {
-    // need to create new reference, causes bug where first entity persits highlighting
-    if (
-      args.length === 2 &&
-      args[0] === 'selectedEntity' &&
-      typeof args[1] !== 'function'
-    ) {
-      args[1] = { ...args[1] };
-    }
-    // @ts-ignore narrowing set store function is annoying due to function overloading
-    setViewDataStore(selectedView(), ...args);
-  };
 
   return {
     viewsDataStore,
     setViewDataStore,
-    getSelectedViewStore,
-    setSelectedViewStore,
     selectedView,
     setSelectedView,
     virtualizerHandleSignal,
@@ -189,8 +170,7 @@ export function createNavigationEntityListShortcut({
 }) {
   const {
     viewsDataStore: viewsData,
-    getSelectedViewStore: viewData,
-    setSelectedViewStore,
+    setViewDataStore,
     entityListRefSignal: [entityListRef],
     virtualizerHandleSignal: [virtualizerHandle],
     selectedView,
@@ -198,6 +178,7 @@ export function createNavigationEntityListShortcut({
     // selectedEntitySignal: [selectedEntity, setSelectedEntity],
     entitiesSignal: [entities],
   } = unifiedListContext;
+  const viewData = createMemo(() => viewsData[selectedView()]);
   const viewIds = createMemo<ViewId[]>(() => Object.keys(viewsData));
 
   const selectedEntity = () => viewData().selectedEntity;
@@ -275,7 +256,7 @@ export function createNavigationEntityListShortcut({
     let index = getHighlightedEntity()?.index ?? -1;
     setJumpedToEnd(false);
 
-    setSelectedViewStore('hasUserInteractedEntity', true);
+    setViewDataStore(selectedView(), 'hasUserInteractedEntity', true);
 
     const entityEl = entityListRef()?.querySelector('[data-entity]');
     const scrollParent = getScrollParent(entityEl);
@@ -328,8 +309,8 @@ export function createNavigationEntityListShortcut({
       const selectedEntity = entities()?.at(index);
       if (selectedEntity) {
         batch(() => {
-          setSelectedViewStore('highlightedId', selectedEntity.id);
-          setSelectedViewStore('selectedEntity', selectedEntity);
+          setViewDataStore(selectedView(), 'highlightedId', selectedEntity.id);
+          setViewDataStore(selectedView(), 'selectedEntity', selectedEntity);
         });
       }
 
@@ -383,7 +364,11 @@ export function createNavigationEntityListShortcut({
 
     const onListScroll = () => {
       if (listScrollEl) {
-        setSelectedViewStore('scrollOffset', listScrollEl.scrollTop);
+        setViewDataStore(
+          selectedView(),
+          'scrollOffset',
+          listScrollEl.scrollTop
+        );
       }
     };
 
@@ -518,7 +503,7 @@ export function createNavigationEntityListShortcut({
     keyDownHandler: () => {
       if (!entityListRef()) return false;
 
-      setSelectedViewStore('display', 'preview', (prev) => !prev);
+      setViewDataStore(selectedView(), 'display', 'preview', (prev) => !prev);
       return true;
     },
     hide: true,
@@ -682,7 +667,10 @@ const useAllViews = ({
         const defaultViewIds = new Set(Object.keys(VIEWCONFIG_DEFAULTS));
         const filteredViewsData = Object.fromEntries(
           Object.entries(viewsData).filter(
-            ([viewId]) => savedViewIds.has(viewId) || defaultViewIds.has(viewId)
+            ([viewId, viewData]) =>
+              savedViewIds.has(viewId) ||
+              defaultViewIds.has(viewId) ||
+              viewData.viewType !== undefined
           )
         );
 
