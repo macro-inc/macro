390bd67cc0a9986b34fe555ff9b5e018c30dacfb
synoet <teonys@nyu.edu>
2025-11-13 15:01:46 -0500

    chg(app/onboarding): make onboarding and email connect more deteministic

--- js/app/packages/app/component/Onboarding.tsx
+++ js/app/packages/app/component/Onboarding.tsx
@@ -1,32 +1,35 @@
 import { DEFAULT_ROUTE } from '@app/constants/defaultRoute';
-import { connectEmail, useEmailLinksStatus } from '@app/signal/emailAuth';
+import {
+  useCheckout,
+  useSignUpAndConnectEmail,
+} from '@app/signal/email-connect';
 import { withAnalytics } from '@coparse/analytics';
-import { updateUserAuth, useIsAuthenticated } from '@core/auth';
 import { useHasPaidAccess } from '@core/auth/license';
 import { BrightJoins } from '@core/component/BrightJoins';
 import BrightJoinsProgressMeter from '@core/component/BrightJoinsProgressMeter';
 import { ActionSequence } from '@core/component/FormControls/ActionSequence';
 import MacroLogo from '@core/component/MacroLogo';
-import { licenseChannel } from '@core/util/licenseUpdateBroadcastChannel';
-import { updateUserInfo } from '@service-gql/client';
-import { stripeServiceClient } from '@service-stripe/client';
-import { useNavigate, useSearchParams } from '@solidjs/router';
-import { createEffect, createSignal, onCleanup, Show } from 'solid-js';
+import { useNavigate } from '@solidjs/router';
+import { createEffect, createSignal, Show } from 'solid-js';
 
 export default function Onboarding() {
   const { track, TrackingEvents } = withAnalytics();
 
   const [progress, setProgress] = createSignal(0);
-  const authenticated = useIsAuthenticated();
-  const connected = useEmailLinksStatus();
   const subscribed = useHasPaidAccess();
 
+  const [authenticatedState, signUpAndConnectEmail] =
+    useSignUpAndConnectEmail();
+
+  const [_, checkout] = useCheckout();
+
+  const authenticated = () => authenticatedState().type === 'authenticated';
+
   const navigate = useNavigate();
-  const [searchParams] = useSearchParams();
 
   // TRACK PROGRESS, REDIRECT ON COMPLETE
   createEffect(() => {
-    const steps = [connected(), subscribed()];
+    const steps = [authenticated(), subscribed()];
 
     const p = steps
       .map((b) => +!!b) // unary + converts bool to int
@@ -47,84 +50,6 @@ export default function Onboarding() {
     }
     return navigate(DEFAULT_ROUTE);
   }
-
-  // WATCH FOR AUTH SUCCESS
-  // createEffect(() => {
-  //   const isAuthed = authenticated();
-  //   const wasAttemptingSSO = localStorage.getItem('new_user_sso_auth_attempt');
-
-  //   if (isAuthed && wasAttemptingSSO) {
-  //     track(TrackingEvents.ONBOARDING.CREATE_ACCOUNT.SSO.GOOGLE.AUTH_SUCCESS);
-  //     localStorage.removeItem('new_user_sso_auth_attempt');
-  //   }
-  // });
-
-  // WATCH FOR SUBSCRIPTION SUCCESS
-  createEffect(() => {
-    const subscriptionSuccess = searchParams.subscriptionSuccess;
-    if (subscriptionSuccess === 'true') {
-      track(TrackingEvents.ONBOARDING.SUBSCRIBE.CHECKOUT_SUCCESS);
-      updateUserInfo();
-    }
-  });
-
-  // Listen for license updates across browser tabs
-  createEffect(() => {
-    const unlisten = licenseChannel.subscribe(() => {
-      updateUserInfo();
-    });
-
-    onCleanup(unlisten);
-  });
-
-  // const location = useLocation<RedirectLocation>();
-  // const startSsoLogin = async (idp_name: string) => {
-  //   localStorage.setItem('new_user_onboarding', 'true');
-  //   localStorage.setItem('new_user_sso_auth_attempt', 'true');
-
-  //   track(TrackingEvents.ONBOARDING.START);
-  //   track(TrackingEvents.ONBOARDING.CREATE_ACCOUNT.SSO.GOOGLE.AUTH_START);
-
-  //   const authUrl = new URL(`${SERVER_HOSTS['auth-service']}/login/sso`);
-  //   authUrl.searchParams.set('idp_name', idp_name);
-  //   if (location.state?.originalLocation) {
-  //     const { pathname, search, hash } = location.state.originalLocation;
-  //     authUrl.searchParams.set(
-  //       'original_url',
-  //       `${window.location.origin}${pathname}${search}${hash}`
-  //     );
-  //   } else {
-  //     authUrl.searchParams.set('original_url', window.location.href);
-  //   }
-  //   window.location.href = authUrl.toString();
-  // };
-
-  // ALSO AUTHS!
-  const connectInbox = async () => {
-    track(TrackingEvents.ONBOARDING.GMAIL.CONNECT_START);
-    try {
-      await connectEmail();
-    } catch (error) {
-      track(TrackingEvents.ONBOARDING.GMAIL.CONNECT_FAILURE, { ...error });
-      console.error(error);
-    }
-
-    if (connected()) track(TrackingEvents.ONBOARDING.GMAIL.CONNECT_SUCCESS);
-    await updateUserAuth();
-    if (subscribed()) complete();
-  };
-
-  const handlePayment = async () => {
-    try {
-      track(TrackingEvents.ONBOARDING.SUBSCRIBE.CHECKOUT_START);
-      const url =
-        await stripeServiceClient.createCheckoutSession('New subscription');
-      window.location.href = url;
-    } catch (error) {
-      track(TrackingEvents.ONBOARDING.SUBSCRIBE.CHECKOUT_FAILURE, { ...error });
-      console.error(error);
-    }
-  };
 
   return (
     <div class="relative flex flex-col gap-4 justify-between items-center p-4 md:p-8 m-1 md:m-8 border border-edge">
@@ -167,22 +92,16 @@ export default function Onboarding() {
 
         <ActionSequence
           steps={[
-            // {
-            //   label: 'Authenticate with Google',
-            //   onClick: () => startSsoLogin('google'),
-            //   disabled: !!authenticated(),
-            //   completed: !!authenticated(),
-            // },
             {
               label: 'Authenticate with Google',
-              onClick: connectInbox,
-              disabled: authenticated() || connected(),
-              completed: connected(),
+              onClick: signUpAndConnectEmail,
+              disabled: authenticatedState().type === 'authenticating',
+              completed: authenticatedState().type === 'authenticated',
             },
             {
               label: 'Start Your Subscription',
-              onClick: handlePayment,
-              disabled: !authenticated() || !connected(),
+              onClick: checkout,
+              disabled: authenticatedState().type !== 'authenticated',
               completed: subscribed(),
             },
           ]}
