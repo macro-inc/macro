918f06ae721f655d591f0cd5b259a1d71ca0c968
gbirman <25272206+gbirman@users.noreply.github.com>
2025-11-14 17:32:25 -0500

    chore: fix project entity preview types and reactivity (#154)
    * fix types and reactivity
    
    * fix export

--- js/app/packages/macro-entity/src/components/EntityWithEverything.tsx
+++ js/app/packages/macro-entity/src/components/EntityWithEverything.tsx
@@ -1,3 +1,4 @@
+import { matches } from '@core/util/match';
 import CheckIcon from '@phosphor-icons/core/assets/regular/check.svg';
 import { useEmail } from '@service-gql/client';
 import { mergeRefs } from '@solid-primitives/refs';
@@ -23,7 +24,11 @@ import {
 import { Dynamic } from 'solid-js/web';
 import { ITEM_WRAPPER } from '../constants/classStrings';
 import { createProfilePictureQuery } from '../queries/auth';
-import { createProjectQuery } from '../queries/project';
+import {
+  createProjectQuery,
+  isProjectContainedEntity,
+  type ProjectContainedEntity,
+} from '../queries/project';
 import type { EntityData, EntityOf } from '../types/entity';
 import type { Notification, WithNotification } from '../types/notification';
 import type { WithSearch } from '../types/search';
@@ -439,7 +444,9 @@ export function EntityWithEverything(
         >
           <div class="flex flex-row items-center justify-end gap-4 min-w-0">
             <Show when={!showActionList()}>
-              <EntityProject entity={props.entity} />
+              <Show when={matches(props.entity, isProjectContainedEntity)}>
+                {(entity) => <EntityProject entity={entity()} />}
+              </Show>
               <Show when={props.timestamp ?? props.entity.updatedAt}>
                 {(date) => {
                   const formattedDate = createFormattedDate(date());
@@ -672,20 +679,8 @@ function NotificationUserIcon(props: { id: string; name?: string }) {
   );
 }
 
-function EntityProject(props: { entity: EntityData }) {
-  if (
-    !(
-      (props.entity.type === 'chat' || props.entity.type === 'document') &&
-      props.entity.projectId
-    )
-  ) {
-    return null;
-  }
-
+function EntityProject(props: { entity: ProjectContainedEntity }) {
   const projectQuery = createProjectQuery(props.entity);
-  const Loading = () => (
-    <div class="h-3 w-10 bg-ink-placeholder animate-pulse" />
-  );
   return (
     <div class="flex gap-1 items-center text-xs text-ink-extra-muted min-w-0">
       <svg
@@ -701,7 +696,9 @@ function EntityProject(props: { entity: EntityData }) {
           fill="currentColor"
         />
       </svg>
-      <Suspense fallback={<Loading />}>
+      <Suspense
+        fallback={<div class="h-3 w-10 bg-ink-placeholder animate-pulse" />}
+      >
         <Show when={projectQuery.data?.name}>
           {(name) => <div class="truncate">{name()}</div>}
         </Show>
