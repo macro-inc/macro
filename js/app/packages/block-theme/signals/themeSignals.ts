fe6604548963faa90cda536ace790212326a6914
Fake-User <45649815+Fake-User@users.noreply.github.com>
2025-11-14 16:12:15 -0500

    Russell/biome is bad sometimes (#149)
    * cook
    
    * 55 formats

--- js/app/packages/block-theme/signals/themeSignals.ts
+++ js/app/packages/block-theme/signals/themeSignals.ts
@@ -10,72 +10,59 @@ import { convertThemev0v1 } from '../utils/themeMigrations';
 
 export const [isThemeSaved, setIsThemeSaved] = createSignal<boolean>(true);
 
-export const [themeUpdate, setThemeUpdate] = createSignal<number>(1);
+export const [themeUpdate, setThemeUpdate] = createSignal<undefined>(undefined, {equals: () => false});
 
 export const [htmlColor, setHtmlColor] = makePersisted(
   createSignal({ color: '' }),
-  { name: 'html-color-theme' }
+  {name: 'html-color-theme'}
 );
 
 export const [userThemes, setUserThemes] = makePersisted(
   createSignal<ThemeV1[]>([]),
-  { name: 'macro-user-themes' }
+  {name: 'macro-user-themes'}
 );
 setUserThemes(
   userThemes().map((theme) => {
-    if (!theme.version) {
-      return convertThemev0v1(theme as unknown as ThemeV0);
-    } else {
-      return theme;
-    }
+    if(!theme.version){return convertThemev0v1(theme as unknown as ThemeV0)}
+    else{return theme}
   })
 );
 
 let convertedDefaultThemes = DEFAULT_THEMES.map((theme) => {
-  if (!theme.version) {
-    return convertThemev0v1(theme as unknown as ThemeV0);
-  } else {
-    return theme;
-  }
+  if(!theme.version){return convertThemev0v1(theme as unknown as ThemeV0)}
+  else{return theme}
 });
 
 export const [currentThemeId, setCurrentThemeId_] = makePersisted(
   createSignal<string>(DEFAULT_DARK_THEME),
-  { name: 'macro-selected-theme' }
+  {name: 'macro-selected-theme'}
 );
 
 // If theme should match system, when we set current theme, we also set the corresponding mode's theme
 // This avoids the issue where a user sets a theme, and then refreshes, and gets reverted to their preferred mode's theme.
-export const setCurrentThemeId = (
-  ...args: Parameters<typeof setCurrentThemeId_>
-) => {
+export const setCurrentThemeId = ( ...args: Parameters<typeof setCurrentThemeId_> ) => {
   setCurrentThemeId_(...args);
-  if (themeShouldMatchSystem()) {
-    systemMode() === 'dark'
-      ? setDarkModeTheme(...args)
-      : setLightModeTheme(...args);
+  if(themeShouldMatchSystem()){
+    systemMode() === 'dark' ? setDarkModeTheme(...args) : setLightModeTheme(...args);
   }
 };
 
-export const themes = createMemo(() => [
-  ...convertedDefaultThemes,
-  ...userThemes(),
-]);
+export const themes = createMemo(() => [...convertedDefaultThemes, ...userThemes()]);
 
 export const [lightModeTheme, setLightModeTheme] = makePersisted(
   createSignal<string>(DEFAULT_LIGHT_THEME),
-  { name: 'macro-light-mode-theme' }
+  {name: 'macro-light-mode-theme'}
 );
 
 export const [darkModeTheme, setDarkModeTheme] = makePersisted(
   createSignal<string>(DEFAULT_DARK_THEME),
-  { name: 'macro-dark-mode-theme' }
+  {name: 'macro-dark-mode-theme'}
 );
 
-export const [themeShouldMatchSystem, setThemeShouldMatchSystem] =
-  makePersisted(createSignal<boolean>(true), {
-    name: 'macro-theme-should-match-system',
-  });
+export const [themeShouldMatchSystem, setThemeShouldMatchSystem] = makePersisted(
+  createSignal<boolean>(true),
+  {name: 'macro-theme-should-match-system'}
+);
 
 export const [systemMode, setSystemMode] = createSignal<'dark' | 'light'>(
   window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
@@ -88,67 +75,35 @@ darkModeQuery.addEventListener('change', (e: MediaQueryListEvent) => {
 
 export const [monochromeIcons, setMonochromeIcons] = makePersisted(
   createSignal<boolean>(false),
-  { name: 'enable-monochrome-icons' }
+  {name: 'enable-monochrome-icons'}
 );
 
 createEffect(() => {
-  if (monochromeIcons()) {
+  if(monochromeIcons()){
     document.documentElement.style.setProperty('--theme-contact', 'var(--c0)');
-    document.documentElement.style.setProperty('--theme-canvas', 'var(--c0)');
-    document.documentElement.style.setProperty('--theme-folder', 'var(--c0)');
-    document.documentElement.style.setProperty('--theme-image', 'var(--c0)');
-    document.documentElement.style.setProperty('--theme-write', 'var(--c0)');
-    document.documentElement.style.setProperty('--theme-video', 'var(--c0)');
-    document.documentElement.style.setProperty('--theme-html', 'var(--c0)');
-    document.documentElement.style.setProperty('--theme-note', 'var(--c0)');
-    document.documentElement.style.setProperty('--theme-code', 'var(--c0)');
-    document.documentElement.style.setProperty('--theme-chat', 'var(--c0)');
-    document.documentElement.style.setProperty('--theme-pdf', 'var(--c0)');
-    document.documentElement.style.setProperty('--theme-rss', 'var(--c0)');
-  } else {
-    document.documentElement.style.setProperty(
-      '--theme-folder',
-      'oklch(var(--a0l) var(--a0c) 240)'
-    );
-    document.documentElement.style.setProperty(
-      '--theme-canvas',
-      'oklch(var(--a0l) var(--a0c)  60)'
-    );
-    document.documentElement.style.setProperty(
-      '--theme-write',
-      'oklch(var(--a0l) var(--a0c) 260)'
-    );
-    document.documentElement.style.setProperty(
-      '--theme-video',
-      'oklch(var(--a0l) var(--a0c) 277)'
-    );
-    document.documentElement.style.setProperty(
-      '--theme-note',
-      'oklch(var(--a0l) var(--a0c) 293)'
-    );
-    document.documentElement.style.setProperty(
-      '--theme-code',
-      'oklch(var(--a0l) var(--a0c) 162)'
-    );
-    document.documentElement.style.setProperty(
-      '--theme-chat',
-      'oklch(var(--a0l) var(--a0c) 220)'
-    );
-    document.documentElement.style.setProperty(
-      '--theme-image',
-      'oklch(var(--a0l) var(--a0c)  95)'
-    );
-    document.documentElement.style.setProperty(
-      '--theme-html',
-      'oklch(var(--a0l) var(--a0c)  47)'
-    );
-    document.documentElement.style.setProperty(
-      '--theme-rss',
-      'oklch(var(--a0l) var(--a0c) 260)'
-    );
-    document.documentElement.style.setProperty(
-      '--theme-pdf',
-      'oklch(var(--a0l) var(--a0c)  25)'
-    );
+    document.documentElement.style.setProperty('--theme-canvas' , 'var(--c0)');
+    document.documentElement.style.setProperty('--theme-folder' , 'var(--c0)');
+    document.documentElement.style.setProperty('--theme-image'  , 'var(--c0)');
+    document.documentElement.style.setProperty('--theme-write'  , 'var(--c0)');
+    document.documentElement.style.setProperty('--theme-video'  , 'var(--c0)');
+    document.documentElement.style.setProperty('--theme-html'   , 'var(--c0)');
+    document.documentElement.style.setProperty('--theme-note'   , 'var(--c0)');
+    document.documentElement.style.setProperty('--theme-code'   , 'var(--c0)');
+    document.documentElement.style.setProperty('--theme-chat'   , 'var(--c0)');
+    document.documentElement.style.setProperty('--theme-pdf'    , 'var(--c0)');
+    document.documentElement.style.setProperty('--theme-rss'    , 'var(--c0)');
+  }
+  else{
+    document.documentElement.style.setProperty( '--theme-folder', 'oklch(var(--a0l) var(--a0c) 240)');
+    document.documentElement.style.setProperty( '--theme-canvas', 'oklch(var(--a0l) var(--a0c)  60)');
+    document.documentElement.style.setProperty( '--theme-write' , 'oklch(var(--a0l) var(--a0c) 260)');
+    document.documentElement.style.setProperty( '--theme-video' , 'oklch(var(--a0l) var(--a0c) 277)');
+    document.documentElement.style.setProperty( '--theme-note'  , 'oklch(var(--a0l) var(--a0c) 293)');
+    document.documentElement.style.setProperty( '--theme-code'  , 'oklch(var(--a0l) var(--a0c) 162)');
+    document.documentElement.style.setProperty( '--theme-chat'  , 'oklch(var(--a0l) var(--a0c) 220)');
+    document.documentElement.style.setProperty( '--theme-image' , 'oklch(var(--a0l) var(--a0c)  95)');
+    document.documentElement.style.setProperty( '--theme-html'  , 'oklch(var(--a0l) var(--a0c)  47)');
+    document.documentElement.style.setProperty( '--theme-rss'   , 'oklch(var(--a0l) var(--a0c) 260)');
+    document.documentElement.style.setProperty( '--theme-pdf'   , 'oklch(var(--a0l) var(--a0c)  25)');
   }
 });
