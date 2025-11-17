1ef38153e769f0998c63227303a68a380d1003e3
Evan Hutnik <evan.hutnik@gmail.com>
2025-11-16 19:30:14 -0500

    Another filter

--- rust/cloud-storage/email_db_client/src/attachments/provider/upload_filters.rs
+++ rust/cloud-storage/email_db_client/src/attachments/provider/upload_filters.rs
@@ -5,6 +5,7 @@ pub const ATTACHMENT_MIME_TYPE_FILTERS: &str = r#"
     AND a.mime_type NOT LIKE 'video/%'
     AND a.mime_type NOT LIKE 'audio/%'
     AND a.mime_type NOT IN ('application/ics', 'application/x-sharing-metadata-xml')
+    AND NOT (a.mime_type = 'application/octet-stream' AND UPPER(SUBSTRING(a.filename FROM '\.([^.]+)$')) NOT IN ('PDF', 'DOC', 'DOCX'))
 "#;
 
 /// always insert attachments sent from these domains.
