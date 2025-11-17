b93bf7873819ae33b59eb7fccbf282ebbb114e92
Evan Hutnik <evan.hutnik@gmail.com>
2025-11-17 16:08:21 -0500

    Email settings table

---
+++ rust/cloud-storage/macro_db_client/migrations/20251117204659_email_settings.sql
@@ -0,0 +1,7 @@
+-- table for storing user settings for email
+CREATE TABLE "email_settings"
+(
+    link_id      UUID NOT NULL PRIMARY KEY REFERENCES email_links (id) ON DELETE CASCADE,
+    signature_on_replies_forwards BOOLEAN NOT NULL DEFAULT FALSE
+);
+
