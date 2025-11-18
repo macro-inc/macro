36480e71af42d296bc19557ebbfcab5edd965311
Evan Hutnik <35308761+evanhutnik@users.noreply.github.com>
2025-11-18 11:53:40 -0500

    feat(email-service): Signature support, Settings support (#203)

--- rust/cloud-storage/email_service/src/pubsub/webhook/operations/upsert_message.rs
+++ rust/cloud-storage/email_service/src/pubsub/webhook/operations/upsert_message.rs
@@ -129,8 +129,8 @@ pub async fn upsert_message(
     // notify downstream services of new messages
     notify_for_new_messages(ctx, link, new_message_provider_ids).await?;
 
-    // internal only, for now
-    if link.macro_id.ends_with("@macro.com") {
+    // temporarily only for macro emails, for testing purposes
+    if link.macro_id.ends_with("@macro.com") && !cfg!(feature = "disable_attachment_upload") {
         // upload attachments to Macro
         let attachments = email_db_client::attachments::provider::upload::fetch_insertable_attachments_for_new_email(
             &ctx.db,
