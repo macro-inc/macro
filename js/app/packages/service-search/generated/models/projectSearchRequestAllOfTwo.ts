7a65171ca3f3ac36a354fc6d7410419991edfe50
gbirman <25272206+gbirman@users.noreply.github.com>
2025-11-19 14:48:44 -0500

    feat: hide deleted search results front end [M-5238] (#232)

--- js/app/packages/service-search/generated/models/projectSearchRequestAllOfTwo.ts
+++ js/app/packages/service-search/generated/models/projectSearchRequestAllOfTwo.ts
@@ -13,6 +13,9 @@ import type { ProjectSearchRequestAllOfTwoTerms } from './projectSearchRequestAl
 export type ProjectSearchRequestAllOfTwo = {
   /** If true, returns only 1 result per entity. False by default. */
   collapse?: ProjectSearchRequestAllOfTwoCollapse;
+  /** If search_on is set to NameContent, you can disable the recency filter
+by setting to true. */
+  disable_recency?: boolean;
   /** The match type to use when searching */
   match_type: MatchType;
   /** The query to search for */
