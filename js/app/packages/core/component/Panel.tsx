df0e19e448da9867edd34bca150fdd768839448a
Fake.User <russellboswell@protonmail.com>
2025-11-19 16:36:23 -0500

    biome

--- js/app/packages/core/component/Panel.tsx
+++ js/app/packages/core/component/Panel.tsx
@@ -1,23 +1,27 @@
-import { cornerClip } from "@core/util/clipPath";
+import { cornerClip } from '@core/util/clipPath';
 
-export function Panel(){
-  return(
-    <div style={{
-      'clip-path': cornerClip(0, 0, 0, 0),
-      'background-color': 'var(--color-edge-muted)',
-      'box-sizing': 'border-box',
-      'padding': '1px',
-      'height': '100%',
-      'width': '100%',
-    }}>
-      <div style={{
+export function Panel() {
+  return (
+    <div
+      style={{
         'clip-path': cornerClip(0, 0, 0, 0),
-        'background-color': 'var(--color-panel)',
-        'height': '100%',
-        'width': '100%',
-      }}>
+        'background-color': 'var(--color-edge-muted)',
+        'box-sizing': 'border-box',
+        padding: '1px',
+        height: '100%',
+        width: '100%',
+      }}
+    >
+      <div
+        style={{
+          'clip-path': cornerClip(0, 0, 0, 0),
+          'background-color': 'var(--color-panel)',
+          height: '100%',
+          width: '100%',
+        }}
+      >
         props.children
       </div>
     </div>
-  )
+  );
 }
