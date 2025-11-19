9ca98301c6502dbd7f3feaad68119f988cf08409
Fake-User <45649815+Fake-User@users.noreply.github.com>
2025-11-19 18:41:25 -0500

    Russell/settings (#249)

---
+++ js/app/packages/core/component/ClippedPanel.tsx
@@ -0,0 +1,47 @@
+import { cornerClip } from '@core/util/clipPath';
+import type { JSXElement } from 'solid-js';
+
+interface PanelProps {
+  children?: JSXElement;
+  active?: boolean;
+  tr?: boolean;
+  tl?: boolean;
+  bl?: boolean;
+  br?: boolean;
+}
+
+export function ClippedPanel(props: PanelProps) {
+  return (
+    <div
+      style={{
+        'background-image': `linear-gradient(${props.active ? 'var(--color-accent)' : 'var(--color-edge-muted)'}, var(--color-edge-muted))`,
+        'box-sizing': 'border-box',
+        'clip-path': cornerClip(
+          props.tr ? '0.5rem' : 0,
+          props.tl ? '0.5rem' : 0,
+          props.bl ? '0.5rem' : 0,
+          props.br ? '0.5rem' : 0
+        ),
+        padding: '1px',
+        height: '100%',
+        width: '100%',
+      }}
+    >
+      <div
+        style={{
+          'background-color': 'var(--color-panel)',
+          'clip-path': cornerClip(
+            props.tr ? 'calc(0.5rem - 0.5px)' : 0,
+            props.tl ? 'calc(0.5rem - 0.5px)' : 0,
+            props.bl ? 'calc(0.5rem - 0.5px)' : 0,
+            props.br ? 'calc(0.5rem - 0.5px)' : 0
+          ),
+          height: '100%',
+          width: '100%',
+        }}
+      >
+        {props.children}
+      </div>
+    </div>
+  );
+}
