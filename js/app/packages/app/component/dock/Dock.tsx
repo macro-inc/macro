9306c8546ae172defdc12134cf119417edafc93e
Fake.User <russellboswell@protonmail.com>
2025-11-19 17:40:04 -0500

    ClippedPanel

--- js/app/packages/app/component/dock/Dock.tsx
+++ js/app/packages/app/component/dock/Dock.tsx
@@ -10,6 +10,7 @@ import { useGlobalNotificationSource } from '../GlobalAppState';
 import IconPower from '@phosphor-icons/core/regular/power.svg';
 import MacroCreateIcon from '@macro-icons/macro-create-b.svg';
 import { globalSplitManager } from '@app/signal/splitLayout';
+import { ClippedPanel } from '@core/component/ClippedPanel';
 import { isMobileWidth } from '@core/mobile/mobileWidth';
 import { PresentModeGlitch } from './PresentModeGlitch';
 import { IconButton } from '@core/component/IconButton';
@@ -24,6 +25,7 @@ import { runCommand } from '@core/hotkey/hotkeys';
 import { cornerClip } from '@core/util/clipPath';
 import { setCreateMenuOpen } from '../Launcher';
 import { useHasPaidAccess } from '@core/auth';
+
 import { TOKENS } from '@core/hotkey/tokens';
 import { playSound } from '@app/util/sound';
 import { QuickAccess } from './QuickAccess';
@@ -152,226 +154,221 @@ export function Dock(){
 
       <div
         style={{
-          'padding': '0 0.5rem 0.5rem 0.5rem'
+          'padding': '0 0.5rem 0.5rem 0.5rem',
+          'height': 'calc(40px + 0.5rem)',
+          'box-sizing': 'border-box',
+          'width': '100vw'
         }}
       >
-        <div style={{
-          'clip-path': cornerClip(0, 0, '0.5rem', '0.5rem'),
-          'background-color': 'var(--color-edge-muted)',
-          'height': '40px',
-        }}>
-        <div style={{
-          'clip-path': cornerClip(0, 0, 'calc(0.5rem + 1.5px)', 'calc(0.5rem + 1.5px)'),
-          'grid-template-columns':'min-content 1fr min-content',
-          'border': '1px solid var(--color-edge-muted)',
-          'background-color': 'var(--color-panel)',
-          'box-sizing': 'border-box',
-          'scrollbar-width': 'none',
-          'align-content': 'center',
-          'overflow-y': 'hidden',
-          'padding': '0 7px',
-          'display': 'grid',
-          'height': '40px',
-          'z-index': '1',
-          'gap': '7px',
-        }}>
+        <ClippedPanel br bl active>
+          <div style={{
+            'grid-template-columns':'min-content 1fr min-content',
+            'box-sizing': 'border-box',
+            'scrollbar-width': 'none',
+            'align-content': 'center',
+            'overflow-y': 'hidden',
+            'padding': '0 7px',
+            'display': 'grid',
+            'height': '40px',
+            'z-index': '1',
+            'gap': '7px',
+          }}>
 
-          <div
-            style={{
-              'border-right': '1px solid var(--color-edge-muted)',
+            <div
+              style={{
+                'border-right': '1px solid var(--color-edge-muted)',
+                'grid-auto-columns': 'min-content',
+                'grid-auto-flow': 'column',
+                'align-items': 'center',
+                'padding-right': '7px',
+                'display': 'grid',
+                'gap': '7px'
+              }}
+            >
+              <div
+                style={{
+                  'grid-template-columns': 'min-content min-content',
+                  'box-sizing': 'border-box',
+                  'align-items': 'center',
+                  'padding': '0 4px',
+                  'display': 'grid',
+                  'height': '24px',
+                  'gap': '7px'
+                }}
+                onClick={() => {setKonsoleOpen(true)}}
+                class="dock-button-hover"
+              >
+                <IconLogo
+                  style={{
+                    'display': 'block',
+                    'height': '9px'
+                  }}
+                />
+                {/*<div style={{
+                  'font-family': 'monospace',
+                  'background-color': '#f00',
+                  'font-size': '10px',
+                  'padding': '0 4px',
+                }}>
+                  <Hotkey token={TOKENS.global.createCommand}/>
+                </div>*/}
+                <div class="**:border-none! flex size-full">
+                  <BasicHotkey shortcut="cmd+k" />
+                </div>
+              </div>
+
+              <div style={{
+                'background-color': 'var(--color-edge-muted)',
+                'height': '38px',
+                'width': '1px',
+              }}/>
+
+              <div
+                style={{
+                  'grid-template-columns': 'min-content min-content',
+                  'box-sizing': 'border-box',
+                  'align-items': 'center',
+                  'padding': '0 4px',
+                  'display': 'grid',
+                  'height': '24px',
+                  'gap': '10px'
+                }}
+                onClick={() => {setCreateMenuOpen(true)}}
+                class="dock-button-hover"
+              >
+                <MacroCreateIcon
+                  style={{
+                    'display': 'block',
+                    'height': '9px'
+                  }}
+                />
+                {/*<div style={{
+                  'background-color': '#f00',
+                  'font-family': 'monospace',
+                  'font-size': '10px',
+                  'padding': '0 4px',
+                }}>
+                  <Hotkey token={TOKENS.global.commandMenu}/>
+                </div>*/}
+                <div class="**:border-none! flex size-full">
+                  <BasicHotkey shortcut="c" />
+                </div>
+              </div>
+            </div>
+
+            <Show when={!isMobileWidth()}>
+              <div style={{
+                'border-top': '1px solid var(--edge-muted)',
+                'color': 'var(--ink-extra-muted)',
+                'justify-content': 'center',
+                'font-family': 'monospace',
+                'align-items': 'center',
+                'font-size': '0.75rem',
+                'line-height': '1rem',
+                'display': 'flex',
+                'gap': '4px',
+              }}>
+                <Show when={!hasPaid()}>
+                  <BasicTierLimit />
+                </Show>
+
+                <Show when={hasPaid()}>
+                  <Hints />
+                </Show>
+
+                <Show when={ENABLE_DOCK_NOTITIFCATIONS}>
+                  <QuickAccess />
+                  <GlobalNotificationBell notificationSource={notificationSource} />
+                </Show>
+              </div>
+            </Show>
+
+            <Show when={isMobileWidth()}>
+              <div></div>
+            </Show>
+
+            <div style={{
+              'border-left': '1px solid var(--color-edge-muted)',
               'grid-auto-columns': 'min-content',
               'grid-auto-flow': 'column',
               'align-items': 'center',
-              'padding-right': '7px',
+              'padding-left': '7px',
               'display': 'grid',
-              'gap': '7px'
-            }}
-          >
-            <div
-              style={{
-                'grid-template-columns': 'min-content min-content',
-                'box-sizing': 'border-box',
-                'align-items': 'center',
-                'padding': '0 4px',
-                'display': 'grid',
-                'height': '24px',
-                'gap': '7px'
-              }}
-              onClick={() => {setKonsoleOpen(true)}}
-              class="dock-button-hover"
-            >
-              <IconLogo
-                style={{
-                  'display': 'block',
-                  'height': '9px'
-                }}
-              />
-              {/*<div style={{
-                'font-family': 'monospace',
-                'background-color': '#f00',
-                'font-size': '10px',
-                'padding': '0 4px',
-              }}>
-                <Hotkey token={TOKENS.global.createCommand}/>
-              </div>*/}
-              <div class="**:border-none! flex size-full">
-                <BasicHotkey shortcut="cmd+k" />
-              </div>
-            </div>
-
-            <div style={{
-              'background-color': 'var(--color-edge-muted)',
               'height': '38px',
-              'width': '1px',
-            }}/>
-
-            <div
-              style={{
-                'grid-template-columns': 'min-content min-content',
-                'box-sizing': 'border-box',
-                'align-items': 'center',
-                'padding': '0 4px',
-                'display': 'grid',
-                'height': '24px',
-                'gap': '10px'
-              }}
-              onClick={() => {setCreateMenuOpen(true)}}
-              class="dock-button-hover"
-            >
-              <MacroCreateIcon
-                style={{
-                  'display': 'block',
-                  'height': '9px'
-                }}
-              />
-              {/*<div style={{
-                'background-color': '#f00',
-                'font-family': 'monospace',
-                'font-size': '10px',
-                'padding': '0 4px',
-              }}>
-                <Hotkey token={TOKENS.global.commandMenu}/>
-              </div>*/}
-              <div class="**:border-none! flex size-full">
-                <BasicHotkey shortcut="c" />
-              </div>
-            </div>
-          </div>
-
-          <Show when={!isMobileWidth()}>
-            <div style={{
-              'border-top': '1px solid var(--edge-muted)',
-              'color': 'var(--ink-extra-muted)',
-              'justify-content': 'center',
-              'font-family': 'monospace',
-              'align-items': 'center',
-              'font-size': '0.75rem',
-              'line-height': '1rem',
-              'display': 'flex',
-              'gap': '4px',
+              'gap': '4px'
             }}>
-              <Show when={!hasPaid()}>
-                <BasicTierLimit />
+              <Show when={isSoupActive()}>
+                <IconButton
+                  onClick={() => {
+                    const showHelp = activeSoupDrawerCommand();
+                    if(!showHelp){return};
+                    runCommand(showHelp);
+                  }}
+                  tooltip={{
+                    hotkeyToken: TOKENS.split.showHelpDrawer,
+                    label: 'Help',
+                  }}
+                  icon={IconQuestion}
+                  theme="clear"
+                  size="sm"
+                />
               </Show>
 
-              <Show when={hasPaid()}>
-                <Hints />
-              </Show>
-
-              <Show when={ENABLE_DOCK_NOTITIFCATIONS}>
-                <QuickAccess />
-                <GlobalNotificationBell notificationSource={notificationSource} />
-              </Show>
-            </div>
-          </Show>
-
-          <Show when={isMobileWidth()}>
-            <div></div>
-          </Show>
-
-          <div style={{
-            'border-left': '1px solid var(--color-edge-muted)',
-            'grid-auto-columns': 'min-content',
-            'grid-auto-flow': 'column',
-            'align-items': 'center',
-            'padding-left': '7px',
-            'display': 'grid',
-            'height': '38px',
-            'gap': '4px'
-          }}>
-            <Show when={isSoupActive()}>
               <IconButton
                 onClick={() => {
-                  const showHelp = activeSoupDrawerCommand();
-                  if(!showHelp){return};
-                  runCommand(showHelp);
+                  if(isRightPanelCollapsed()){track(TrackingEvents.RIGHTBAR.OPEN)}
+                  else{track(TrackingEvents.RIGHTBAR.CLOSE)}
+                  toggleRightPanel();
                 }}
+                theme={isRightPanelCollapsed() ? 'clear' : 'accent'}
                 tooltip={{
-                  hotkeyToken: TOKENS.split.showHelpDrawer,
-                  label: 'Help',
+                  hotkeyToken: TOKENS.global.toggleRightPanel,
+                  label: 'Toggle AI Panel',
                 }}
-                icon={IconQuestion}
+                icon={IconAtom}
+                size="sm"
+              />
+
+              <IconButton
+                tooltip={{
+                  hotkeyToken: TOKENS.global.createNewSplit,
+                  label: 'Create New Split'
+                }}
+                onClick={() => {
+                  const manager = globalSplitManager();
+                  if(manager){
+                    manager.createNewSplit({
+                      id: 'unified-list',
+                      type: 'component',
+                    });
+                  }
+                }}
+                icon={SplitIcon}
                 theme="clear"
                 size="sm"
               />
-            </Show>
 
-            <IconButton
-              onClick={() => {
-                if(isRightPanelCollapsed()){track(TrackingEvents.RIGHTBAR.OPEN)}
-                else{track(TrackingEvents.RIGHTBAR.CLOSE)}
-                toggleRightPanel();
-              }}
-              theme={isRightPanelCollapsed() ? 'clear' : 'accent'}
-              tooltip={{
-                hotkeyToken: TOKENS.global.toggleRightPanel,
-                label: 'Toggle AI Panel',
-              }}
-              icon={IconAtom}
-              size="sm"
-            />
+              <IconButton
+                tooltip={{label: isPresentMode() ? 'Exit Present Mode' : 'Enter Present Mode'}}
+                theme={isPresentMode() ? 'accent' : 'clear'}
+                onClick={togglePresentMode}
+                icon={IconPower}
+                size="sm"
+              />
 
-            <IconButton
-              tooltip={{
-                hotkeyToken: TOKENS.global.createNewSplit,
-                label: 'Create New Split'
-              }}
-              onClick={() => {
-                const manager = globalSplitManager();
-                if(manager){
-                  manager.createNewSplit({
-                    id: 'unified-list',
-                    type: 'component',
-                  });
-                }
-              }}
-              icon={SplitIcon}
-              theme="clear"
-              size="sm"
-            />
-
-            <IconButton
-              tooltip={{label: isPresentMode() ? 'Exit Present Mode' : 'Enter Present Mode'}}
-              theme={isPresentMode() ? 'accent' : 'clear'}
-              onClick={togglePresentMode}
-              icon={IconPower}
-              size="sm"
-            />
-
-            <IconButton
-              tooltip={{
-                label: settingsOpen() ? 'Close Settings' : 'Open Settings',
-                hotkeyToken: TOKENS.global.toggleSettings,
-              }}
-              theme={settingsOpen() ? 'accent' : 'clear'}
-              onDeepClick={() => {setSettingsOpen(true)}}
-              icon={IconGear}
-              size="sm"
-            />
+              <IconButton
+                tooltip={{
+                  label: settingsOpen() ? 'Close Settings' : 'Open Settings',
+                  hotkeyToken: TOKENS.global.toggleSettings,
+                }}
+                theme={settingsOpen() ? 'accent' : 'clear'}
+                onDeepClick={() => {setSettingsOpen(true)}}
+                icon={IconGear}
+                size="sm"
+              />
+            </div>
           </div>
-
-        </div>
-        </div>
+        </ClippedPanel>
       </div>
 
       <PresentModeGlitch
