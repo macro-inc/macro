1f05bd5c2a19283a32d0bb3610a137adc072b873
gbirman <25272206+gbirman@users.noreply.github.com>
2025-11-19 14:48:44 -0500

    feat: hide deleted search results front end [M-5238] (#232)

--- js/app/packages/service-search/generated/models/emailSearchResponseItemWithMetadataAllOfSnippet.ts
+++ js/app/packages/service-search/generated/models/emailSearchResponseItemWithMetadataAllOfSnippet.ts
@@ -5,4 +5,4 @@
  * OpenAPI spec version: 0.1.0
  */
 
-export type DocumentSearchResponseItemWithMetadataAllOfProjectId = string | null;
+export type EmailSearchResponseItemWithMetadataAllOfSnippet = string | null;
