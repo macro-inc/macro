727d0527a6adf2ed296226631b6bb1bae0b4f1ed
Evan Hutnik <evan.hutnik@gmail.com>
2025-11-19 13:16:28 -0500

    Don't make that change

--- rust/cloud-storage/macro_db_client/migrations/20251119150742_document_email_cascade.sql
+++
@@ -1,9 +0,0 @@
--- removing ON DELETE CASCADE for the email_attachments id row
-ALTER TABLE "document_email"
-DROP CONSTRAINT document_email_email_attachment_id_fkey;
-
--- Add it back without ON DELETE CASCADE
-ALTER TABLE "document_email"
-    ADD CONSTRAINT document_email_email_attachment_id_fkey
-        FOREIGN KEY (email_attachment_id)
-            REFERENCES email_attachments (id);
