973925e779d6185e8b015add6d17590c1fd28b5b
gbirman <25272206+gbirman@users.noreply.github.com>
2025-11-20 17:29:48 -0500

    fix: hide search results with no metadata (#281)

--- js/app/packages/app/component/command/useDocumentItems.ts
+++ js/app/packages/app/component/command/useDocumentItems.ts
@@ -12,7 +12,11 @@ export function useDocumentItems(fullTextSearchTerm: () => string) {
 
     const items: CommandItemCard[] = [];
     for (const doc of docResults.results) {
-      if (doc.document_search_results.length === 0 || doc.metadata?.deleted_at)
+      if (
+        doc.document_search_results.length === 0 ||
+        !doc.metadata ||
+        doc.metadata.deleted_at
+      )
         continue;
 
       for (const result of doc.document_search_results) {
