1d673a95b57e4b0ad759e80bf3e236604fce3cd5
Sean Aye <hello@seanaye.ca>
2025-11-21 21:44:36 +0000

    fix(soup): project parent filter (#298)

--- rust/cloud-storage/soup/src/outbound/pg_soup_repo/expanded/dynamic.rs
+++ rust/cloud-storage/soup/src/outbound/pg_soup_repo/expanded/dynamic.rs
@@ -250,7 +250,7 @@ fn build_project_filter(ast: Option<&Expr<ProjectLiteral>>) -> String {
         filter_ast::ExprFrame::Or(a, b) => format!("({a} OR {b})"),
         filter_ast::ExprFrame::Not(a) => format!("(NOT {a})"),
         filter_ast::ExprFrame::Literal(ProjectLiteral::ProjectId(p)) => {
-            format!("p.id = '{p}'")
+            format!(r#"p."parentId" = '{p}'"#)
         }
         filter_ast::ExprFrame::Literal(ProjectLiteral::Owner(o)) => format!("p.owner = '{o}'"),
     });
