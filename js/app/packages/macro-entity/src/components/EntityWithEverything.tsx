038f00276edbacb4df326fc63eaf7641152c4518
gbirman <25272206+gbirman@users.noreply.github.com>
2025-11-24 14:30:56 -0500

    fix: entity data click on project icon go to not working (#319)

--- js/app/packages/macro-entity/src/components/EntityWithEverything.tsx
+++ js/app/packages/macro-entity/src/components/EntityWithEverything.tsx
@@ -327,7 +327,7 @@ export function EntityWithEverything(
   // the split. Those buttons should have a 'data-blocks-navigation'
   function blocksNavigation(e: PointerEvent | MouseEvent): boolean {
     const { target } = e;
-    if (target instanceof HTMLElement) {
+    if (target instanceof Element) {
       const closest = target.closest('[data-blocks-navigation]');
       if (closest && entityDivRef()?.contains(closest)) return true;
     }
