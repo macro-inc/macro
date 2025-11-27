9d584ed22e239d7e2d5196c51d9ec8f759771fe9
Will Hutchinson <hutch@macro.com>
2025-11-27 12:15:29 -0500

    fix(infra): pulumi deleting tags accidentally (#353)

--- infra/stacks/cloud-storage-cache/index.ts
+++ infra/stacks/cloud-storage-cache/index.ts
@@ -1,3 +1,4 @@
+import * as aws from '@pulumi/aws';
 import * as awsx from '@pulumi/awsx';
 import { stack } from '@shared';
 
@@ -13,7 +14,32 @@ const ecrRepository = new awsx.ecr.Repository(`${BASE_NAME}-ecr`, {
   name: `${BASE_NAME}`,
   imageTagMutability: 'MUTABLE',
   forceDelete: true,
+  lifecyclePolicy: {
+    // we don't want the default lifecycle policy
+    skip: true,
+  },
   tags: tags,
+});
+
+new aws.ecr.LifecyclePolicy(`${BASE_NAME}-lifecycle-policy`, {
+  repository: ecrRepository.repository.id,
+  policy: {
+    rules: [
+      {
+        rulePriority: 1,
+        description: 'remove untagged images older than 1 day',
+        selection: {
+          tagStatus: 'untagged',
+          countType: 'sinceImagePushed',
+          countUnit: 'days',
+          countNumber: 1,
+        },
+        action: {
+          type: 'expire',
+        },
+      },
+    ],
+  },
 });
 
 export const cloudStorageCacheEcrRepositoryUrl = ecrRepository.url;
