ea8b83a2f9c7080910d0a21105b13eac59efe131
Hutch <hutch@macro.com>
2025-12-01 16:24:55 -0500

    feat: add get_existing_users internal call to auth service

--- rust/cloud-storage/authentication_service/src/api/internal/mod.rs
+++ rust/cloud-storage/authentication_service/src/api/internal/mod.rs
@@ -10,10 +10,12 @@ use super::user::post_get_names;
 // needs to be public in api crate for swagger
 mod google_access_token;
 mod remove_link;
+mod post_get_existing_users;
 
 pub fn router() -> Router<ApiContext> {
     Router::new()
         .route("/google_access_token", get(google_access_token::handler))
         .route("/get_names", post(post_get_names::handler_internal))
+        .route("/get_existing_users", get(post_get_existing_users::handler))
         .route("/remove_link", delete(remove_link::handler))
 }
