cf18e57c03d990ab30315fb46dc5c8d14c1daf7a
gbirman <25272206+gbirman@users.noreply.github.com>
2025-12-01 16:17:00 -0500

    Revert "feat: send token to services ws (#335)" (#386)

--- js/app/packages/service-storage/websocket.ts
+++ js/app/packages/service-storage/websocket.ts
@@ -1,8 +1,6 @@
 import { SERVER_HOSTS } from '@core/constant/servers';
-import { getMacroApiToken } from '@service-auth/fetch';
 import {
   ConstantBackoff,
-  type UrlResolver,
   type Websocket,
   WebsocketBuilder,
   WebsocketEvent,
@@ -22,13 +20,7 @@ const HEARTBEAT_INTERVAL = 300000;
  */
 const HEARTBEAT_TIMEOUT = 5000;
 
-const getUrl: UrlResolver = async () => {
-  const token = await getMacroApiToken();
-  const baseUrl = SERVER_HOSTS['websocket-service'];
-  return `${baseUrl}?token=${token}`;
-};
-
-export const ws = new WebsocketBuilder(getUrl)
+export const ws = new WebsocketBuilder(SERVER_HOSTS['websocket-service'])
   .withBackoff(new ConstantBackoff(1500))
   .withHeartbeat({
     pingMessage: JSON.stringify({ action: 'wsping' }),
