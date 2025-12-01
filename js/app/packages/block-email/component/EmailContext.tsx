e5a4a5b99b942506abb7387e586da45ccdac0208
Gabriel Birman <25272206+gbirman@users.noreply.github.com>
2025-12-01 15:21:48 -0500

    nit make it an accessor

--- js/app/packages/block-email/component/EmailContext.tsx
+++ js/app/packages/block-email/component/EmailContext.tsx
@@ -30,7 +30,7 @@ export type EmailContextValue = {
   setFocusedMessageId: Setter<string | undefined>;
   filteredMessages: Accessor<MessageWithBodyReplyless[]>;
   threadData: Accessor<Thread | undefined>;
-  archiveThread: () => boolean;
+  archiveThread: Accessor<boolean>;
   activeTargetMessageId: Accessor<string | undefined>;
 };
 
