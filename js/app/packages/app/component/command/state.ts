3b99bbfa8ebbbd7135b30905a7a1e709c547ee04
Gabriel Birman <25272206+gbirman@users.noreply.github.com>
2025-12-02 13:38:22 -0500

    lower serach service debounce ms to 200 in command k

--- js/app/packages/app/component/command/state.ts
+++ js/app/packages/app/component/command/state.ts
@@ -2,7 +2,7 @@ import { createControlledOpenSignal } from '@core/util/createControlledOpenSigna
 import { debounce } from '@solid-primitives/scheduled';
 import { createEffect, createSignal, untrack } from 'solid-js';
 
-const SEARCH_SERVICE_DEBOUNCE_MS = 300;
+const SEARCH_SERVICE_DEBOUNCE_MS = 200;
 const LOCAL_FUZZY_SEARCH_DEBOUNCE_MS = 10;
 
 export const [konsoleOpen, setKonsoleOpen] = createControlledOpenSignal();
