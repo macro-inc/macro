fef3a5290c21d38b1f5179026aec70de5c954590
Caleb Taylor <aquaductape@gmail.com>
2025-12-04 16:21:33 -0500

    update

--- js/app/packages/app/component/ViewConfig.tsx
+++ js/app/packages/app/component/ViewConfig.tsx
@@ -255,9 +255,6 @@ const ALL_VIEWCONFIG_DEFAULTS = {
     sort: {
       sortBy: 'viewed_at',
     },
-    filters: {
-      typeFilter: ['channel', 'chat', 'document', 'email', 'project'],
-    },
     hotkeyOptions: {
       e: (entity: EntityData) => {
         if (entity.type === 'email') {
