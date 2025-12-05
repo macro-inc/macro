6c4f79bff07258a3eaf233de2d70f1d6e0b8f07e
Peter Chinman <peter.chinman@gmail.com>
2025-12-05 09:55:49 -0500

    use get or create document endpoint

--- js/app/packages/block-email/component/AttachmentPill.tsx
+++ js/app/packages/block-email/component/AttachmentPill.tsx
@@ -1,13 +1,15 @@
 import { EntityIcon } from '@core/component/EntityIcon';
+import { toast } from '@core/component/Toast/Toast';
 import { fileTypeToBlockName } from '@core/constant/allBlocks';
 import { isErr } from '@core/util/maybeResult';
-import { uploadFile } from '@core/util/upload';
 import X from '@icon/regular/x.svg';
+import { queryKeys, useQueryClient } from '@macro-entity';
+import { logger } from '@observability';
 import { emailClient } from '@service-email/client';
 import type { Attachment } from '@service-email/generated/schemas/attachment';
+import { storageServiceClient } from '@service-storage/client';
 import { FileTypeMap } from '@service-storage/fileTypeMap';
 import type { FileType } from '@service-storage/generated/schemas/fileType';
-import { platformFetch } from 'core/util/platformFetch';
 import { Show } from 'solid-js';
 import { useSplitLayout } from '../../app/component/split-layout/layout';
 
@@ -23,40 +25,61 @@ export function EmailAttachmentPill(props: {
   let parentDiv!: HTMLDivElement;
 
   const { replaceOrInsertSplit } = useSplitLayout();
+  const entityQueryClient = useQueryClient();
 
   const fileType = props.attachment.mime_type
     ? (mimeToFileExtTypeMap.get(props.attachment.mime_type) as FileType)
     : undefined;
 
+  const clickHandler = async () => {
+    const dbId = props.attachment.db_id;
+    if (!dbId) return;
+    const response = await emailClient.getOrCreateAttachmentDocumentId({
+      id: dbId,
+    });
+    if (isErr(response)) {
+      toast.failure('Failed to get attachment. Please try again.');
+      return logger.error('Failed to get or create attachment document id', {
+        error: new Error(
+          'Failed to get or create attachment document id: ' + response[0]
+        ),
+      });
+    }
+    const { document_id } = response[1];
+
+    const maybeDocumentMetadata =
+      await storageServiceClient.getDocumentMetadata({
+        documentId: document_id,
+      });
+    if (isErr(maybeDocumentMetadata)) {
+      toast.failure('Failed to get attachment. Please try again.');
+      return logger.error(
+        'Failed to get or create attachment document metadata',
+        {
+          error: new Error(
+            'Failed to get or create attachment document metadata: ' +
+              maybeDocumentMetadata[0]
+          ),
+        }
+      );
+    }
+
+    entityQueryClient.invalidateQueries({
+      queryKey: queryKeys.all.dss,
+    });
+
+    const blockName = fileTypeToBlockName(fileType);
+    replaceOrInsertSplit({
+      type: blockName,
+      id: document_id,
+    });
+  };
+
   return (
     <div
       ref={parentDiv}
       class={`items-center text-xs flex flex-row ${props.removable ? 'pl-2' : 'p-2'} m-1 w-36 rounded border border-edge hover:bg-hover hover-transition-bg`}
-      onclick={async () => {
-        const dbId = props.attachment.db_id;
-        if (!dbId) return;
-        const response = await emailClient.getAttachmentUrl({ id: dbId });
-        if (isErr(response)) return;
-
-        const dataUrl = response[1].attachment.data_url;
-        if (!dataUrl) return;
-        const fileBlob = await platformFetch(dataUrl)
-          .then((res) => res.blob())
-          .then((blob) => {
-            return blob;
-          });
-        const file = new File(
-          [fileBlob],
-          props.attachment.filename ?? 'EmailAttachment'
-        );
-        const uploadedFile = await uploadFile(file, 'dss');
-        if (uploadedFile.failed || uploadedFile.type !== 'document') return;
-        const blockName = fileTypeToBlockName(uploadedFile.fileType);
-        replaceOrInsertSplit({
-          type: blockName,
-          id: uploadedFile.documentId,
-        });
-      }}
+      onclick={clickHandler}
     >
       <Show when={fileType !== undefined || props.attachment.mime_type}>
         <EntityIcon
