ffc4b2a0c4e08c4702374dd0ff751cf1e65a0537
Sean Aye <hello@seanaye.ca>
2025-12-05 11:53:53 -0500

    make it a workspace

--- js/app/tauri/src-tauri/src/lib.rs
+++ js/app/tauri/src-tauri/src/lib.rs
@@ -1,20 +1,19 @@
-use crate::logger::{Context, ContextErr, Logger};
+use logger::Logger;
+use navigation_plugin::MacroNavigationPlugin;
+use navigation_plugin::scheme::MacroScheme;
 use reqwest::cookie::CookieStore;
 use reqwest::header::COOKIE;
-use serde::{Deserialize, Serialize};
-use std::{borrow::Cow, collections::HashMap};
+use rootcause::{Report, report};
+use serde::Serialize;
 use tauri::http::{HeaderMap, HeaderValue};
 use tauri::{AppHandle, Emitter};
-use tauri::{Manager, Runtime, plugin::Plugin};
+use tauri::{Manager, Runtime};
 use tauri_plugin_deep_link::{DeepLinkExt, OpenUrlEvent};
-use tauri_plugin_opener::OpenerExt;
-use thiserror::Error;
 use url::Url;
 
 /// This module provides debuging utilities and should not be compiled in prodiction builds
 #[cfg(debug_assertions)] // do not remove this
 mod debug;
-mod logger;
 
 /// domains which the tauri webview can render.
 /// This should be as restrictive as possible.
@@ -136,21 +135,14 @@ impl AppChain for tauri::App {
     }
 }
 
-#[derive(Debug, Error)]
-enum AppError {
-    #[error(transparent)]
-    SchemeErr(#[from] SchemeError),
-    #[error(transparent)]
-    ContextErr(#[from] ContextErr),
-    #[error(transparent)]
-    Tauri(#[from] tauri::Error),
-}
-
 fn attach_deep_link_handler(app: &mut tauri::App) {
-    fn inner_handler(ev: OpenUrlEvent, handle: &AppHandle) -> Result<(), AppError> {
+    fn inner_handler(ev: OpenUrlEvent, handle: &AppHandle) -> Result<(), Report> {
         let urls = ev.urls();
         tracing::info!("received open url event {urls:?}");
-        let url = urls.into_iter().next().context("expected at least 1 url")?;
+        let url = urls
+            .into_iter()
+            .next()
+            .ok_or_else(|| report!("expected at least 1 url"))?;
 
         let macro_scheme = MacroScheme::new(url)?;
 
@@ -177,148 +169,4 @@ fn attach_deep_link_handler(app: &mut tauri::App) {
             inner_handler(ev, &handle).log_and_consume();
         }
     });
-}
-
-struct MacroNavigationPlugin {
-    internal_domains: Vec<Url>,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(transparent)]
-struct MacroScheme(Url);
-
-impl MacroScheme {
-    fn new(url: Url) -> Result<Self, SchemeError> {
-        let "macro" = url.scheme() else {
-            return Err(SchemeError::InvalidScheme {
-                expected: "macro".to_string(),
-                found: url.scheme().to_string(),
-            });
-        };
-        Ok(Self(url))
-    }
-    /// turn a http(s) url into a macro scheme url
-    fn from_url(url: &Url) -> Result<Self, SchemeError> {
-        let ("http" | "https" | "tauri") = url.scheme() else {
-            return Err(SchemeError::InvalidScheme {
-                expected: "http(s) or tauri".to_string(),
-                found: url.scheme().to_string(),
-            });
-        };
-
-        let Some(rest) = url.fragment().map(|s| s.trim_start_matches('/')) else {
-            return Err(SchemeError::MissingFragment);
-        };
-        let query = url.query();
-        let inner = match query {
-            Some(q) => format!("macro://{rest}?{q}"),
-            None => format!("macro://{rest}"),
-        }
-        .parse::<Url>()?;
-        Ok(MacroScheme(inner))
-    }
-}
-
-impl AsRef<str> for MacroScheme {
-    fn as_ref(&self) -> &str {
-        self.0.as_str()
-    }
-}
-
-#[derive(Debug, Error)]
-enum SchemeError {
-    #[error("The input url did not have a fragment")]
-    MissingFragment,
-    #[error("{0}")]
-    Parse(#[from] url::ParseError),
-    #[error("Invalid scheme received. Expected {expected}, found {found}")]
-    InvalidScheme { expected: String, found: String },
-}
-
-impl MacroNavigationPlugin {
-    pub fn new(allow_list: &'static [&'static str]) -> Result<Self, url::ParseError> {
-        Ok(MacroNavigationPlugin {
-            internal_domains: allow_list
-                .iter()
-                .map(|s| s.parse())
-                .collect::<Result<Vec<_>, _>>()?,
-        })
-    }
-
-    #[tracing::instrument(ret, level = tracing::Level::DEBUG, skip(self))]
-    fn is_internal_domain(&self, url: &Url) -> bool {
-        self.internal_domains.iter().any(|cur| {
-            cur.scheme().eq(url.scheme())
-                && cur.domain().eq(&url.domain())
-                && cur.port().eq(&url.port())
-        })
-    }
-
-    #[tracing::instrument(ret, level = tracing::Level::DEBUG)]
-    fn transform_external(mut url: Url) -> Url {
-        let Some(query) = url.query() else {
-            return url;
-        };
-
-        #[derive(Debug, Deserialize)]
-        struct AuthCallbackQuery<'a> {
-            original_url: Option<Url>,
-            #[serde(flatten, borrow)]
-            remaining: HashMap<Cow<'a, str>, Cow<'a, str>>,
-        }
-
-        #[derive(Debug, Serialize)]
-        struct MacroCallbackQuery<'a> {
-            original_url: MacroScheme,
-            #[serde(flatten, borrow)]
-            remaining: HashMap<Cow<'a, str>, Cow<'a, str>>,
-        }
-
-        if let Ok(AuthCallbackQuery {
-            original_url: Some(cb),
-            remaining,
-        }) = serde_qs::from_str(query).log_err()
-        {
-            let Ok(macro_scheme) = MacroScheme::from_url(&cb) else {
-                return url;
-            };
-
-            url.set_query(Some(
-                serde_qs::to_string(&MacroCallbackQuery {
-                    original_url: macro_scheme,
-                    remaining,
-                })
-                .expect("serialization should not fail")
-                .as_str(),
-            ));
-        }
-        url.query_pairs_mut().append_pair("is_mobile", "true");
-        url
-    }
-}
-
-impl<R: Runtime> Plugin<R> for MacroNavigationPlugin {
-    fn name(&self) -> &'static str {
-        std::any::type_name_of_val(self)
-    }
-
-    fn on_navigation(&mut self, webview: &tauri::Webview<R>, url: &tauri::Url) -> bool {
-        let allowed_internal = self.is_internal_domain(url);
-
-        if !allowed_internal {
-            // we are navigating somewhere external to the app
-            // open in system default browser
-            // spawn a detached thread to avoid blocking,
-            // on android this panics if called on the main thread
-            let app_handle = webview.app_handle().clone();
-            let url = url.clone();
-            std::thread::spawn(move || {
-                app_handle
-                    .opener()
-                    .open_url(Self::transform_external(url).as_str(), None::<&str>)
-                    .log_and_consume();
-            });
-        }
-        allowed_internal
-    }
 }
