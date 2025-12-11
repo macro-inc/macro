e9d5ded44343a192a53bc6acb9cb7bd231abda57
Evan Hutnik <evan.hutnik@gmail.com>
2025-12-11 13:01:18 -0500

    width and height for attachments

--- rust/cloud-storage/comms_db_client/src/model/mod.rs
+++ rust/cloud-storage/comms_db_client/src/model/mod.rs
@@ -36,6 +36,8 @@ pub struct Attachment {
     pub message_id: Uuid,
     pub entity_type: String,
     pub entity_id: String,
+    pub width: Option<i32>,
+    pub height: Option<i32>,
     pub created_at: chrono::DateTime<chrono::Utc>,
 }
 
@@ -43,6 +45,9 @@ pub struct Attachment {
 pub struct NewAttachment {
     pub entity_type: String,
     pub entity_id: String,
+    // dimensions of image/video
+    pub height: Option<i32>,
+    pub width: Option<i32>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, ToSchema, PartialEq)]
