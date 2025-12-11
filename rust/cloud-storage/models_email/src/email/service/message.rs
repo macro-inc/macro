48eea6decf0fc94bec6c0f2923b782992ec9239a
Hutch <hutch@macro.com>
2025-12-11 12:39:14 -0500

    feat(email_db): update get_thread_histories with sender and pretty_sender

--- rust/cloud-storage/models_email/src/email/service/message.rs
+++ rust/cloud-storage/models_email/src/email/service/message.rs
@@ -310,6 +310,11 @@ pub struct ThreadHistoryInfo {
     pub snippet: Option<String>,
     pub user_id: String,
     pub subject: Option<String>,
+    /// The sender of the latest email in the thread.
+    pub sender: String,
+    /// The pretty sender of the latest email in the thread.
+    /// This could be the sender's email if there is no contact name for the sender.
+    pub pretty_sender: String,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
