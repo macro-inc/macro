72b94c006dacb37ad8cbc6c09afbd0bd0a6bbad2
Rahul <43100342+dev-rb@users.noreply.github.com>
2025-12-12 13:44:26 -0500

    Add tooltip. Add remove signature message to paywall

--- js/app/packages/block-email/component/MacroSignatureButton.tsx
+++ js/app/packages/block-email/component/MacroSignatureButton.tsx
@@ -1,6 +1,7 @@
 import { MACRO_EMAIL_SIGNATURE } from '@block-email/constants';
 import { useHasPaidAccess } from '@core/auth';
-import { usePaywallState } from '@core/constant/PaywallState';
+import { Tooltip } from '@core/component/Tooltip';
+import { PaywallKey, usePaywallState } from '@core/constant/PaywallState';
 import { Show } from 'solid-js';
 
 interface MacroSignatureButtonProps {
@@ -12,20 +13,22 @@ export const MacroSignatureButton = (props: MacroSignatureButtonProps) => {
   const hasPaidAccess = useHasPaidAccess();
   return (
     <Show when={hasPaidAccess()}>
-      <button
-        type="button"
-        class="hover:bg-hover pointer-events-all"
-        tabindex={-1}
-        // The text area uses non delegated events to capture on click and restore focus
-        // to the editor. We want to capture the click here so we can open the paywall.
-        // That's why we use `on:click` instead of `onClick`
-        on:click={(e) => {
-          e.stopImmediatePropagation();
-          paywall.showPaywall();
-        }}
-      >
-        {props.signature ?? MACRO_EMAIL_SIGNATURE}
-      </button>
+      <Tooltip tooltip="Subscribe to remove watermark" class="w-fit">
+        <button
+          type="button"
+          class="hover:bg-hover pointer-events-all"
+          tabindex={-1}
+          // The text area uses non delegated events to capture on click and restore focus
+          // to the editor. We want to capture the click here so we can open the paywall.
+          // That's why we use `on:click` instead of `onClick`
+          on:click={(e) => {
+            e.stopImmediatePropagation();
+            paywall.showPaywall(PaywallKey.REMOVE_SIGNATURE);
+          }}
+        >
+          {props.signature ?? MACRO_EMAIL_SIGNATURE}
+        </button>
+      </Tooltip>
     </Show>
   );
 };
