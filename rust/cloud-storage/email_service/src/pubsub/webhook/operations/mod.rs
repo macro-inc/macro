02123c964f96cef563483c81911d8a6af4df0c6f
Evan Hutnik <evan.hutnik@gmail.com>
2025-12-15 14:37:19 -0500

    Notify search on label updates

--- rust/cloud-storage/email_service/src/pubsub/webhook/operations/mod.rs
+++ rust/cloud-storage/email_service/src/pubsub/webhook/operations/mod.rs
@@ -1,4 +1,5 @@
 pub(in crate::pubsub) mod delete_message;
 pub(in crate::pubsub) mod gmail_message;
+pub(in crate::pubsub) mod shared;
 pub(in crate::pubsub) mod update_labels;
 pub(in crate::pubsub) mod upsert_message;
