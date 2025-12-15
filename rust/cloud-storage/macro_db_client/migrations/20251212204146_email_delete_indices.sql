ef3caa676151ebd3a2fa19c53041558390408cea
Evan Hutnik <evan.hutnik@gmail.com>
2025-12-15 17:00:28 -0500

    If not exists

--- rust/cloud-storage/macro_db_client/migrations/20251212204146_email_delete_indices.sql
+++ rust/cloud-storage/macro_db_client/migrations/20251212204146_email_delete_indices.sql
@@ -1,13 +1,13 @@
 -- Indices to speed up email link deletion
-CREATE INDEX idx_email_messages_replying_to_id
+CREATE INDEX IF NOT EXISTS idx_email_messages_replying_to_id
     ON public.email_messages (replying_to_id)
     WHERE replying_to_id IS NOT NULL;
 
-CREATE INDEX idx_email_user_history_thread_id
+CREATE INDEX IF NOT EXISTS idx_email_user_history_thread_id
     ON public.email_user_history (thread_id);
 
-CREATE INDEX idx_email_threads_link_id
+CREATE INDEX IF NOT EXISTS idx_email_threads_link_id
     ON public.email_threads (link_id);
 
-CREATE INDEX idx_email_messages_link_id
+CREATE INDEX IF NOT EXISTS idx_email_messages_link_id
     ON public.email_messages (link_id);
