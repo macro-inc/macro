ee983eeded5e405b067974fd814d5fd985e00531
Peter Chinman <peter.chinman@gmail.com>
2025-12-15 18:01:40 -0500

    clean up

--- js/app/packages/block-email/component/EmailMessageBody.tsx
+++ js/app/packages/block-email/component/EmailMessageBody.tsx
@@ -32,9 +32,7 @@ interface EmailMessageBodyProps {
 }
 
 export function EmailMessageBody(props: EmailMessageBodyProps) {
-  const [showFullHTML, setShowFullHTML] = createSignal<boolean>(
-    props.threadMessageIndex === 0
-  );
+  const [showFullHTML, setShowFullHTML] = createSignal<boolean>(false);
   const userEmail = useEmail();
 
   if (DEV_MODE_ENV) {
@@ -45,45 +43,61 @@ export function EmailMessageBody(props: EmailMessageBodyProps) {
   }
 
   // If we don't have body replyless, it may be because it hasn't been generated yet. For instance, this is the case immediately after a message is sent. We can use the HTML to parse the message correctly.
-  let bodyReplyless = props.message.body_replyless;
-  if (!props.message.body_replyless) {
-    if (props.message.body_html_sanitized) {
-      const parser = new DOMParser();
-      const doc = parser.parseFromString(
-        props.message.body_html_sanitized.toString(),
-        'text/html'
-      );
-      const quoted = doc.body.querySelector('.macro_quote');
-      if (quoted) {
-        quoted?.remove();
-        bodyReplyless = doc.body.innerHTML;
+  const bodyReplylessProcessed = createMemo(() => {
+    let replyless = props.message.body_replyless ?? '';
+    if (!replyless) {
+      if (props.message.body_html_sanitized) {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(
+          props.message.body_html_sanitized.toString(),
+          'text/html'
+        );
+        const styleTags = Array.from(doc.head?.querySelectorAll('style') ?? [])
+          .map((style) => style.outerHTML)
+          .join('\n');
+        const quoted = doc.body.querySelector('.macro_quote');
+        if (quoted) {
+          quoted?.remove();
+          return styleTags
+            ? `${styleTags}\n${doc.body.innerHTML}`
+            : doc.body.innerHTML;
+        }
       }
     }
-  }
+    return replyless;
+  });
 
-  const isPlaintext = !props.message.body_html_sanitized;
+  const isPlaintext = () => !props.message.body_html_sanitized;
 
-  // TODO the only reason to parse the body html is to find out if it contains a table. Instead the backend should return this and we should remove this parsing.
-  const parsedBodyHtml = props.message.body_html_sanitized
-    ? parseEmailContent(props.message.body_html_sanitized, false, false)
-    : undefined;
+  const parsedBodyHtml = createMemo(() => {
+    return props.message.body_html_sanitized
+      ? parseEmailContent(
+          props.message.body_html_sanitized,
+          !showFullHTML(),
+          !showFullHTML()
+        )
+      : undefined;
+  });
 
-  const parsedBodyReplyless = bodyReplyless
-    ? parseEmailContent(bodyReplyless)
-    : undefined;
+  const parsedBodyReplyless = createMemo(() => {
+    const processed = bodyReplylessProcessed();
+    return processed ? parseEmailContent(processed) : undefined;
+  });
 
   const source = () => {
-    return showFullHTML() ? parsedBodyHtml : parsedBodyReplyless;
+    return showFullHTML() || props.threadMessageIndex === 0
+      ? parsedBodyHtml()
+      : parsedBodyReplyless();
   };
 
   const hasHiddenReplyStructure = () => {
     return (
-      !isPlaintext &&
-      ((bodyReplyless &&
-        bodyReplyless?.toString().replace(/\s+/g, '').length !==
+      !isPlaintext() &&
+      ((bodyReplylessProcessed() &&
+        bodyReplylessProcessed().toString().replace(/\s+/g, '').length !==
           props.message.body_html_sanitized?.toString().replace(/\s+/g, '')
             .length) ||
-        parsedBodyReplyless?.signature)
+        source()?.signature)
     );
   };
 
@@ -92,7 +106,7 @@ export function EmailMessageBody(props: EmailMessageBodyProps) {
     return (
       (props.message.from?.email === userEmail() ||
         props.message.labels.some((l) => l.name === 'CATEGORY_PERSONAL')) &&
-      !parsedBodyHtml?.hasTable
+      !parsedBodyHtml()?.hasTable
     );
   });
 
@@ -105,9 +119,7 @@ export function EmailMessageBody(props: EmailMessageBodyProps) {
     styleEl.textContent = `img{display: var(--macro-email-img-display, initial);}`;
     shadow.appendChild(styleEl);
     const messageDiv = document.createElement('div');
-    messageDiv.innerHTML = showFullHTML()
-      ? (parsedBodyHtml?.mainContent ?? '')
-      : (parsedBodyReplyless?.mainContent ?? '');
+    messageDiv.innerHTML = source()?.mainContent ?? '';
     messageDiv.style.userSelect = 'text';
     messageDiv.style.cursor = 'var(--cursor-auto)';
     messageDiv.style.overflow = 'auto';
@@ -221,7 +233,7 @@ export function EmailMessageBody(props: EmailMessageBodyProps) {
               );
             }}
           </Match>
-          <Match when={isPlaintext}>
+          <Match when={isPlaintext()}>
             <StaticMarkdown
               markdown={props.message.body_text!}
               theme={channelTheme}
