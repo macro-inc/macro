7c06ac3cdd49480bd2f82fa9bf6a51eafc79f3e8
Sean Aye <hello@seanaye.ca>
2025-12-15 16:56:26 -0500

    fix test fixtures

--- rust/cloud-storage/notification_db_client/src/user_notification/get/unsent.rs
+++ rust/cloud-storage/notification_db_client/src/user_notification/get/unsent.rs
@@ -76,25 +76,25 @@ mod tests {
             (
                 "11111111-1111-1111-1111-111111111111",
                 "item_id",
-                "item_type",
+                "document",
                 notification_event_type,
             ),
             (
                 "22222222-2222-2222-2222-222222222222",
                 "item_id2",
-                "item_type",
+                "document",
                 notification_event_type,
             ),
             (
                 "33333333-3333-3333-3333-333333333333",
                 "item_id3",
-                "item_type",
+                "document",
                 notification_event_type,
             ),
             (
                 "44444444-4444-4444-4444-444444444444",
                 "item_id4",
-                "item_type",
+                "document",
                 notification_event_type,
             ),
         ];
