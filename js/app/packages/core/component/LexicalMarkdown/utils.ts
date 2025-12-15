c2f494ef6ad899cebc0e8f0295c1f8a4e6a49a28
Peter Chinman <peter.chinman@gmail.com>
2025-12-15 14:09:32 -0500

    fix[email]: stop email draft autofocus

--- js/app/packages/core/component/LexicalMarkdown/utils.ts
+++ js/app/packages/core/component/LexicalMarkdown/utils.ts
@@ -369,8 +369,7 @@ export function setEditorStateFromHtml(
       const nodes = $generateNodesFromDOM(editor, dom);
       const root = $getRoot();
       root.clear();
-      root.select();
-      $insertNodes(nodes);
+      root.append(...nodes);
     });
     editor.read(() => {});
     return editor.getEditorState();
@@ -380,8 +379,7 @@ export function setEditorStateFromHtml(
     const nodes = $generateNodesFromDOM(editor, dom);
     const root = $getRoot();
     root.clear();
-    root.select();
-    $insertNodes(nodes);
+    root.append(...nodes);
   }
 }
 
