220b9afd8905a2184193991d26c245c0a58d29eb
Gabriel Birman <25272206+gbirman@users.noreply.github.com>
2025-12-16 13:38:04 -0500

    some scrolling fixes

--- js/app/packages/app/component/UnifiedListView.tsx
+++ js/app/packages/app/component/UnifiedListView.tsx
@@ -2,6 +2,7 @@ import {
   useGlobalBlockOrchestrator,
   useGlobalNotificationSource,
 } from '@app/component/GlobalAppState';
+import { BlockChannelProps } from '@block-channel/component/Block';
 import { URL_PARAMS as CHANNEL_PARAMS } from '@block-channel/constants';
 import { URL_PARAMS as EMAIL_PARAMS } from '@block-email/constants';
 import { URL_PARAMS as MD_PARAMS } from '@block-md/constants';
@@ -1101,9 +1102,14 @@ export function UnifiedListView(props: UnifiedListViewProps) {
     if (entity.type === 'document')
       return documentEntityClickHandler(entity, event, location);
 
+    const params =
+      entity.type === 'channel' && location?.type === 'channel'
+        ? ({ target: { messageId: location.messageId } } as BlockChannelProps)
+        : undefined;
+
     const handle = event.altKey
-      ? insertSplit({ type: entity.type, id: entity.id })
-      : replaceOrInsertSplit({ type: entity.type, id: entity.id });
+      ? insertSplit({ type: entity.type, id: entity.id, params })
+      : replaceOrInsertSplit({ type: entity.type, id: entity.id, params });
 
     handle?.activate();
 
@@ -1111,10 +1117,11 @@ export function UnifiedListView(props: UnifiedListViewProps) {
 
     switch (location.type) {
       case 'channel': {
-        const blockHandle = await blockOrchestrator.getBlockHandle(entity.id);
-        await blockHandle?.goToLocationFromParams({
-          [CHANNEL_PARAMS.message]: location.messageId,
-        });
+        // NOTE: this is handled by the channel block params
+        // const blockHandle = await blockOrchestrator.getBlockHandle(entity.id);
+        // await blockHandle?.goToLocationFromParams({
+        //   [CHANNEL_PARAMS.message]: location.messageId,
+        // });
         break;
       }
       case 'email': {
