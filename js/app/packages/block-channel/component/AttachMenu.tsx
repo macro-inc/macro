56cfb4041bcc7a7064ef6f8695dd9f59214c8c04
Rahul <43100342+dev-rb@users.noreply.github.com>
2025-12-17 15:50:27 -0500

    Replace overscan property usage with bufferSize

--- js/app/packages/block-channel/component/AttachMenu.tsx
+++ js/app/packages/block-channel/component/AttachMenu.tsx
@@ -150,7 +150,7 @@ export function AttachMenu(props: AttachMenuProps) {
               >
                 <VList
                   data={rankedHistory()}
-                  overscan={10}
+                  bufferSize={400}
                   itemSize={40}
                   style={{ height: '100%', contain: 'content' }}
                 >
