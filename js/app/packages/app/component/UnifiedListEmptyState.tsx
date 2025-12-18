a6db3f2ad7cdde96e37a7840df6a9caf771fecdc
Gabriel Birman <25272206+gbirman@users.noreply.github.com>
2025-12-18 17:40:49 -0500

    lower timeout more

--- js/app/packages/app/component/UnifiedListEmptyState.tsx
+++ js/app/packages/app/component/UnifiedListEmptyState.tsx
@@ -13,7 +13,7 @@ import { useSplitPanelOrThrow } from './split-layout/layoutUtils';
 false && fileSelector;
 false && folderSelector;
 
-const EMPTY_STATE_HELP_DRAWER_TIMEOUT_MS = 100;
+const EMPTY_STATE_HELP_DRAWER_TIMEOUT_MS = 0;
 
 const DEFAULT_EMPTY_MESSAGE = 'No items to show.';
 
