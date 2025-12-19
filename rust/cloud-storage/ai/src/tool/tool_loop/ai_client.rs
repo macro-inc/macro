c9d34732723ad2b74647ccb9116d47771962f1d0
ehayes <eqhayes@protonmail.com>
2025-12-19 11:39:59 -0500

    feat(ai): Add support for stream extensions
    - Add trait to support streams that extend openai stream with additional
      items
    - Rename `client` -> `tool_loop`
    - Update tool loop to use new client trait and handle extended streams
    - Implement extension client for existing clients (placeholder for
      anthropic)

--- rust/cloud-storage/ai/src/tool/tool_loop/ai_client.rs
+++ rust/cloud-storage/ai/src/tool/tool_loop/ai_client.rs
@@ -2,21 +2,23 @@ use super::chained::Chained;
 use super::chat::Chat;
 use crate::tool::types::AsyncToolSet;
 use crate::types::AnthropicClient;
-use crate::types::Client;
+use crate::types::ExtendedClient;
+use anthropic::openai::request::AnthropicRequestExtensions;
 use std::sync::Arc;
 
-pub struct AiClient<I, T, R>
+pub struct ToolLoop<I, T, R>
 where
-    I: Client + Clone + Send + Sync,
+    I: ExtendedClient + Clone + Send + Sync,
     T: Clone + Send + Sync,
     R: Clone + Send + Sync,
 {
     inner: I,
     context: T,
     toolset: Arc<AsyncToolSet<T, R>>,
+    extensions: I::RequestExtension,
 }
 
-impl<T, R> AiClient<AnthropicClient, T, R>
+impl<T, R> ToolLoop<AnthropicClient, T, R>
 where
     T: Clone + Send + Sync,
     R: Clone + Send + Sync,
@@ -24,17 +26,20 @@ where
     pub fn new(toolset: AsyncToolSet<T, R>, context: T) -> Self {
         let client = AnthropicClient::new();
         let toolset = Arc::new(toolset);
+        let extensions = AnthropicRequestExtensions(vec![]);
         Self {
             inner: client,
             context,
             toolset,
+            extensions,
         }
     }
 }
 
-impl<I, T, R> AiClient<I, T, R>
+impl<I, T, R> ToolLoop<I, T, R>
 where
-    I: Client + Clone + Send + Sync,
+    I: ExtendedClient + Clone + Send + Sync,
+    I::RequestExtension: Clone,
     T: Clone + Send + Sync,
     R: Clone + Send + Sync,
 {
@@ -43,6 +48,7 @@ where
             self.inner.clone(),
             self.toolset.clone(),
             self.context.clone(),
+            self.extensions.clone(),
         )
     }
 
@@ -51,6 +57,7 @@ where
             self.inner.clone(),
             self.toolset.clone(),
             self.context.clone(),
+            self.extensions.clone(),
         )
     }
 }
--- rust/cloud-storage/ai/src/types/client/anthropic.rs
+++ rust/cloud-storage/ai/src/types/client/anthropic.rs
@@ -1,5 +1,13 @@
-use super::Client;
-use crate::types::AiError;
+use super::{ExtendedClient, ExtendedOpenAIStreamItem};
+use crate::{
+    tool::types::{StreamPart, ToolCall, ToolResponse},
+    types::AiError,
+};
+use anthropic::openai::{
+    request::AnthropicRequestExtensions,
+    stream_extension::{AnthropicResponseExtension, ExtendedAnthropicStreamItem},
+};
+use futures::StreamExt;
 
 #[derive(Clone, Debug)]
 pub struct AnthropicClient {
@@ -19,15 +27,57 @@ impl Default for AnthropicClient {
     }
 }
 
-impl Client for AnthropicClient {
+impl From<ExtendedAnthropicStreamItem> for ExtendedOpenAIStreamItem<AnthropicResponseExtension> {
+    fn from(value: ExtendedAnthropicStreamItem) -> Self {
+        match value {
+            ExtendedAnthropicStreamItem::Extension(ext) => ExtendedOpenAIStreamItem::Extension(ext),
+            ExtendedAnthropicStreamItem::OpenAI(oai) => ExtendedOpenAIStreamItem::Response(oai),
+        }
+    }
+}
+
+impl ExtendedClient for AnthropicClient {
+    type RequestExtension = AnthropicRequestExtensions;
+    type ResponseExtension = AnthropicResponseExtension;
+
     async fn chat_stream(
         &self,
         request: async_openai::types::CreateChatCompletionRequest,
-        extensions: Option<super::RequestExtensions>,
-    ) -> Result<async_openai::types::ChatCompletionResponseStream, AiError> {
-        if extensions.is_some() {
-            tracing::warn!("extensions are not yet supported");
+        extensions: &Self::RequestExtension,
+    ) -> anyhow::Result<super::traits::ExtendedOpenAIStream<Self::ResponseExtension>, AiError> {
+        Ok(Box::pin(
+            self.inner
+                .chat()
+                .create_stream_openai_extended(request, extensions)
+                .await
+                .map(|f| f.map(ExtendedOpenAIStreamItem::from)),
+        ))
+    }
+
+    // TODO: this is incomplete
+    // The request must be updated to record the call / response to correctly save / load chat
+    // This won't be hit until extensions are enabled in the next pr
+    fn handle_extension_item(
+        &self,
+        _: &mut async_openai::types::CreateChatCompletionRequest,
+        item: Self::ResponseExtension,
+    ) -> Option<crate::tool::types::StreamPart> {
+        match item {
+            AnthropicResponseExtension::Citation(_) => None,
+            AnthropicResponseExtension::ServerToolUse(tool_call) => {
+                Some(StreamPart::ToolCall(ToolCall {
+                    id: tool_call.id,
+                    json: serde_json::Value::Null,
+                    name: tool_call.name,
+                }))
+            }
+            AnthropicResponseExtension::WebSearchToolResponse(response) => {
+                Some(StreamPart::ToolResponse(ToolResponse::Json {
+                    id: "searchington".into(),
+                    json: serde_json::to_value(&response.content).unwrap(),
+                    name: "web-search".into(),
+                }))
+            }
         }
-        Ok(self.inner.chat().create_stream_openai_lossy(request).await)
     }
 }
