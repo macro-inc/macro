97277ae28868bf2cdef463d03a3e4b05abb9fa78
Blake Griffith <blake@macro.com>
2025-12-30 10:32:02 -0500

    Make some things infallible

--- rust/cloud-storage/macro_notify/src/message_attribute.rs
+++ rust/cloud-storage/macro_notify/src/message_attribute.rs
@@ -4,10 +4,22 @@
 #[tracing::instrument]
 pub(crate) fn build_string_message_attribute(
     attr: &str,
-) -> anyhow::Result<aws_sdk_sqs::types::MessageAttributeValue> {
-    let result = aws_sdk_sqs::types::MessageAttributeValue::builder()
+) -> aws_sdk_sqs::types::MessageAttributeValue {
+    aws_sdk_sqs::types::MessageAttributeValue::builder()
         .data_type("String")
         .string_value(attr)
-        .build()?;
-    Ok(result)
+        .build()
+        .expect("only fails when `data_type` is not set. it is set above")
+}
+
+/// Builds a number message attribute
+#[tracing::instrument]
+pub(crate) fn build_number_message_attribute(
+    attr: i32,
+) -> aws_sdk_sqs::types::MessageAttributeValue {
+    aws_sdk_sqs::types::MessageAttributeValue::builder()
+        .data_type("Number")
+        .string_value(attr.to_string())
+        .build()
+        .expect("only fails when `data_type` not set. it is set above")
 }
