0dd8e177c83010a11595e39d8a9022fd758610e9
Nick Noble <nick@macro.com>
2026-01-06 15:45:47 -0500

    hook up playwright+vitest properly to storybook

--- js/app/vitest.config.ts
+++ js/app/vitest.config.ts
@@ -1,6 +1,13 @@
+import path from 'node:path';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
 import solidPlugin from 'vite-plugin-solid';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { configDefaults, defineConfig } from 'vitest/config';
+
+const storybookConfigDir = path.resolve(
+  import.meta.dirname,
+  'packages/ui/.storybook'
+);
 
 export default defineConfig({
   plugins: [tsconfigPaths(), solidPlugin()],
@@ -29,6 +36,24 @@ export default defineConfig({
         test: {
           include: ['packages/queries/**/*.{test,spec}.{ts,tsx}'],
           name: 'queries',
+        },
+      },
+      {
+        // @ui package component tests
+        plugins: [
+          storybookTest({
+            configDir: storybookConfigDir,
+          }),
+        ],
+        test: {
+          name: 'storybook',
+          browser: {
+            enabled: true,
+            provider: 'playwright',
+            headless: true,
+            instances: [{ browser: 'chromium' }],
+          },
+          setupFiles: [path.join(storybookConfigDir, 'vitest.setup.ts')],
         },
       },
     ],
