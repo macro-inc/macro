90800202c623b1468ee0f26f733da1580d1270c2
Hutch <hutch@macro.com>
2026-01-09 15:41:09 -0500

    improved filter trait

--- rust/cloud-storage/search_service/src/api/search/simple/filter.rs
+++ rust/cloud-storage/search_service/src/api/search/simple/filter.rs
@@ -29,144 +29,140 @@ pub(super) enum UnifiedSearchArgsVariant {
     Email(UnifiedEmailSearchArgs),
 }
 
-/// Trait to convert item_filters into a UnifiedSearchArgsVariant
+/// Trait to convert item_filters into search args
 pub(super) trait FilterVariantToSearchArgs {
+    type Output;
+
     fn filter_to_search_args(
         &self,
         ctx: &ApiContext,
         user_id: &str,
         user_organization_id: Option<i32>,
         should_include: bool,
-    ) -> impl Future<Output = Result<UnifiedSearchArgsVariant, SearchError>> + Send;
+    ) -> impl Future<Output = Result<Self::Output, SearchError>> + Send;
 }
 
 impl FilterVariantToSearchArgs for item_filters::DocumentFilters {
+    type Output = UnifiedDocumentSearchArgs;
+
     async fn filter_to_search_args(
         &self,
         ctx: &ApiContext,
         user_id: &str,
         _user_organization_id: Option<i32>,
         should_include: bool,
-    ) -> Result<UnifiedSearchArgsVariant, SearchError> {
+    ) -> Result<Self::Output, SearchError> {
         if !should_include {
-            Ok(UnifiedSearchArgsVariant::Document(
-                UnifiedDocumentSearchArgs::default(),
-            ))
+            Ok(UnifiedDocumentSearchArgs::default())
         } else {
             let filter_document_response = filter_documents(ctx, user_id, self).await?;
 
-            Ok(UnifiedSearchArgsVariant::Document(
-                UnifiedDocumentSearchArgs {
-                    document_ids: filter_document_response.document_ids,
-                    ids_only: filter_document_response.ids_only,
-                },
-            ))
+            Ok(UnifiedDocumentSearchArgs {
+                document_ids: filter_document_response.document_ids,
+                ids_only: filter_document_response.ids_only,
+            })
         }
     }
 }
 
 impl FilterVariantToSearchArgs for item_filters::ChannelFilters {
+    type Output = UnifiedChannelMessageSearchArgs;
+
     async fn filter_to_search_args(
         &self,
         ctx: &ApiContext,
         user_id: &str,
         user_organization_id: Option<i32>,
         should_include: bool,
-    ) -> Result<UnifiedSearchArgsVariant, SearchError> {
+    ) -> Result<Self::Output, SearchError> {
         if !should_include {
-            Ok(UnifiedSearchArgsVariant::Channel(
-                UnifiedChannelMessageSearchArgs::default(),
-            ))
+            Ok(UnifiedChannelMessageSearchArgs::default())
         } else {
             let filter_channel_response =
                 filter_channels(ctx, user_id, user_organization_id, self).await?;
 
-            Ok(UnifiedSearchArgsVariant::Channel(
-                UnifiedChannelMessageSearchArgs {
-                    channel_ids: filter_channel_response
-                        .channel_ids
-                        .iter()
-                        .map(|c| c.to_string())
-                        .collect(),
-                    thread_ids: self.thread_ids.clone(),
-                    mentions: self.mentions.clone(),
-                    sender_ids: self.sender_ids.clone(),
-                },
-            ))
+            Ok(UnifiedChannelMessageSearchArgs {
+                channel_ids: filter_channel_response
+                    .channel_ids
+                    .iter()
+                    .map(|c| c.to_string())
+                    .collect(),
+                thread_ids: self.thread_ids.clone(),
+                mentions: self.mentions.clone(),
+                sender_ids: self.sender_ids.clone(),
+            })
         }
     }
 }
 
 impl FilterVariantToSearchArgs for item_filters::ChatFilters {
+    type Output = UnifiedChatSearchArgs;
+
     async fn filter_to_search_args(
         &self,
         ctx: &ApiContext,
         user_id: &str,
         _user_organization_id: Option<i32>,
         should_include: bool,
-    ) -> Result<UnifiedSearchArgsVariant, SearchError> {
+    ) -> Result<Self::Output, SearchError> {
         if !should_include {
-            Ok(UnifiedSearchArgsVariant::Chat(
-                UnifiedChatSearchArgs::default(),
-            ))
+            Ok(UnifiedChatSearchArgs::default())
         } else {
             let filter_chat_response = filter_chats(ctx, user_id, self).await?;
 
-            Ok(UnifiedSearchArgsVariant::Chat(UnifiedChatSearchArgs {
+            Ok(UnifiedChatSearchArgs {
                 chat_ids: filter_chat_response.chat_ids,
                 ids_only: filter_chat_response.ids_only,
                 role: self.role.clone(),
-            }))
+            })
         }
     }
 }
 
 impl FilterVariantToSearchArgs for item_filters::ProjectFilters {
+    type Output = UnifiedProjectSearchArgs;
+
     async fn filter_to_search_args(
         &self,
         ctx: &ApiContext,
         user_id: &str,
         _user_organization_id: Option<i32>,
         should_include: bool,
-    ) -> Result<UnifiedSearchArgsVariant, SearchError> {
+    ) -> Result<Self::Output, SearchError> {
         if !should_include {
-            Ok(UnifiedSearchArgsVariant::Project(
-                UnifiedProjectSearchArgs::default(),
-            ))
+            Ok(UnifiedProjectSearchArgs::default())
         } else {
             let filter_project_response = filter_projects(ctx, user_id, self).await?;
 
-            Ok(UnifiedSearchArgsVariant::Project(
-                UnifiedProjectSearchArgs {
-                    project_ids: filter_project_response.project_ids,
-                    ids_only: filter_project_response.ids_only,
-                },
-            ))
+            Ok(UnifiedProjectSearchArgs {
+                project_ids: filter_project_response.project_ids,
+                ids_only: filter_project_response.ids_only,
+            })
         }
     }
 }
 
 impl FilterVariantToSearchArgs for item_filters::EmailFilters {
+    type Output = UnifiedEmailSearchArgs;
+
     async fn filter_to_search_args(
         &self,
         _ctx: &ApiContext,
         _user_id: &str,
         _user_organization_id: Option<i32>,
         should_include: bool,
-    ) -> Result<UnifiedSearchArgsVariant, SearchError> {
+    ) -> Result<Self::Output, SearchError> {
         if !should_include {
-            Ok(UnifiedSearchArgsVariant::Email(
-                UnifiedEmailSearchArgs::default(),
-            ))
+            Ok(UnifiedEmailSearchArgs::default())
         } else {
-            Ok(UnifiedSearchArgsVariant::Email(UnifiedEmailSearchArgs {
+            Ok(UnifiedEmailSearchArgs {
                 thread_ids: vec![],
                 link_ids: vec![],
                 sender: self.senders.clone(),
                 cc: self.cc.clone(),
                 bcc: self.bcc.clone(),
                 recipients: self.recipients.clone(),
-            }))
+            })
         }
     }
 }
