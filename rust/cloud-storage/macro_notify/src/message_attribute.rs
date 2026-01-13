86ad69884bcf604b8f5fa2a511c2528a4d707df2
Blake Griffith <blake@macro.com>
2026-01-13 12:47:57 -0500

    rm build_number_message_attribute

--- rust/cloud-storage/macro_notify/src/message_attribute.rs
+++ rust/cloud-storage/macro_notify/src/message_attribute.rs
@@ -11,15 +11,3 @@ pub(crate) fn build_string_message_attribute(
         .build()
         .expect("only fails when `data_type` is not set. it is set above")
 }
-
-/// Builds a number message attribute
-#[tracing::instrument]
-pub(crate) fn build_number_message_attribute(
-    attr: i32,
-) -> aws_sdk_sqs::types::MessageAttributeValue {
-    aws_sdk_sqs::types::MessageAttributeValue::builder()
-        .data_type("Number")
-        .string_value(attr.to_string())
-        .build()
-        .expect("only fails when `data_type` not set. it is set above")
-}
