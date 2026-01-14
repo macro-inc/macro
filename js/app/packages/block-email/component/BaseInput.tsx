58ca859080b5ba2f680d47cd57042d4500769afa
Rahul <43100342+dev-rb@users.noreply.github.com>
2026-01-14 17:45:49 -0500

    Only send attachments mutation when there are attachments

--- js/app/packages/block-email/component/BaseInput.tsx
+++ js/app/packages/block-email/component/BaseInput.tsx
@@ -356,17 +356,19 @@ export function BaseInput(props: {
         { type: 'local' }
       >[];
 
-      const uploaded = await uploadAttachmentMutation.mutateAsync({
-        draftID: draftResponse,
-        attachments: attachments.map((a) => a.file),
-      });
+      if (attachments.length) {
+        const uploaded = await uploadAttachmentMutation.mutateAsync({
+          draftID: draftResponse,
+          attachments: attachments.map((a) => a.file),
+        });
 
-      // Assign the attachment ids to attachments for later use
-      for (const attachment of uploaded.attachments) {
-        form().attachments.assignAttachmentID(
-          attachment.file,
-          attachment.attachmentID
-        );
+        // Assign the attachment ids to attachments for later use
+        for (const attachment of uploaded.attachments) {
+          form().attachments.assignAttachmentID(
+            attachment.file,
+            attachment.attachmentID
+          );
+        }
       }
 
       setSavedDraftId(draftResponse);
--- js/app/packages/block-email/component/Compose.tsx
+++ js/app/packages/block-email/component/Compose.tsx
@@ -201,17 +201,19 @@ export function EmailCompose() {
         { type: 'local' }
       >[];
 
-      const uploaded = await uploadAttachmentMutation.mutateAsync({
-        draftID: draftResponse,
-        attachments: attachments.map((a) => a.file),
-      });
+      if (attachments.length) {
+        const uploaded = await uploadAttachmentMutation.mutateAsync({
+          draftID: draftResponse,
+          attachments: attachments.map((a) => a.file),
+        });
 
-      // Assign the attachment ids to attachments for later use
-      for (const attachment of uploaded.attachments) {
-        form.attachments.assignAttachmentID(
-          attachment.file,
-          attachment.attachmentID
-        );
+        // Assign the attachment ids to attachments for later use
+        for (const attachment of uploaded.attachments) {
+          form.attachments.assignAttachmentID(
+            attachment.file,
+            attachment.attachmentID
+          );
+        }
       }
 
       setCurrentDraftID(draftResponse);
