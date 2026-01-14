b0f6ef896d7b12ec1dcc7cb2a69cc98c1649f8e0
Rahul <43100342+dev-rb@users.noreply.github.com>
2026-01-14 16:33:16 -0500

    Change key for form context to be object

--- js/app/packages/block-email/component/EmailFormContext.tsx
+++ js/app/packages/block-email/component/EmailFormContext.tsx
@@ -6,8 +6,16 @@ import {
 
 type EmailFormContextValue = ReturnType<typeof createEmailFormState>;
 
+type FormAccessKey =
+  | { type: 'replying_to'; messageID: string }
+  | { type: 'draft'; messageID: string };
+
+const stringifyKey = (key: FormAccessKey) => {
+  return `${key.type}_${key.messageID}`;
+};
+
 type RegistryApi = {
-  getOrInit: (key?: string) => EmailFormContextValue;
+  getOrInit: (key?: FormAccessKey) => EmailFormContextValue;
 };
 
 const EmailFormRegistryCtx = createContext<RegistryApi>();
@@ -21,10 +29,11 @@ export function EmailFormContextProvider(
     if (!key) {
       return createEmailFormState();
     }
-    let existing = map.get(key);
+    const stringifiedKey = stringifyKey(key);
+    let existing = map.get(stringifiedKey);
     if (!existing) {
       existing = createEmailFormState(key, props.formOptions);
-      map.set(key, existing);
+      map.set(stringifiedKey, existing);
     }
     return existing;
   };
@@ -46,7 +55,9 @@ export function getEmailFormRegistry(): RegistryApi {
   return ctx;
 }
 
-export function getOrInitEmailFormContext(key?: string): EmailFormContextValue {
+export function getOrInitEmailFormContext(
+  key?: FormAccessKey
+): EmailFormContextValue {
   const ctx = useContext(EmailFormRegistryCtx);
   if (!ctx)
     throw new Error(
