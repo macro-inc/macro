ad5193ce92568225ea1ffe571c89fcfeaf6e76fd
Evan Hutnik <evan.hutnik@gmail.com>
2026-01-15 16:36:03 -0500

    Fmt

--- infra/stacks/email-sfs-delete-handler/sfs_delete_lambda.ts
+++ infra/stacks/email-sfs-delete-handler/sfs_delete_lambda.ts
@@ -111,7 +111,8 @@ export class EmailSfsDeleteHandler extends pulumi.ComponentResource {
       {
         name: `${EMAIL_LAMBDA_BASE_NAME}-rule-${stack}`,
         // run at 7am UTC every day, when people be sleepin
-        scheduleExpression: stack === 'prod' ? 'cron(0 8 * * ? *)' : 'rate(5 minutes)',
+        scheduleExpression:
+          stack === 'prod' ? 'cron(0 8 * * ? *)' : 'rate(5 minutes)',
         tags: this.tags,
       },
       { parent: this }
