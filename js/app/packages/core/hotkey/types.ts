fdabba3932958bf9e821933c35a7a699f3136bd6
Caleb Taylor <aquaductape@gmail.com>
2026-01-14 19:54:46 -0500

    fix ts import issue

--- js/app/packages/core/hotkey/types.ts
+++ js/app/packages/core/hotkey/types.ts
@@ -1,11 +1,11 @@
 import type { Component, JSX } from 'solid-js';
 import type { HotkeyToken } from './tokens';
 
-export declare const HOTKEY_PRIORITY_DEFAULT = 0;
-export declare const HOTKEY_PRIORITY_LOW = 1;
-export declare const HOTKEY_PRIORITY_NORMAL = 2;
-export declare const HOTKEY_PRIORITY_HIGH = 3;
-export declare const HOTKEY_PRIORITY_CRITICAL = 4;
+export const HOTKEY_PRIORITY_DEFAULT = 0;
+export const HOTKEY_PRIORITY_LOW = 1;
+export const HOTKEY_PRIORITY_NORMAL = 2;
+export const HOTKEY_PRIORITY_HIGH = 3;
+export const HOTKEY_PRIORITY_CRITICAL = 4;
 
 const HOTKEY_TYPE = [
   HOTKEY_PRIORITY_DEFAULT,
