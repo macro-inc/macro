a9dde1dc44461f19bf65149024670bfd7ee5ca91
Rahul <43100342+dev-rb@users.noreply.github.com>
2026-01-20 11:04:29 -0500

    Move scroll position maintain to context

--- js/app/packages/block-email/component/MessageList.tsx
+++ js/app/packages/block-email/component/MessageList.tsx
@@ -96,37 +96,6 @@ export function MessageList(props: MessageListProps) {
               return manuallyExpanded || isLastMessage() || isNewMessage();
             });
 
-            const onToggleMessage = (expanded: boolean) => {
-              const messageID = message().db_id;
-              if (!messageID) return;
-              const listContainer = context.messagesListRef();
-
-              const lastScrollPosition = listContainer?.scrollTop;
-              const lastScrollHeight = listContainer?.scrollHeight;
-
-              context.messages.setExpandedBodyId(messageID, expanded);
-
-              if (
-                !listContainer ||
-                lastScrollPosition == null ||
-                lastScrollHeight == null
-              )
-                return;
-
-              // Maintain the scroll position when expansion changes
-              queueMicrotask(() => {
-                const lastPos = lastScrollHeight + lastScrollPosition;
-                const currentPos =
-                  listContainer.scrollHeight + listContainer.scrollTop;
-
-                // List is reversed, we need a negative value to maintain scroll
-                // position
-                const diff = lastPos - currentPos;
-
-                context.messagesListRef()?.scrollBy({ top: diff });
-              });
-            };
-
             return (
               <MessageContainer
                 isFirstMessage={normalizedIndex() === 0}
@@ -138,7 +107,6 @@ export function MessageList(props: MessageListProps) {
                 isTarget={isTargetSelector(message().db_id ?? undefined)}
                 message={message()}
                 isExpanded={isExpanded()}
-                onToggleBodyExpand={onToggleMessage}
               />
             );
           }}
