2058fd84a77d636c20d1a80e8fef306c364b984d
Hutch <hutch@macro.com>
2026-01-28 16:17:21 -0500

    add maybe_env_var macro

--- rust/cloud-storage/macro_aws_config/src/lib.rs
+++ rust/cloud-storage/macro_aws_config/src/lib.rs
@@ -2,21 +2,23 @@
 
 //! This crate creates a standard way to make AWS configs.
 
-use macro_env_var::env_var;
+use macro_env_var::maybe_env_var;
 
-maybe_env_var!{
-    LocalHost
+maybe_env_var! {
+    #[derive(Clone)]
+    pub struct LocalAwsUrl;
 }
+
 /// Creates a aws_config to use.
 /// If you provide `LOCAL_AWS_URL` environment variable we create a local aws
 /// config with test credentials.
 /// Otherwise we load normally.
 pub async fn get_macro_aws_config() -> aws_config::SdkConfig {
-    let is_local = 
-    let aws_config = if is_local {
+    if let Some(local_aws_url) = LocalAwsUrl::new() {
         aws_config::defaults(aws_config::BehaviorVersion::latest())
             .region("us-east-1")
             .test_credentials()
+            .endpoint_url(local_aws_url.as_ref())
             .load()
             .await
     } else {
@@ -24,7 +26,5 @@ pub async fn get_macro_aws_config() -> aws_config::SdkConfig {
             .region("us-east-1")
             .load()
             .await
-    };
-
-    aws_config
+    }
 }
