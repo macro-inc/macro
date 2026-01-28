4a0db68bd411846c49722eb19d0e19544c49bb3f
synoet <teonys@nyu.edu>
2026-01-27 19:23:13 -0500

    useContacts hook

--- js/app/packages/queries/contacts/contacts.ts
+++ js/app/packages/queries/contacts/contacts.ts
@@ -1,8 +1,11 @@
+import type { IUser } from '@core/user/types';
+import { idToDisplayName, idToEmail } from '@core/user/util';
 import { throwOnErr } from '@core/util/maybeResult';
 import { contactsKeys } from './keys';
 import { contactsClient } from '@service-contacts/client';
 import { useQuery } from '@tanstack/solid-query';
 import { queryClient } from '@queries/client';
+import type { Accessor } from 'solid-js';
 
 function contactsQueryOptions() {
   return {
@@ -13,6 +16,22 @@ function contactsQueryOptions() {
 
 export function useContactsQuery() {
   return useQuery(() => contactsQueryOptions());
+}
+
+/**
+ * Returns contacts as IUser objects.
+ * Compatible with the previous createResource-based implementation.
+ */
+export function useContacts(): Accessor<IUser[]> {
+  const query = useContactsQuery();
+  return () => {
+    const contacts = query.data?.contacts ?? [];
+    return contacts.map((c) => ({
+      id: c,
+      email: idToEmail(c),
+      name: idToDisplayName(c),
+    }));
+  };
 }
 
 export function invalidateContacts() {
