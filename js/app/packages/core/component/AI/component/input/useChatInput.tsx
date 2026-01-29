1913be3488ea2c9df44549ab89f91e78120800be
ehayes <eqhayes@protonmail.com>
2026-01-29 13:12:02 -0500

    ...

--- js/app/packages/core/component/AI/component/input/useChatInput.tsx
+++ js/app/packages/core/component/AI/component/input/useChatInput.tsx
@@ -342,7 +342,7 @@ function ChatInput(props: ChatInputInternalProps) {
       <div class="relative px-2 py-1.5">
         <div
           id="chat-input-text-area"
-          class="text-base sm:text-sm text-ink transition-all duration-150 ease-out bg-accent"
+          class="text-base sm:text-sm text-ink transition-all duration-150 ease-out"
           classList={{
             'pl-8 pr-[130px]': !isMultiline(),
             'pl-0 pr-0 pb-8': isMultiline(),
