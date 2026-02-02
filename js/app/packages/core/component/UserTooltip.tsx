e9bc4e8e870505a85a2ead7d933c64c4afd67c70
Rahul <43100342+dev-rb@users.noreply.github.com>
2026-02-02 17:20:03 -0500

    Update user tooltip

--- js/app/packages/core/component/UserTooltip.tsx
+++ js/app/packages/core/component/UserTooltip.tsx
@@ -24,7 +24,7 @@ export type UserTooltipProps = {
 
 export function UserTooltip(props: UserTooltipProps) {
   const currentUserId = useUserId();
-  const { replaceOrInsertSplit } = useSplitLayout();
+  const { openWithSplit } = useSplitLayout();
 
   const openDM = async (e: PointerEvent | MouseEvent) => {
     e.preventDefault();
@@ -36,9 +36,12 @@ export function UserTooltip(props: UserTooltipProps) {
         });
         const channelId = isOk(result) && result[1]?.channel_id;
         if (channelId) {
-          replaceOrInsertSplit({
-            type: 'channel',
-            id: channelId,
+          openWithSplit({
+            content: {
+              type: 'channel',
+              id: channelId,
+            },
+            force: e.shiftKey ? 'insert' : undefined,
           });
         } else {
           toast.failure('Failed to open direct message');
