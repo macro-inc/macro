71e425c001623c215be8ce22804eff5515546914
Hutch <hutch@macro.com>
2026-02-10 14:16:16 -0500

    cleanup

--- rust/cloud-storage/authentication_service/src/lib.rs
+++ rust/cloud-storage/authentication_service/src/lib.rs
@@ -1,9 +1,1 @@
-pub mod fetch_and_checksum;
-mod generate_password;
 pub mod service;
-
-/// expose auth client to be used in bin
-pub use fusionauth::FusionAuthClient;
-pub use fusionauth::user::create::User;
-
-pub use generate_password::generate_random_password;
