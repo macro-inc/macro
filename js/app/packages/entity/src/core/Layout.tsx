f11c04d05a98878f065e718b089b4a2b870bb2eb
Gabriel Birman <25272206+gbirman@users.noreply.github.com>
2026-02-12 12:38:28 -0500

    remove invalid styles

--- js/app/packages/entity/src/core/Layout.tsx
+++ js/app/packages/entity/src/core/Layout.tsx
@@ -1,12 +1,11 @@
 import { cn } from '@ui/utils/classname';
 import { type JSX, splitProps } from 'solid-js';
-import '../index.css';
 
 export function Layout(props: JSX.HTMLAttributes<HTMLDivElement>) {
   const [local, rest] = splitProps(props, ['class', 'children']);
 
   return (
-    <div class={cn('entity-layout', local.class)} {...rest}>
+    <div class={cn(local.class)} {...rest}>
       {local.children}
     </div>
   );
