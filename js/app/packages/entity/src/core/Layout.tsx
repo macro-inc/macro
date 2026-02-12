32dbee5dedf96fb878a6979966b6ab50eff89831
Gabriel Birman <25272206+gbirman@users.noreply.github.com>
2026-02-12 12:38:08 -0500

    move entity layout styles into packages/entity

--- js/app/packages/entity/src/core/Layout.tsx
+++ js/app/packages/entity/src/core/Layout.tsx
@@ -1,5 +1,6 @@
 import { cn } from '@ui/utils/classname';
 import { type JSX, splitProps } from 'solid-js';
+import '../index.css';
 
 export function Layout(props: JSX.HTMLAttributes<HTMLDivElement>) {
   const [local, rest] = splitProps(props, ['class', 'children']);
