b1d6b6895d66405b7db0586a8dbb442820b5bae3
Hutch <hutch@macro.com>
2026-02-13 09:24:43 -0500

    fix: location v3

--- rust/cloud-storage/sync_service_client/src/metadata.rs
+++ rust/cloud-storage/sync_service_client/src/metadata.rs
@@ -4,6 +4,7 @@ use super::SyncServiceClient;
 use anyhow::Result;
 
 impl SyncServiceClient {
+    #[tracing::instrument(skip(self), err)]
     pub async fn get_metadata(&self, document_id: &str) -> Result<DocumentMetadata> {
         let full_url = format!("{}/document/{}/metadata", self.url, document_id);
         let res = self.client.get(&full_url).send().await?;
