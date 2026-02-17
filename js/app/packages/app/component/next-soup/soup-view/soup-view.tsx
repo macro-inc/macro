da45786b2725683077b367f4bfa6ba23ce1e1976
Gabriel Birman <25272206+gbirman@users.noreply.github.com>
2026-02-17 16:11:00 -0500

    do not hide featured content hit

--- js/app/packages/app/component/next-soup/soup-view/soup-view.tsx
+++ js/app/packages/app/component/next-soup/soup-view/soup-view.tsx
@@ -613,9 +613,6 @@ export const SoupViewList = (props: SoupViewListProps) => {
                               highlighted={
                                 panel.isPanelActive() && row.isFocused()
                               }
-                              hideContentHits={
-                                featuredCount() > 0 && i() < featuredCount()
-                              }
                               onMouseMove={() => {
                                 if (isKeypressActive()) return;
                                 if (soup.previewEntity()) return;
