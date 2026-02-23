ce6e161dc0a799db9b9e5460d739d1eb0af5c69f
Evan Hutnik <evan.hutnik@gmail.com>
2026-02-23 11:11:28 -0500

    Always open attachments in split

--- js/app/packages/block-email/component/MessageContainer.tsx
+++ js/app/packages/block-email/component/MessageContainer.tsx
@@ -185,7 +185,7 @@ export function MessageContainer(props: MessageContainerProps) {
     const blockName = fileType ? fileTypeToBlockName(fileType) : 'unknown';
     openWithSplit(
       { type: blockName, id: document_id },
-      { preferNewSplit: event.shiftKey }
+      { preferNewSplit: true }
     );
   };
 
