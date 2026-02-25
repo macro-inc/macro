11de3bcd656b33ddb7bec29496bf3ebfc2e23cf1
Rahul <43100342+dev-rb@users.noreply.github.com>
2026-02-25 16:39:20 -0500

    Styling

--- js/app/packages/app/component/app-sidebar/sidebar.tsx
+++ js/app/packages/app/component/app-sidebar/sidebar.tsx
@@ -59,7 +59,7 @@ export const SIDEBAR_LINKS = [
 
 export const AppSidebar = () => {
   return (
-    <div class="w-56 h-full border-r-edge-muted border-r-1 bg-panel py-2 flex flex-col gap-6">
+    <div class="w-56 h-full border-r-edge-muted border-r-1 bg-panel py-2 flex flex-col gap-4">
       <div class="py-2 px-4">
         <LogoIcon class="size-8" />
       </div>
@@ -82,11 +82,12 @@ const SidebarLink = (props: SidebarLinkProps) => {
   const [isHovering, setIsHovering] = createSignal(false);
   return (
     <A
-      class="w-full px-2 py-1.5 rounded-md hover:bg-ink/10 active:bg-ink/20 flex items-center gap-2 text-sm text-ink-muted hover:text-ink"
+      class="w-full px-2 py-1.5 rounded-md flex items-center gap-2 text-sm hover:text-ink transition-colors"
       href={`/component${props.href}`}
       onMouseEnter={() => setIsHovering(true)}
       onMouseLeave={() => setIsHovering(false)}
-      activeClass="bg-edge hover:!bg-edge"
+      activeClass="bg-ink/10 text-white"
+      inactiveClass="text-ink/70 hover:bg-ink/10 active:bg-ink/15"
     >
       <Show when={props.icon}>
         <div class="size-4">
