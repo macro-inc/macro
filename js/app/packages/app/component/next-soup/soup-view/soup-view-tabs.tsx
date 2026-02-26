14b97d0b5581154da185e0ce03ca83a9c2953fa5
Rahul <43100342+dev-rb@users.noreply.github.com>
2026-02-26 17:31:36 -0500

    Tabs styling

--- js/app/packages/app/component/next-soup/soup-view/soup-view-tabs.tsx
+++ js/app/packages/app/component/next-soup/soup-view/soup-view-tabs.tsx
@@ -230,7 +230,7 @@ export const SegmentedControl: ParentComponent<
 
   return (
     <KSegmentedControl
-      class="size-full text-sm bg-ink/10 p-1 rounded-md"
+      class="size-full text-sm bg-ink/10 rounded-md p-0.5"
       defaultValue={props.list[0]?.value}
       onChange={onChange}
       disabled={props.disabled}
@@ -247,11 +247,10 @@ export const SegmentedControl: ParentComponent<
               return (
                 <KSegmentedControl.Item
                   value={itemValue()}
-                  class="relative text-ink-placeholder px-2 py-0.5 text-sm font-medium data-[checked]:text-ink transition-colors not-first-of-type:before:absolute not-first-of-type:before:inset-0 not-first-of-type:before:bg-ink/10 not-first-of-type:before:h-full not-first-of-type:before:w-0.5 data-[checked]:before:opacity-0 data-[checked]:[&+::before]:opacity-0 not-first-of-type:before:opacity-100 not-first-of-type:before:transition-opacity"
                   disabled={props.disabled}
                 >
-                  <KSegmentedControl.ItemInput class="absolute inset-0 size-full pointer-events-none" />
-                  <KSegmentedControl.ItemLabel class="flex">
+                  <KSegmentedControl.ItemInput class="absolute inset-0 pointer-events-none" />
+                  <KSegmentedControl.ItemLabel class="relative text-ink-placeholder size-full px-4 py-0.5 text-sm font-medium data-[checked]:text-ink transition-colors block">
                     {itemLabel()}
                   </KSegmentedControl.ItemLabel>
                 </KSegmentedControl.Item>
