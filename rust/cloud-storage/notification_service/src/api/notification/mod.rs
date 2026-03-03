b7560564a7ed8c2f251b9e6ac63d912ed6b6a94b
Sean Aye <hello@seanaye.ca>
2026-03-03 12:20:21 -0500

    remove unused handlers

--- rust/cloud-storage/notification_service/src/api/notification/mod.rs
+++
@@ -1,9 +0,0 @@
-use axum::{Router, routing::delete};
-
-use crate::api::context::ApiContext;
-
-pub(in crate::api) mod delete_user_notifications;
-
-pub fn router() -> Router<ApiContext> {
-    Router::new().route("/user/:user_id", delete(delete_user_notifications::handler))
-}
