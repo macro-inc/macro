44d679805041d4143c297a204b18bf74009c3b2f
Rahul <43100342+dev-rb@users.noreply.github.com>
2026-03-03 14:51:24 -0500

    Responsive styling

--- js/app/packages/app/component/next-soup/soup-view/filters-bar/soup-filters-bar.tsx
+++ js/app/packages/app/component/next-soup/soup-view/filters-bar/soup-filters-bar.tsx
@@ -4,16 +4,16 @@ import { SoupSearchbar } from '@app/component/next-soup/soup-view/filters-bar/so
 
 export const SoupFiltersBar = () => {
   return (
-    <div class="flex gap-2 items-center h-12">
-      <div class="flex gap-2 items-center size-full">
+    <div class="@container w-full overflow-hidden flex gap-2 @min-small/split:items-center flex-wrap @min-small/split:flex-nowrap py-2">
+      <div class="flex gap-2 items-center @min-small/split:size-full">
         <SoupViewContextFilters />
       </div>
-      <div class="ml-auto w-full flex items-center justify-end gap-2">
-        <div class="max-w-56 w-full">
-          <SoupSearchbar />
-        </div>
-        <SoupViewContextSort />
+
+      <div class="@min-small/split:ml-auto max-w-60 w-full">
+        <SoupSearchbar />
       </div>
+
+      <SoupViewContextSort />
     </div>
   );
 };
