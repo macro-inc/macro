0c058380d27ee2501574466ce25e867b5e79c901
Sean Aye <hello@seanaye.ca>
2026-03-04 11:22:34 -0500

    add macro tower layers crate

---
+++ rust/cloud-storage/macro_tower_layers/src/lib.rs
@@ -0,0 +1,173 @@
+#![deny(missing_docs)]
+//! This crate provides small reusable tower-http utilities which are useful across macro's http services
+use std::{
+    cmp,
+    sync::{
+        Arc,
+        atomic::{self, AtomicU64},
+    },
+    time::Duration,
+};
+
+use http::{HeaderValue, Request, Response};
+use tower::{
+    ServiceBuilder,
+    layer::util::{Identity, Stack},
+};
+use tower_http::{
+    ServiceBuilderExt,
+    classify::{ServerErrorsAsFailures, SharedClassifier},
+    request_id::{MakeRequestId, PropagateRequestIdLayer, RequestId, SetRequestIdLayer},
+    trace::{DefaultMakeSpan, DefaultOnRequest, OnResponse, TraceLayer},
+};
+use tracing::{Level, Span};
+
+/// A very simple builder for x-request-ids
+#[derive(Default, Clone)]
+pub struct RequestIdBuilder(Arc<AtomicU64>);
+
+impl MakeRequestId for RequestIdBuilder {
+    fn make_request_id<B>(
+        &mut self,
+        _request: &Request<B>,
+    ) -> Option<tower_http::request_id::RequestId> {
+        Some(RequestId::new(HeaderValue::from(
+            self.0.fetch_add(1, atomic::Ordering::SeqCst),
+        )))
+    }
+}
+
+/// fork of the [DefaultOnResponse] which is deisgined to work with [RequestIdBuilder]
+/// This emits a tracing warn event if a request takes more than a certain threshold to complete.
+#[derive(Clone)]
+pub struct CustomOnResponse {
+    warning_threshold: Duration,
+}
+
+impl CustomOnResponse {
+    /// create a new instance of self given a warning threshold duration
+    pub fn new_with_threshold(warning_threshold: Duration) -> Self {
+        CustomOnResponse { warning_threshold }
+    }
+}
+
+/// dynamic tracing event macro
+/// copied from [`tower_http`] source (its not public)
+macro_rules! event_dynamic_lvl {
+    ( $(target: $target:expr,)? $(parent: $parent:expr,)? $lvl:expr, $($tt:tt)* ) => {
+        match $lvl {
+            tracing::Level::ERROR => {
+                tracing::event!(
+                    $(target: $target,)?
+                    $(parent: $parent,)?
+                    tracing::Level::ERROR,
+                    $($tt)*
+                );
+            }
+            tracing::Level::WARN => {
+                tracing::event!(
+                    $(target: $target,)?
+                    $(parent: $parent,)?
+                    tracing::Level::WARN,
+                    $($tt)*
+                );
+            }
+            tracing::Level::INFO => {
+                tracing::event!(
+                    $(target: $target,)?
+                    $(parent: $parent,)?
+                    tracing::Level::INFO,
+                    $($tt)*
+                );
+            }
+            tracing::Level::DEBUG => {
+                tracing::event!(
+                    $(target: $target,)?
+                    $(parent: $parent,)?
+                    tracing::Level::DEBUG,
+                    $($tt)*
+                );
+            }
+            tracing::Level::TRACE => {
+                tracing::event!(
+                    $(target: $target,)?
+                    $(parent: $parent,)?
+                    tracing::Level::TRACE,
+                    $($tt)*
+                );
+            }
+        }
+    };
+}
+
+impl<B> OnResponse<B> for CustomOnResponse {
+    fn on_response(self, response: &Response<B>, latency: Duration, _span: &Span) {
+        struct Latency {
+            duration: Duration,
+        }
+
+        impl std::fmt::Display for Latency {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                write!(f, "{} ms", self.duration.as_millis())
+            }
+        }
+
+        let level = match latency.cmp(&self.warning_threshold) {
+            cmp::Ordering::Less => Level::INFO,
+            cmp::Ordering::Equal | cmp::Ordering::Greater => Level::WARN,
+        };
+
+        let latency = Latency { duration: latency };
+
+        let response_headers = tracing::field::debug(response.headers());
+        event_dynamic_lvl!(
+            level,
+            %latency,
+            status = response.status().as_u16(),
+            response_headers,
+            "finished processing request"
+        );
+    }
+}
+
+type ServiceBuilderAlias = ServiceBuilder<
+    Stack<
+        PropagateRequestIdLayer,
+        Stack<
+            TraceLayer<
+                SharedClassifier<ServerErrorsAsFailures>,
+                DefaultMakeSpan,
+                DefaultOnRequest,
+                CustomOnResponse,
+            >,
+            Stack<SetRequestIdLayer<RequestIdBuilder>, Identity>,
+        >,
+    >,
+>;
+
+/// A wrapper over a [ServiceBuilder] which handles both request id and tracing.
+/// See [CustomOnResponse] and [RequestIdBuilder] for more info.
+pub struct MacroRequestIdAndTracingLayer {
+    inner: ServiceBuilderAlias,
+}
+
+impl MacroRequestIdAndTracingLayer {
+    /// contruct a new instance of self with the input warning threshold
+    pub fn new(warning_threshold: Duration) -> Self {
+        let svc_builder = ServiceBuilder::new()
+            .set_x_request_id(RequestIdBuilder::default())
+            .layer(
+                TraceLayer::new_for_http()
+                    .make_span_with(DefaultMakeSpan::new().include_headers(true))
+                    .on_response(CustomOnResponse::new_with_threshold(warning_threshold)),
+            )
+            .propagate_x_request_id();
+
+        MacroRequestIdAndTracingLayer { inner: svc_builder }
+    }
+
+    /// return the inner [ServiceBuilder] so that you can call the .layer method
+    pub fn into_inner(self) -> ServiceBuilderAlias {
+        self.inner
+    }
+}
