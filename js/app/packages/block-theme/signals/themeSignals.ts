b1740359618d95d095a6b57e6b24a07a0a28ed1b
seamus <seamus@macro.com>
2026-03-09 18:20:48 -0400

    rounded-menus

--- js/app/packages/block-theme/signals/themeSignals.ts
+++ js/app/packages/block-theme/signals/themeSignals.ts
@@ -74,12 +74,11 @@ export const [monochromeIcons, setMonochromeIcons] = makePersisted(
   {name: 'enable-monochrome-icons'}
 );
 
-// export const [beveledCorners, setBeveledCorners] = makePersisted(
-//   createSignal<boolean>(BEVELED_CORNERS),
-//   {name: 'macro-beveled-corners'}
-// );
 
-export const [beveledCorners, setBeveledCorners] =  createSignal<boolean>(BEVELED_CORNERS);
+export const [beveledCorners, setBeveledCorners] = makePersisted(
+  createSignal<boolean>(BEVELED_CORNERS),
+  {name: 'macro-beveled-corners'}
+);
 
 export const [blackBezels, setBlackBezels] = makePersisted(
   createSignal<boolean>(BLACK_BEZELS),
