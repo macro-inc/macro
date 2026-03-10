3d072136d7df506c4a3442079874282017f57920
Rahul <43100342+dev-rb@users.noreply.github.com>
2026-03-10 15:36:23 -0400

    Remove providers usage

--- js/app/packages/app/component/analytics-context.tsx
+++ js/app/packages/app/component/analytics-context.tsx
@@ -1,15 +1,10 @@
-import {
-  createAnalytics,
-  googleAnalyticsProvider,
-  metaPixelProvider,
-} from '@app/lib/analytics';
+import { createAnalytics } from '@app/lib/analytics';
 import type { AnalyticsInterface } from '@app/lib/analytics/analytics';
 import { createAssertedContextProvider } from '@core/context/createContext';
 
 export const [AnalyticsContextProvider, useAnalytics] =
   createAssertedContextProvider<AnalyticsInterface>('analytics', () => {
     const analytics = createAnalytics({
-      providers: [googleAnalyticsProvider, metaPixelProvider],
       disabled: import.meta.hot != null,
     });
 
