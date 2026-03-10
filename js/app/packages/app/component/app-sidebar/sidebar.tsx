beaa3ed6beee537354e241e8f91867c9829a339d
Rahul <43100342+dev-rb@users.noreply.github.com>
2026-03-10 13:40:28 -0400

    Replace plus icon

--- js/app/packages/app/component/app-sidebar/sidebar.tsx
+++ js/app/packages/app/component/app-sidebar/sidebar.tsx
@@ -12,7 +12,7 @@ import { AnimatedFileMdIcon } from '@macro-icons/wide/animating/fileMd';
 import { AnimatedFolderIcon } from '@macro-icons/wide/animating/folder';
 import { useLocation } from '@solidjs/router';
 import LogoIcon from '@macro-icons/macro-logo.svg';
-import PlusIcon from '@macro-icons/wide/plus.svg';
+import PlusIcon from '@phosphor-icons/core/bold/plus-bold.svg?component-solid';
 import SearchIcon from '@phosphor-icons/core/bold/magnifying-glass-bold.svg?component-solid';
 import CommandIcon from '@phosphor-icons/core/assets/regular/command.svg';
 import { LIST_VIEW_PATHS, type ListView } from '@app/constants/list-views';
