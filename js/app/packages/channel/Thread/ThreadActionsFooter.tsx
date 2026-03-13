66d362ab5160268ed25a43331d4a985b0329cd0b
Peter Chinman <peter.chinman@gmail.com>
2026-03-13 19:04:46 -0400

    biome

--- js/app/packages/channel/Thread/ThreadActionsFooter.tsx
+++ js/app/packages/channel/Thread/ThreadActionsFooter.tsx
@@ -6,9 +6,7 @@ type ThreadActionsFooterProps = {
 
 export function ThreadActionsFooter(props: ThreadActionsFooterProps) {
   return (
-    <div
-      class="relative z-10 w-fit ml-(--message-padding-x)"
-    >
+    <div class="relative z-10 w-fit ml-(--message-padding-x)">
       {props.children}
     </div>
   );
