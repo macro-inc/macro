a8cbf5b5395ac68664ef7d99d4d8296de5a26f9f
Hutch <hutch@macro.com>
2026-03-18 13:45:58 -0400

    preserve query params for login redirect

--- js/app/packages/core/component/TopBar/LoginButton.tsx
+++ js/app/packages/core/component/TopBar/LoginButton.tsx
@@ -6,18 +6,24 @@ export function openLoginModal() {
   const isAuthenticated = useIsAuthenticated();
   if (isAuthenticated()) return;
 
-  window.location.href = `${window.location.origin}/app/login`;
+  const params = new URLSearchParams(window.location.search);
+  const queryString = params.size > 0 ? `?${params.toString()}` : '';
+  window.location.href = `${window.location.origin}/app/login${queryString}`;
 }
 
 export function LoginButton() {
   const navigate = useNavigate();
   return (
     <div class="flex gap-2 justify-center w-max items-baseline font-mono">
-      <DeprecatedButton onClick={() => navigate('/login')}>
+      <DeprecatedButton
+        onClick={() => navigate(`/login${window.location.search}`)}
+      >
         Login
       </DeprecatedButton>
       <span class="italic">or</span>
-      <DeprecatedButton onClick={() => navigate('/signup')}>
+      <DeprecatedButton
+        onClick={() => navigate(`/signup${window.location.search}`)}
+      >
         Sign Up
       </DeprecatedButton>
     </div>
