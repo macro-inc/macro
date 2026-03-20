0178feffaa6f139f79db56f3533ce87528a84165
Rahul <43100342+dev-rb@users.noreply.github.com>
2026-03-20 11:30:56 -0400

    Add analytics reset when logout

--- js/app/packages/core/auth/logout.ts
+++ js/app/packages/core/auth/logout.ts
@@ -3,10 +3,13 @@ import { authServiceClient } from '@service-auth/client';
 import { authKeys } from '@queries/auth/user-info';
 import { queryClient } from '@queries/client';
 import { createCallback } from '@solid-primitives/rootless';
+import { useAnalytics } from '@app/component/analytics-context';
 
 const { track, TrackingEvents } = withAnalytics();
 
 export function useLogout() {
+  const analytics = useAnalytics();
+
   return createCallback(async (redirectUrl?: string) => {
     document.cookie =
       'login=false; expires=Thu, 01 Jan 1970 00:00:00 UTC; max-age=0; path=/; SameSite=Lax';
@@ -26,6 +29,7 @@ export function useLogout() {
     });
 
     await authServiceClient.logout();
+    analytics.reset();
 
     track(TrackingEvents.AUTH.LOGOUT);
     if (redirectUrl) window.location.href = redirectUrl;
