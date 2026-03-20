bddf833ac7fbfa3f63aeab0bad38b113723c0029
Rahul <43100342+dev-rb@users.noreply.github.com>
2026-03-20 15:21:58 -0400

    Use switch/match

--- js/app/packages/block-channel/component/ChannelBlockSwitch.tsx
+++ js/app/packages/block-channel/component/ChannelBlockSwitch.tsx
@@ -1,15 +1,19 @@
-import { ShowFeatureFlag } from '@app/lib/analytics/posthog';
+import { ShowFeatureFlag, useFeatureFlag } from '@app/lib/analytics/posthog';
 import type { BlockChannelProps } from './Block';
 import BlockChannel from './Block';
 import { NewChannelBlockAdapter } from './NewChannelBlockAdapter';
+import { Match, Switch } from 'solid-js';
 
 export function ChannelBlockSwitch(props: BlockChannelProps) {
+  const ff = useFeatureFlag('enable-new-channels');
   return (
-    <ShowFeatureFlag
-      key="enable-new-channels"
-      fallback={<BlockChannel {...props} />}
-    >
-      <NewChannelBlockAdapter />
-    </ShowFeatureFlag>
+    <Switch>
+      <Match when={ff().enabled}>
+        <NewChannelBlockAdapter />
+      </Match>
+      <Match when={true}>
+        <BlockChannel {...props} />
+      </Match>
+    </Switch>
   );
 }
