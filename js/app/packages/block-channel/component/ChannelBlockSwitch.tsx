11c62c843964a2a59c01a263d541ffe1fe6782e9
synoet <teonys@nyu.edu>
2026-03-23 18:40:47 -0400

    chg: temporarily disable override

--- js/app/packages/block-channel/component/ChannelBlockSwitch.tsx
+++ js/app/packages/block-channel/component/ChannelBlockSwitch.tsx
@@ -1,5 +1,5 @@
 import { ShowFeatureFlag } from '@app/lib/analytics/posthog';
-import { ENABLE_NEW_CHANNELS } from '@core/constant/featureFlags';
+// import { ENABLE_NEW_CHANNELS } from '@core/constant/featureFlags';
 import type { BlockChannelProps } from './Block';
 import BlockChannel from './Block';
 import { NewChannelBlockAdapter } from './NewChannelBlockAdapter';
@@ -8,7 +8,7 @@ export function ChannelBlockSwitch(props: BlockChannelProps) {
   return (
     <ShowFeatureFlag
       key="enable-new-channels"
-      enabledOverride={ENABLE_NEW_CHANNELS()}
+      // enabledOverride={ENABLE_NEW_CHANNELS()}
       fallback={<BlockChannel {...props} />}
     >
       <NewChannelBlockAdapter />
