8f8c8fda0290a79082673e4cbb12cb53d5a2c3b0
Rahul <43100342+dev-rb@users.noreply.github.com>
2026-03-25 12:09:57 -0400

    Biome

--- js/app/packages/app/component/app-sidebar/sidebar.tsx
+++ js/app/packages/app/component/app-sidebar/sidebar.tsx
@@ -32,7 +32,7 @@ import { useSettingsState } from '@core/constant/SettingsState';
 import type { ValidHotkey } from '@core/hotkey/types';
 import { registerHotkey } from '@core/hotkey/hotkeys';
 import { GO_TO_COMMAND_SCOPE, GO_TO_LEADER_KEY } from '@app/constants/hotkeys';
-import { debounce, Scheduled } from '@solid-primitives/scheduled';
+import { debounce, type Scheduled } from '@solid-primitives/scheduled';
 import { Hotkey } from '@core/component/Hotkey';
 import { clearPressedKeys } from '@core/hotkey/state';
 import { activateClosestDOMScope } from '@core/hotkey/utils';
