d5a6abd60b86461070e938c7818ff679ade69ae9
Hutch <hutch@macro.com>
2026-03-26 13:20:23 -0400

    genapi

--- js/app/packages/service-clients/service-auth/generated/schemas/teamMember.ts
+++ js/app/packages/service-clients/service-auth/generated/schemas/teamMember.ts
@@ -14,6 +14,8 @@ export interface TeamMember {
   role: TeamRole;
   /** The id of the team */
   team_id: string;
+  /** The tier of the team member */
+  tier: string;
   /** The user id of the team member */
   user_id: string;
 }
