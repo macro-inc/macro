28c79d54e0978950b49f71d22a75fc9f15b3040e
Gabriel Birman <25272206+gbirman@users.noreply.github.com>
2026-03-26 14:51:51 -0400

    add static file service key to s3_key crate

--- rust/cloud-storage/s3_key/src/lib.rs
+++ rust/cloud-storage/s3_key/src/lib.rs
@@ -1,6 +1,9 @@
 #![deny(missing_docs)]
 
-//! S3 key building and parsing for the document storage bucket.
+//! S3 key building and parsing for cloud storage buckets.
 
 mod document_key;
 pub use document_key::*;
+
+mod static_file_key;
+pub use static_file_key::*;
