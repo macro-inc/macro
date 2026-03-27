135e077f14de377174a3bc9b1b966d46ff39d6c6
Hutch <hutch@macro.com>
2026-03-27 10:29:00 -0400

    fix tests

--- rust/cloud-storage/github/fixtures/github_installation_test_data.sql
+++ rust/cloud-storage/github/fixtures/github_installation_test_data.sql
@@ -1,12 +1,14 @@
 INSERT INTO
     public.macro_user (id, username, email, stripe_customer_id)
 VALUES
-    ('bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb'::uuid, 'testuser', 'testuser@test.com', 'cus_test');
+    ('bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb'::uuid, 'testuser', 'testuser@test.com', 'cus_test'),
+    ('cccccccc-cccc-cccc-cccc-cccccccccccc'::uuid, 'testuser2', 'testuser2@test.com', 'cus_test2');
 
 INSERT INTO
     public."User" (id, email, macro_user_id)
 VALUES
-    ('macro|user@user.com', 'testuser@test.com', 'bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb'::uuid);
+    ('macro|user@user.com', 'testuser@test.com', 'bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb'::uuid),
+    ('macro|user2@user.com', 'testuser2@test.com', 'cccccccc-cccc-cccc-cccc-cccccccccccc'::uuid);
 
 INSERT INTO
     public.github_links (id, macro_id, fusionauth_user_id, github_username, github_user_id)
@@ -17,10 +19,10 @@ INSERT INTO
     public.team (id, name, owner_id)
 VALUES
     ('dddddddd-dddd-dddd-dddd-dddddddddddd'::uuid, 'Team Alpha', 'macro|user@user.com'),
-    ('eeeeeeee-eeee-eeee-eeee-eeeeeeeeeeee'::uuid, 'Team Beta', 'macro|user@user.com');
+    ('eeeeeeee-eeee-eeee-eeee-eeeeeeeeeeee'::uuid, 'Team Beta', 'macro|user2@user.com');
 
 INSERT INTO
     public.team_user (user_id, team_id, team_role)
 VALUES
     ('macro|user@user.com', 'dddddddd-dddd-dddd-dddd-dddddddddddd'::uuid, 'owner'),
-    ('macro|user@user.com', 'eeeeeeee-eeee-eeee-eeee-eeeeeeeeeeee'::uuid, 'member');
+    ('macro|user2@user.com', 'eeeeeeee-eeee-eeee-eeee-eeeeeeeeeeee'::uuid, 'owner');
