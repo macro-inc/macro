a93d5adc4aa863f615a828d9c042f07d15ae2125
Gabriel Birman <25272206+gbirman@users.noreply.github.com>
2026-04-02 14:50:46 -0400

    add fixme

--- js/app/packages/app/component/next-soup/soup-view/soup-view.tsx
+++ js/app/packages/app/component/next-soup/soup-view/soup-view.tsx
@@ -533,6 +533,7 @@ export const SoupViewList = (props: SoupViewListProps) => {
       type === 'entity' ? args.entity : args.projectEntity
     ) as EntityData;
 
+    // FIXME: this never gets called because we have overrides
     if (event.metaKey || event.ctrlKey) {
       openEntityInNewTab({ entity, location });
       return;
