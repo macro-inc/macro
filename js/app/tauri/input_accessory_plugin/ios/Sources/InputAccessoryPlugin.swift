345386023f159fa5598e60245164a3f101260c55
Peter Chinman <peter.chinman@gmail.com>
2026-04-08 18:12:40 -0400

    fix: remove  ios vitual keyboard accessory bar

---
+++ js/app/tauri/input_accessory_plugin/ios/Sources/InputAccessoryPlugin.swift
@@ -0,0 +1,62 @@
+import UIKit
+import WebKit
+import Tauri
+
+class InputAccessoryPlugin: Plugin {
+    private static var isSwizzled = false
+
+    override public func load(webview: WKWebView) {
+        webview.scrollView.keyboardDismissMode = .interactive
+
+        guard !InputAccessoryPlugin.isSwizzled else { return }
+        InputAccessoryPlugin.isSwizzled = true
+        disableAccessoryBar(in: webview)
+    }
+}
+
+private func disableAccessoryBar(in rootView: UIView) {
+    var queue = rootView.subviews
+    while !queue.isEmpty {
+        let view = queue.removeFirst()
+        if NSStringFromClass(type(of: view)).hasPrefix("WKContent") {
+            if let cls = object_getClass(view) {
+                swizzleInputAccessoryView(on: cls)
+            }
+            return
+        }
+        queue.append(contentsOf: view.subviews)
+    }
+}
+
+private class NoInputAccessoryView: UIView {
+    override var inputAccessoryView: UIView? { return nil }
+}
+
+private func swizzleInputAccessoryView(on cls: AnyClass) {
+    let selector = #selector(getter: UIResponder.inputAccessoryView)
+    guard let noAccessoryMethod = class_getInstanceMethod(
+        NoInputAccessoryView.self,
+        #selector(getter: NoInputAccessoryView.inputAccessoryView)
+    ) else { return }
+
+    let added = class_addMethod(
+        cls,
+        selector,
+        method_getImplementation(noAccessoryMethod),
+        method_getTypeEncoding(noAccessoryMethod)
+    )
+
+    if !added {
+        class_replaceMethod(
+            cls,
+            selector,
+            method_getImplementation(noAccessoryMethod),
+            method_getTypeEncoding(noAccessoryMethod)
+        )
+    }
+}
+
+@_cdecl("init_plugin_input_accessory")
+func initPlugin() -> Plugin {
+    return InputAccessoryPlugin()
+}
