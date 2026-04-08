fd0ed883548f45eb80eb5d0f58445b9a8824da8a
Hutch <hutch@macro.com>
2026-04-08 13:31:53 -0400

    scaffolding for ports and service

--- rust/cloud-storage/entity_access_management/src/domain/models.rs
+++ rust/cloud-storage/entity_access_management/src/domain/models.rs
@@ -1,1 +1,14 @@
 //! Domain models for entity access management.
+
+use model_entity::EntityType;
+
+/// Errors that can occur in entity_access_management crate.
+#[derive(Debug, thiserror::Error)]
+pub enum EntityAccessManagementError {
+    /// Unsupported entity type provided
+    #[error("unsupported entity type provided {0}")]
+    UnsupportedEntityType(EntityType),
+    /// Datbase error occured
+    #[error(transparent)]
+    DatabaseError(#[from] anyhow::Error),
+}
--- rust/cloud-storage/entity_access_management/src/domain/ports.rs
+++ rust/cloud-storage/entity_access_management/src/domain/ports.rs
@@ -1,3 +1,48 @@
 //! Port definitions for entity access management.
 //!
 //! These traits define the contracts that adapters must implement.
+
+use model_entity::EntityType;
+
+use crate::domain::models::EntityAccessManagementError;
+
+/// Repository for persisting entity_access in the database.
+pub trait EntityAccessManagementRepository: Clone + Send + Sync + 'static {
+    /// The error type returned by repository operations
+    type Err: Into<anyhow::Error> + Send + std::fmt::Debug;
+
+    /// Updates entity access when an entity is **added** to a project
+    fn add_entity_to_project(
+        &self,
+        entity_id: &uuid::Uuid,
+        entity_type: EntityType,
+        project_id: &uuid::Uuid,
+    ) -> impl Future<Output = Result<(), Self::Err>> + Send;
+
+    /// Updates entity access when an entity is **removed** from a project
+    fn remove_entity_from_project(
+        &self,
+        entity_id: &uuid::Uuid,
+        entity_type: EntityType,
+        old_project_id: &uuid::Uuid,
+    ) -> impl Future<Output = Result<(), Self::Err>> + Send;
+}
+
+/// Service for managing entity access.
+pub trait EntityAccessManagementService: Clone + Send + Sync + 'static {
+    /// Updates entity access when an entity is **added** to a project
+    fn add_entity_to_project(
+        &self,
+        entity_id: &uuid::Uuid,
+        entity_type: EntityType,
+        project_id: &uuid::Uuid,
+    ) -> impl Future<Output = Result<(), EntityAccessManagementError>> + Send;
+
+    /// Updates entity access when an entity is **removed** from a project
+    fn remove_entity_from_project(
+        &self,
+        entity_id: &uuid::Uuid,
+        entity_type: EntityType,
+        old_project_id: &uuid::Uuid,
+    ) -> impl Future<Output = Result<(), EntityAccessManagementError>> + Send;
+}
