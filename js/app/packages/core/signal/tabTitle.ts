929abed8a415ed13f3040ef6abfae22afd669767
Peter Chinman <peter.chinman@gmail.com>
2026-04-14 15:31:53 -0400

    chg: show localhost port # in tab title

--- js/app/packages/core/signal/tabTitle.ts
+++ js/app/packages/core/signal/tabTitle.ts
@@ -3,7 +3,16 @@ import { DEV_MODE_ENV, LOCAL_ONLY } from '@core/constant/featureFlags';
 
 export const tabTitleSignal = createSignal<string | undefined>();
 
-const ENV_PREFIX = LOCAL_ONLY ? '[L] ' : DEV_MODE_ENV ? '[D] ' : '';
+function getEnvPrefix(): string {
+  if (LOCAL_ONLY) {
+    const port = window.location.port;
+    if (port && port !== '3000') return `[L:${port.slice(-1)}] `;
+    return '[L] ';
+  }
+  return DEV_MODE_ENV ? '[D] ' : '';
+}
+
+const ENV_PREFIX = getEnvPrefix();
 
 export function formatTabTitle(title: string | undefined) {
   if (title) return `${ENV_PREFIX}Macro - ${title}`;
