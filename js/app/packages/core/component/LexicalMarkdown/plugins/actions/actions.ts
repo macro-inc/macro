773f93b82baa50819d07b82622624968d47b1f23
seamus <seamus@macro.com>
2026-04-15 14:07:21 -0400

    style(md): restyle media resize handles

--- js/app/packages/core/component/LexicalMarkdown/plugins/actions/actions.ts
+++ js/app/packages/core/component/LexicalMarkdown/plugins/actions/actions.ts
@@ -150,7 +150,7 @@ export const ACTIONS: Action[] = [
     icon: ImageIcon,
     action: (editor: LexicalEditor) => {
       queueMicrotask(() => {
-        editor.dispatchCommand(TRY_INSERT_MEDIA_UPLOAD_COMMAND, 'image');
+        editor.dispatchCommand(TRY_INSERT_MEDIA_UPLOAD_COMMAND, 'all');
       });
     },
   },
