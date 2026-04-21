d367f697053ad07dfcb8c0a6edaae4819612a426
Rahul <43100342+dev-rb@users.noreply.github.com>
2026-04-21 17:58:07 -0400

    Bump persisted state version

--- js/app/packages/app/component/next-soup/soup-view/soup-view.tsx
+++ js/app/packages/app/component/next-soup/soup-view/soup-view.tsx
@@ -173,7 +173,7 @@ type PersistedSoupViewState = {
   assigneeFilter: string[];
 };
 
-const PERSISTED_STATE_VERSION = 3;
+const PERSISTED_STATE_VERSION = 4;
 
 const listStateCache = new Map<
   string,
