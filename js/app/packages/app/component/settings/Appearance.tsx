a8bb6667553180885b574f28404ceb8ec8940e11
Fake-User <self@destruct.dev>
2026-04-27 11:39:12 -0400

    close

--- js/app/packages/app/component/settings/Appearance.tsx
+++ js/app/packages/app/component/settings/Appearance.tsx
@@ -1,17 +1,41 @@
-import { ThemeEditorAdvanced } from '../../../theme/components/ThemeEditorAdvanced';
-import { ThemeEditorBasic } from '../../../theme/components/ThemeEditorBasic';
-import ThemeTools from '../../../theme/components/ThemeTools';
-import ThemeList from '../../../theme/components/ThemeList';
+import { ThemeEditorAdvanced } from '@theme/components/ThemeEditorAdvanced';
+import { ThemeEditorBasic } from '@theme/components/ThemeEditorBasic';
+import ThemeTools from '@theme/components/ThemeTools';
+import ThemeList from '@theme/components/ThemeList';
+import { isMobile } from '@core/mobile/isMobile';
+import { Panel } from '@ui';
 
 export function Appearance() {
   return (
-      <div
-        class="absolute inset-0 overflow-hidden bg-edge-muted @container gap-px grid grid-cols-1 grid-rows-[min-content_min-content_1fr_1fr] @[650px]:grid-cols-2 @[650px]:grid-rows-[min-content_min-content_1fr]"
-      >
-        <div class="@[650px]:col-span-2"><ThemeTools /></div>
-        <div class="@[650px]:col-span-2"><ThemeEditorBasic /></div>
-        <div class="overflow-hidden"><ThemeList/></div>
-        <div class="overflow-hidden"><ThemeEditorAdvanced /></div>
+    <div
+      style={{
+        'grid-template-rows': 'min-content min-content 1fr',
+        'grid-template-columns': '1fr',
+        'position': 'absolute',
+        'overflow': 'hidden',
+        'display': 'grid',
+        'padding': '20px',
+        'gap': '20px',
+      }}
+    >
+
+      <Panel>
+        <ThemeTools />
+        <ThemeEditorBasic />
+      </Panel>
+
+      <div style={{
+        'grid-template-columns': isMobile() ? '1fr' : '1fr 1fr',
+        'display': 'grid',
+        'gap': '20px',
+      }}>
+        <Panel>
+          <ThemeList />
+        </Panel>
+        <Panel>
+          <ThemeEditorAdvanced />
+        </Panel>
       </div>
+    </div>
   );
 }
