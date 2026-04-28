93ab5571fced255c85100f3a261c7a1e8538855d
Gabriel Birman <25272206+gbirman@users.noreply.github.com>
2026-04-28 17:52:21 -0400

    add comment

--- js/app/packages/app/component/command/recency.ts
+++ js/app/packages/app/component/command/recency.ts
@@ -3,6 +3,7 @@ import { createStore } from 'solid-js/store';
 
 const STORE_NAME = 'command-recency-v1';
 
+// stores command id -> last used timestamp in ms
 type RecencyStore = Record<string, number>;
 
 const [recencyStore, setRecencyStore] = makePersisted(
