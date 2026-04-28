2054f4d6c5f5c957cd7d168a6d4cd27b2a43ab05
Sean Aye <hello@seanaye.ca>
2026-04-28 11:49:33 -0400

    update graph impl

--- rust/cloud-storage/contacts/src/domain/models/graph/test.rs
+++ rust/cloud-storage/contacts/src/domain/models/graph/test.rs
@@ -2,81 +2,92 @@ use super::*;
 
 #[test]
 fn test_init() {
-    let a: Vertex<u32> = Vertex::new(1234);
-    let b: Vertex<u32> = Vertex::new(5678);
-
-    let edge: Edge<u32> = Edge { a: &a, b: &b };
-
-    assert_eq!(edge.a.data, a.data);
-    assert_eq!(edge.b.data, b.data);
-}
-
-#[test]
-fn test_append() {
-    let a: Vertex<u32> = Vertex::new(1234);
-    let b: Vertex<u32> = Vertex::new(5678);
-    let c: Vertex<u32> = Vertex::new(1111);
-    let c_id = c.data;
-
-    let mut vset: HashSet<Vertex<u32>> = HashSet::new();
-    let mut expected: HashSet<u32> = HashSet::new();
-    expected.insert(a.data);
-    expected.insert(b.data);
-
-    vset.insert(a);
-    vset.insert(b);
-
-    let edges = append(&vset, &c);
-
-    assert_eq!(edges.len(), 2);
-
-    for edge in edges {
-        assert_eq!(edge.a.data, c_id);
-        assert!(expected.contains(&edge.b.data));
-    }
+    let a_val: u32 = 1234;
+    let b_val: u32 = 5678;
+    let graph = UndirectedGraph::new([Vertex::new(&a_val), Vertex::new(&b_val)]).complete();
+    let edges: Vec<_> = graph.inner().edges().collect();
+    assert_eq!(edges.len(), 1);
+    let vals = [*edges[0].a().data(), *edges[0].b().data()];
+    assert!(vals.contains(&1234));
+    assert!(vals.contains(&5678));
 }
 
 #[test]
 fn test_generate() {
-    let a: Vertex<u32> = Vertex::new(1234);
-    let b: Vertex<u32> = Vertex::new(5678);
-    let c: Vertex<u32> = Vertex::new(6666);
-    let d: Vertex<u32> = Vertex::new(7777);
+    let vals: [u32; 4] = [1234, 5678, 6666, 7777];
+    let graph = UndirectedGraph::new(vals.iter().map(Vertex::new)).complete();
+    let edges: Vec<_> = graph.inner().edges().collect();
 
-    let expected_edges = [
-        [a.data, b.data],
-        [c.data, a.data],
-        [c.data, b.data],
-        [d.data, a.data],
-        [d.data, b.data],
-        [d.data, c.data],
-    ];
+    assert_eq!(edges.len(), 6);
 
-    let mut vset: HashSet<Vertex<u32>> = HashSet::new();
+    let expected: HashSet<(u32, u32)> = [
+        (1234, 5678),
+        (1234, 6666),
+        (1234, 7777),
+        (5678, 6666),
+        (5678, 7777),
+        (6666, 7777),
+    ]
+    .into_iter()
+    .flat_map(|(a, b)| [(a, b), (b, a)])
+    .collect();
 
-    let mut expected_edges_lookup: HashSet<(u32, u32)> = HashSet::new();
-
-    for edge in &expected_edges {
-        expected_edges_lookup.insert((edge[0], edge[1]));
-        // Insert phantom edge. Ordering of hashset is unknown
-        expected_edges_lookup.insert((edge[1], edge[0]));
+    for edge in &edges {
+        let a = *edge.a().data();
+        let b = *edge.b().data();
+        assert!(expected.contains(&(a, b)), "Unexpected edge ({a}, {b})",);
     }
+}
 
-    vset.insert(a);
-    vset.insert(b);
-    vset.insert(c);
-    vset.insert(d);
+#[test]
+fn test_vertex_same_ref_eq() {
+    let val: u32 = 42;
+    let v1 = Vertex::new(&val);
+    let v2 = Vertex::new(&val);
+    assert_eq!(v1, v2);
+}
 
-    let edges = generate(&vset);
+#[test]
+fn test_vertex_different_alloc_neq() {
+    let a: u32 = 42;
+    let b: u32 = 42;
+    let v1 = Vertex::new(&a);
+    let v2 = Vertex::new(&b);
+    assert_ne!(v1, v2);
+}
 
-    assert_eq!(edges.len(), expected_edges.len());
+#[test]
+fn test_edge_unordered_eq() {
+    let a_val: u32 = 1;
+    let b_val: u32 = 2;
+    let a = Vertex::new(&a_val);
+    let b = Vertex::new(&b_val);
+    let e1 = Edge { a, b };
+    let e2 = Edge { a: b, b: a };
+    assert_eq!(e1, e2);
+}
 
-    for edge in edges {
-        assert!(
-            expected_edges_lookup.contains(&(edge.a.data, edge.b.data)),
-            "Could not find edge ({}, {})",
-            edge.a.data,
-            edge.b.data
-        );
-    }
+#[test]
+fn test_edge_neq() {
+    let a_val: u32 = 1;
+    let b_val: u32 = 2;
+    let c_val: u32 = 3;
+    let a = Vertex::new(&a_val);
+    let b = Vertex::new(&b_val);
+    let c = Vertex::new(&c_val);
+    let e1 = Edge { a, b };
+    let e2 = Edge { a, b: c };
+    assert_ne!(e1, e2);
+}
+
+#[test]
+fn test_edge_dedup_in_hashset() {
+    let a_val: u32 = 1;
+    let b_val: u32 = 2;
+    let a = Vertex::new(&a_val);
+    let b = Vertex::new(&b_val);
+    let mut set: HashSet<Edge<u32>> = HashSet::new();
+    set.insert(Edge { a, b });
+    set.insert(Edge { a: b, b: a });
+    assert_eq!(set.len(), 1);
 }
