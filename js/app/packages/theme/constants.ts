1f2e20c2dee1558f9eaf5bb9405f374e9b59394b
Fake-User <self@destruct.dev>
2026-04-28 17:50:54 -0400

    light theme

--- js/app/packages/theme/constants.ts
+++ js/app/packages/theme/constants.ts
@@ -2,9 +2,6 @@ import type { ThemeV1 } from './types/themeTypes';
 
 export const DEFAULT_LIGHT_THEME: DefaultTheme = 'Macro Light';
 export const DEFAULT_DARK_THEME: DefaultTheme = 'Macro Dark';
-export const BEVELED_CORNERS: boolean = true;
-export const BLACK_BEZELS: boolean = false;
-
 
 export const DEFAULT_THEMES = [
   {
@@ -13,11 +10,11 @@ export const DEFAULT_THEMES = [
     version: 1,
     tokens: {
       a0: { l: 0.75, c: 0.20, h:  59 },
-      a1: { l: 0.88, c: 0.20, h:  99 },
-      a2: { l: 0.88, c: 0.20, h: 139 },
-      a3: { l: 0.88, c: 0.20, h: 179 },
-      a4: { l: 0.88, c: 0.20, h: 219 },
-      b0: { l: 0.14, c: 0.00, h: 158 },
+      a1: { l: 0.75, c: 0.20, h:  99 },
+      a2: { l: 0.75, c: 0.20, h: 139 },
+      a3: { l: 0.75, c: 0.20, h: 179 },
+      a4: { l: 0.75, c: 0.20, h: 219 },
+      b0: { l: 0.14, c: 0.00, h:  59 },
       b1: { l: 0.16, c: 0.00, h:  59 },
       b2: { l: 0.18, c: 0.00, h:  59 },
       b3: { l: 0.20, c: 0.00, h:  59 },
@@ -34,21 +31,23 @@ export const DEFAULT_THEMES = [
     name: 'Macro Light',
     version: 1,
     tokens: {
-      a0: { l: 0.67, c: 0.29, h:  82 },
-      a1: { l: 0.70, c: 0.30, h: 185 },
-      a2: { l: 0.70, c: 0.30, h: 225 },
-      a3: { l: 0.70, c: 0.30, h: 265 },
-      a4: { l: 0.70, c: 0.30, h: 305 },
-      b0: { l: 0.72, c: 0.00, h: 158 },
-      b1: { l: 0.87, c: 0.00, h: 158 },
-      b2: { l: 0.88, c: 0.00, h: 158 },
-      b3: { l: 0.89, c: 0.00, h: 158 },
-      b4: { l: 0.90, c: 0.00, h: 158 },
-      c0: { l: 0.14, c: 0.00, h: 153 },
-      c1: { l: 0.50, c: 0.00, h: 153 },
-      c2: { l: 0.60, c: 0.00, h: 153 },
-      c3: { l: 0.70, c: 0.00, h: 153 },
-      c4: { l: 0.70, c: 0.00, h: 153 },
+      a0: { l: 0.75, c: 0.20, h:  59 },
+      a1: { l: 0.75, c: 0.20, h:  99 },
+      a2: { l: 0.75, c: 0.20, h: 139 },
+      a3: { l: 0.75, c: 0.20, h: 179 },
+      a4: { l: 0.75, c: 0.20, h: 219 },
+
+      b0: { l: 0.84, c: 0.00, h:  59 },
+      b1: { l: 0.80, c: 0.00, h:  59 },
+      b2: { l: 0.78, c: 0.00, h:  59 },
+      b3: { l: 0.76, c: 0.00, h:  59 },
+      b4: { l: 0.74, c: 0.00, h:  59 },
+
+      c0: { l: 0.04, c: 0.00, h:  59 },
+      c1: { l: 0.07, c: 0.00, h:  59 },
+      c2: { l: 0.10, c: 0.00, h:  59 },
+      c3: { l: 0.13, c: 0.00, h:  59 },
+      c4: { l: 0.15, c: 0.00, h:  59 },
     },
   },
   {
