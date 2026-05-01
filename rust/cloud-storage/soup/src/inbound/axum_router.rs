af7a0c9f07d611b61f3a0ac5adcc53c3e625db53
synoet <teonys@nyu.edu>
2026-05-01 11:03:49 -0400

    cleanup

--- rust/cloud-storage/soup/src/inbound/axum_router.rs
+++ rust/cloud-storage/soup/src/inbound/axum_router.rs
@@ -134,7 +134,6 @@ where
         SoupRequest<R>: IntoSoupReqAst,
         R: Clone + Serialize + Send,
     {
-        println!("remove me please");
         let create_fallback = move || -> SoupQuery<R> {
             let params_sort = params
                 .sort_method
