afb136ddaf7c84655af2baccb1882598ce9329f2
Sean Aye <hello@seanaye.ca>
2026-05-05 11:23:46 -0400

    dev shell + mold + cachix

--- flake.nix
+++ flake.nix
@@ -180,35 +180,39 @@
           }
         );
 
-        shellTools = with pkgs; [
-          parallel
-          docker-compose
-          zip
-          cargo-info
-          cargo-udeps
-          cargo-lambda
-          cargo-deny
-          cargo-nextest
-          cargo-expand
-          wasm-pack
-          pkg-config
-          bacon
-          just
-          just-lsp
-          taplo
-          bun
-          pnpm
-          sqlx-cli
-          typescript-language-server
-          nodejs_24
-          pulumi
-          pulumiPackages.pulumi-nodejs
-          sops
-          biome
-          jq
-          stripe-cli
-          rustToolchain
-        ];
+        shellTools =
+          with pkgs;
+          [
+            parallel
+            docker-compose
+            zip
+            cargo-info
+            cargo-udeps
+            cargo-lambda
+            cargo-deny
+            cargo-nextest
+            cargo-expand
+            wasm-pack
+            pkg-config
+            bacon
+            just
+            just-lsp
+            taplo
+            bun
+            pnpm
+            sqlx-cli
+            typescript-language-server
+            nodejs_24
+            pulumi
+            pulumiPackages.pulumi-nodejs
+            sops
+            biome
+            jq
+            stripe-cli
+            sccache
+            rustToolchain
+          ]
+          ++ pkgs.lib.optionals isLinux [ mold ];
 
         # ── js-app (frontend Tauri app) ────────────────────────────────
 
@@ -376,6 +380,7 @@
               PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
               LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
               SOPS_KMS_ARN = "arn:aws:kms:us-east-1:569036502058:key/mrk-cab29bf948044eb79005a81f48d40e93,arn:aws:kms:us-west-1:569036502058:key/mrk-cab29bf948044eb79005a81f48d40e93";
+              RUSTC_WRAPPER = "${pkgs.sccache}/bin/sccache";
             }
             // pkgs.lib.optionalAttrs isLinux {
               LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath libraries}";
