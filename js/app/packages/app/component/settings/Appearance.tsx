70304003e252f786a49863140b1efbd00af569eb
Fake-User <self@destruct.dev>
2026-05-05 08:42:19 -0400

    theme

--- js/app/packages/app/component/settings/Appearance.tsx
+++ js/app/packages/app/component/settings/Appearance.tsx
@@ -1,37 +1,30 @@
-import { ThemeEditorAdvanced } from '@theme/components/ThemeEditorAdvanced';
 import { ThemeEditorBasic } from '@theme/components/ThemeEditorBasic';
 import ThemeTools from '@theme/components/ThemeTools';
 import ThemeList from '@theme/components/ThemeList';
 
-import { isMobile } from '@core/mobile/isMobile';
 import { Panel } from '@ui';
-import { cn } from '@ui/utils/classname';
 
 export function Appearance() {
   return (
-    <div
-      style={{
-        'grid-template-rows': 'min-content 1fr',
-        'grid-template-columns': '1fr',
-        'overflow': 'hidden',
-        'display': 'grid',
-        'height': '100%',
-        'padding': '8px',
-        'gap': '8px',
-      }}
-    >
-      <Panel depth={2}>
-        <ThemeTools />
-        <ThemeEditorBasic />
-      </Panel>
-
-      <div class="@container grid grid-cols-1 @[700px]:grid-cols-2 gap-2 overflow-hidden min-h-0">
+    <div class="h-full overflow-hidden flex justify-center p-2">
+      <div
+        class="max-w-200 w-full h-full"
+        style={{
+          'grid-template-rows': 'min-content 1fr',
+          'grid-template-columns': '1fr',
+          'overflow': 'hidden',
+          'display': 'grid',
+          'gap': '8px',
+        }}
+      >
         <Panel depth={2}>
-          <ThemeList />
+          <ThemeTools />
+          <ThemeEditorBasic />
         </Panel>
-        <div class={cn('contents', isMobile() && 'hidden')}>
+
+        <div class="grid grid-cols-1 gap-2 overflow-hidden min-h-0">
           <Panel depth={2}>
-            <ThemeEditorAdvanced />
+            <ThemeList />
           </Panel>
         </div>
       </div>
