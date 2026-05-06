007db8abb3e5039f013885ac29c53176225047d8
seamus <seamus@macro.com>
2026-05-06 17:24:47 -0400

    fix particip

--- js/app/packages/channel/Participants/ParticipantsListItem.tsx
+++ js/app/packages/channel/Participants/ParticipantsListItem.tsx
@@ -30,7 +30,7 @@ export function ParticipantsListItem(props: {
         type="button"
         class="flex min-w-0 flex-1 items-center gap-2 rounded-xs px-2 py-1 text-left focus:outline-none"
       >
-        <div class="shrink-0">
+        <div class="shrink-0 flex items-center">
           <UserIcon
             id={props.participant.user_id}
             size="sm"
