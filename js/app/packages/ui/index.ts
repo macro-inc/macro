b32ebb08ee6950bb1397c7fd78fd65989b1e2d38
Fake-User <self@destruct.dev>
2026-05-08 18:09:33 -0400

    order

--- js/app/packages/ui/index.ts
+++ js/app/packages/ui/index.ts
@@ -13,9 +13,8 @@ export { Layer } from './components/Layer';
 
 /* types */
 export type { AvatarSize, AvatarProps, AvatarGroupSize, AvatarGroupProps } from './components/Avatar';
-export type { DialogProps } from './components/Dialog';
-export type { DropdownTriggerProps } from './components/Dropdown';
 export type { ButtonProps } from './components/Button';
+export type { DialogProps } from './components/Dialog';
 
 /* utils */
 export { cn } from './utils/classname';
