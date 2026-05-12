e6a2515628b028ab494db104684241a3432d5b9b
synoet <teonys@nyu.edu>
2026-05-12 18:46:48 -0400

    fix

--- js/app/packages/block-call/component/CallRecording/CallRecordingTranscriptColumn.tsx
+++ js/app/packages/block-call/component/CallRecording/CallRecordingTranscriptColumn.tsx
@@ -78,7 +78,6 @@ export function CallRecordingTranscriptColumn(props: {
             >
               <CallRecordingParticipantsSection
                 record={props.record}
-                withShell={false}
               />
             </CallRecordingSectionShell>
           </Show>
