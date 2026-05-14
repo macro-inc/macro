000a1939ae6e739c825f7e17fca844558a4e8b64
Jacob Beckerman <jbecke@users.noreply.github.com>
2026-05-14 14:33:55 -0400

    Add symmetric list gutters and remove split toolbar divider

--- js/app/packages/app/component/next-soup/soup-view/views/tasks/TaskListEntity.tsx
+++ js/app/packages/app/component/next-soup/soup-view/views/tasks/TaskListEntity.tsx
@@ -125,7 +125,7 @@ export function TaskListEntity(props: TaskListEntityProps) {
       }}
       ref={mergeRefs(props.ref, draggable)}
       class={cn(
-        'soup-list-entity @container/entity w-full relative group/narrow flex flex-col mx-1 py-0.5',
+        'soup-list-entity @container/entity w-[calc(100%-0.5rem)] mx-1 relative group/narrow flex flex-col py-0.5',
         {
           'min-h-10': !isMobile(),
           'bg-ink/3 rounded-sm': props.checked,
