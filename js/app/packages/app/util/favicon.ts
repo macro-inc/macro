21dd391d533ee3185a77a93d1049d4807e06d461
Fake-User <self@destruct.dev>
2026-05-14 17:01:11 -0400

    kill

--- js/app/packages/app/util/favicon.ts
+++ js/app/packages/app/util/favicon.ts
@@ -1,5 +1,5 @@
+import FaviconBadgeSvg from '@macro-icons/macro-badge.svg?raw';
 import FaviconSvg from '@macro-icons/macro-macro.svg?raw';
-import FaviconBadgeSvg from '@macro-icons/macro-macro-badge.svg?raw';
 
 const FAVICON_SIZE = 48;
 
