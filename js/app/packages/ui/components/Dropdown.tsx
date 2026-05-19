e60942b3a7de2b22f3f47acb094a500789381a4f
Fake-User <self@destruct.dev>
2026-05-19 15:31:05 -0400

    drop

--- js/app/packages/ui/components/Dropdown.tsx
+++ js/app/packages/ui/components/Dropdown.tsx
@@ -1,6 +1,8 @@
 import { DropdownMenu as KobalteDropdownMenu } from '@kobalte/core/dropdown-menu';
 import { Button, type ButtonProps } from './Button';
-import type { ComponentProps } from 'solid-js';
+import { Surface, type SurfaceProps } from './Surface';
+import { cn } from '../utils/classname';
+import { splitProps, type ComponentProps } from 'solid-js';
 
 /*
 <Dropdown>
@@ -13,6 +15,9 @@ import type { ComponentProps } from 'solid-js';
 </Dropdown>
 */
 
+
+export type DropdownSubContentProps = ComponentProps<typeof KobalteDropdownMenu.SubContent> & { depth?: SurfaceProps['depth']; };
+export type DropdownContentProps = ComponentProps<typeof KobalteDropdownMenu.Content> & { depth?: SurfaceProps['depth']; };
 export type DropdownTriggerProps = ComponentProps<typeof KobalteDropdownMenu.Trigger> & ButtonProps;
 
 function DropdownTrigger(props: DropdownTriggerProps) {
@@ -26,21 +31,46 @@ function DropdownTrigger(props: DropdownTriggerProps) {
   );
 }
 
+function DropdownContent(props: DropdownContentProps) {
+  const [local, rest] = splitProps(props, ['depth', 'class']);
+  return (
+    <KobalteDropdownMenu.Content
+      class={cn('size-auto z-action-menu min-w-40 p-1', local.class)}
+      depth={local.depth ?? 2}
+      as={Surface}
+      {...rest}
+    />
+  );
+}
+
+function DropdownSubContent(props: DropdownSubContentProps) {
+  const [local, rest] = splitProps(props, ['depth', 'class']);
+  return (
+    <KobalteDropdownMenu.SubContent
+      class={cn('size-auto z-action-menu min-w-40 p-1', local.class)}
+      depth={local.depth ?? 2}
+      as={Surface}
+      {...rest}
+    />
+  );
+}
+
 export const Dropdown = Object.assign((props: ComponentProps<typeof KobalteDropdownMenu>) => (<KobalteDropdownMenu {...props} />), {
   ItemDescription: KobalteDropdownMenu.ItemDescription, /* todo */
   ItemIndicator: KobalteDropdownMenu.ItemIndicator,     /* todo */
   CheckboxItem: KobalteDropdownMenu.CheckboxItem,       /* todo */
   RadioGroup: KobalteDropdownMenu.RadioGroup,           /* todo */
   GroupLabel: KobalteDropdownMenu.GroupLabel,           /* todo */
-  SubContent: KobalteDropdownMenu.SubContent,           /* todo */
   SubTrigger: KobalteDropdownMenu.SubTrigger,           /* todo */
   ItemLabel: KobalteDropdownMenu.ItemLabel,             /* todo */
   RadioItem: KobalteDropdownMenu.RadioItem,             /* todo */
-  Content: KobalteDropdownMenu.Content,                 /* todo */
   Portal: KobalteDropdownMenu.Portal,                   /* todo */
   Group: KobalteDropdownMenu.Group,                     /* todo */
   Item: KobalteDropdownMenu.Item,                       /* todo */
   Icon: KobalteDropdownMenu.Icon,                       /* todo */
   Sub: KobalteDropdownMenu.Sub,                         /* todo */
+
+  SubContent: DropdownSubContent,
+  Content: DropdownContent,
   Trigger: DropdownTrigger,
 });
