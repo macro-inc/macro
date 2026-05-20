6363fde6e9c855680bae40d3e231ea9b09740706
Aidan Holloway-Bidwell <aidanhb17@gmail.com>
2026-05-20 15:04:40 -0400

    fix(downloads): fix image download typing and permissions

--- infra/stacks/static-file-service/distribution.ts
+++ infra/stacks/static-file-service/distribution.ts
@@ -86,6 +86,14 @@ export class StaticFileCloudFront extends pulumi.ComponentResource {
     const responseHeadersPolicy = new aws.cloudfront.ResponseHeadersPolicy(
       'corp-policy',
       {
+        corsConfig: {
+          accessControlAllowOrigins: { items: ['*'] },
+          accessControlAllowHeaders: { items: ['*'] },
+          accessControlAllowMethods: { items: ['GET', 'HEAD', 'OPTIONS'] },
+          accessControlAllowCredentials: false,
+          accessControlMaxAgeSec: 300,
+          originOverride: true,
+        },
         securityHeadersConfig: {
           contentSecurityPolicy: {
             contentSecurityPolicy:
