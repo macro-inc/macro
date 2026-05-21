420ebbba4cbeb5b71b584d148f4f3d3b9a62ed77
Hutch <hutch@macro.com>
2026-05-21 14:06:37 -0400

    fix(stripe): webhook for new price id

--- rust/cloud-storage/authentication_service/src/api/webhooks/user/stripe_webhook.rs
+++ rust/cloud-storage/authentication_service/src/api/webhooks/user/stripe_webhook.rs
@@ -363,19 +363,7 @@ async fn handle_customer_subscription_event(
         .map(|item| item.price.id.as_str().to_string())
         .context("no price id attached to subscription")?;
 
-    let product_tier = match price_id.as_str() {
-        id if id == ctx.legacy_stripe_price_ids.stripe_price_id_haiku.as_ref() => {
-            ProductTier::Haiku
-        }
-        id if id == ctx.legacy_stripe_price_ids.stripe_price_id_sonnet.as_ref() => {
-            ProductTier::Sonnet
-        }
-        id if id == ctx.legacy_stripe_price_ids.stripe_price_id_opus.as_ref() => ProductTier::Opus,
-        _ => {
-            tracing::warn!(price_id=?price_id.as_str(), "unsupported price id, defaulting to haiku tier");
-            ProductTier::Haiku
-        }
-    };
+    let product_tier = ProductTier::Opus;
 
     ctx.user_roles_and_permissions_service
         .update_user_roles_and_permissions_for_subscription(
