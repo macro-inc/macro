44d8ec5e8692ea1d8acc4bf27f2b111bdaba3e6f
Wolf Mermelstein <wolf@404wolf.com>
2026-06-04 17:07:04 +0000

    revert other pr

--- js/app/packages/app/vite.base.ts
+++ js/app/packages/app/vite.base.ts
@@ -186,10 +186,6 @@ export const createAppViteConfig = (): UserConfigFn => {
           host: process.env.TAURI_DEV_HOST || 'localhost',
         },
         cors: true,
-        proxy: {
-          // always redirect root to /app
-          '^/$': { target: 'http://localhost:3000', rewrite: () => '/app' },
-        },
         watch: {
           usePolling: true,
           interval: 100,
