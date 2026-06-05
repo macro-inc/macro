65a66a53d017e536c2a3b9593d32157cded9c994
Hutch <will@thehutchery.com>
2026-06-05 09:55:20 -0400

    feat(infra): update infra to use doppler token

--- infra/stacks/authentication-service/service.ts
+++ infra/stacks/authentication-service/service.ts
@@ -17,9 +17,8 @@ import {
 const BASE_NAME = 'authentication-service';
 const BASE_PATH = '../../../rust/cloud-storage';
 
-export const SERVICE_DOMAIN_NAME = `auth-service${
-  stack === 'prod' ? '' : `-${stack}`
-}.${BASE_DOMAIN}`;
+export const SERVICE_DOMAIN_NAME = `auth-service${stack === 'prod' ? '' : `-${stack}`
+  }.${BASE_DOMAIN}`;
 
 type Args = {
   secretKeyArns: (pulumi.Output<string> | string)[];
@@ -34,6 +33,7 @@ type Args = {
   serviceContainerPort: number;
   isPrivate?: boolean;
   containerEnvVars?: { name: string; value: pulumi.Output<string> | string }[];
+  containerSecrets: { name: string, valueFrom: pulumi.Output<string> | string }[];
   healthCheckPath: string;
   tags: { [key: string]: string };
   queueArns: pulumi.Output<string>[];
@@ -62,6 +62,7 @@ export class AuthenticationService extends pulumi.ComponentResource {
       healthCheckPath,
       isPrivate,
       containerEnvVars,
+      containerSecrets,
       clusterName,
       tags,
       secretKeyArns,
@@ -103,7 +104,7 @@ export class AuthenticationService extends pulumi.ComponentResource {
           Statement: [
             {
               Action: ['secretsmanager:GetSecretValue'],
-              Resource: [...secretKeyArns],
+              Resource: [...secretKeyArns, ...containerSecrets.map((s) => s.valueFrom)],
               Effect: 'Allow',
             },
           ],
@@ -232,10 +233,9 @@ export class AuthenticationService extends pulumi.ComponentResource {
               environment: [
                 { name: 'BASE_URL', value: this.domain },
                 ...(containerEnvVars ?? []),
-                {
-                  name: 'FUSIONAUTH_OAUTH_REDIRECT_URI',
-                  value: `https://${SERVICE_DOMAIN_NAME}/oauth/redirect`,
-                },
+              ],
+              secrets: [
+                ...containerSecrets,
               ],
               logConfiguration: {
                 logDriver: 'awsfirelens',
@@ -262,11 +262,10 @@ export class AuthenticationService extends pulumi.ComponentResource {
           },
           runtimePlatform: {
             operatingSystemFamily: `${platform.family.toUpperCase()}`,
-            cpuArchitecture: `${
-              platform.architecture === 'amd64'
-                ? 'X86_64'
-                : platform.architecture.toUpperCase()
-            }`,
+            cpuArchitecture: `${platform.architecture === 'amd64'
+              ? 'X86_64'
+              : platform.architecture.toUpperCase()
+              }`,
           },
         },
         desiredCount: 1,
