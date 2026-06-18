2bed90b552011f0267ff46eda891327f05f91f2e
Peter Chinman <peter.chinman@gmail.com>
2026-06-18 14:03:07 -0400

    add github query

--- js/app/packages/queries/auth/keys.ts
+++ js/app/packages/queries/auth/keys.ts
@@ -1,6 +1,7 @@
 import { createQueryKeys } from '@lukemorales/query-key-factory';
 
 export const authKeys = createQueryKeys('auth', {
+  githubLinkStatus: null,
   userInfo: null,
   userName: (userId: string) => ({
     queryKey: [userId],
