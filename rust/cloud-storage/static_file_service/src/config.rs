5efdcf3ed447995027dd3bce9da4cd87864f9a74
Hutch <will@thehutchery.com>
2026-06-22 10:27:08 -0400

    update sfs to use macro config

--- rust/cloud-storage/static_file_service/src/config.rs
+++ rust/cloud-storage/static_file_service/src/config.rs
@@ -1,70 +1,41 @@
 use anyhow::Context;
 pub use macro_env::Environment;
-use macro_env_var::{env_vars, maybe_env_vars};
+use macro_env_var::env_vars;
+use macro_middleware::auth::internal_access::InternalApiSecretKey;
 use macro_service_urls::StaticFileServiceUrl;
+use secretsmanager_client::LocalOrRemoteSecret;
 
 env_vars! {
-    struct StaticFileServiceDynamodbTableName;
-    struct StaticStorageBucket;
-    struct StaticFileServiceS3EventQueueUrl;
-    struct InternalApiSecretKey;
+    pub struct StaticFileServiceDynamodbTableName;
+    pub struct StaticStorageBucket;
+    pub struct StaticFileServiceS3EventQueueUrl;
 }
 
-maybe_env_vars! {
-    struct Port;
-}
-
-#[derive(Debug, Clone)]
+#[derive(macro_config::MacroConfig)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
 pub struct Config {
     /// self explanatory
+    #[macro_config_default(Environment::new_or_prod())]
     pub environment: Environment,
     /// port (8080)
+    #[macro_config_default(8080)]
     pub port: usize,
     /// the tablename of the metadata table
-    pub dynamodb_table: String,
+    pub static_file_service_dynamodb_table_name: StaticFileServiceDynamodbTableName,
     /// s3 storage bucket
-    pub storage_bucket_name: String,
+    pub static_storage_bucket: StaticStorageBucket,
     /// service url
-    pub service_url: String,
+    #[macro_config_default(StaticFileServiceUrl::unwrap_new().to_string())]
+    pub static_file_service_url: String,
     /// s3 upload notification queue
-    pub s3_event_queue_url: String,
+    pub static_file_service_s3_event_queue_url: StaticFileServiceS3EventQueueUrl,
     /// Internal API secret key
-    pub internal_api_secret_key: String,
+    pub internal_api_secret_key: LocalOrRemoteSecret<InternalApiSecretKey>,
 }
 
 impl Config {
     pub fn from_env() -> anyhow::Result<Self> {
-        let environment = Environment::new_or_prod();
-        let port = Port::new()
-            .map(|port| port.parse::<usize>().context("PORT must be a valid usize"))
-            .transpose()?
-            .unwrap_or(8080);
-        let dynamodb_table = StaticFileServiceDynamodbTableName::new()
-            .context("STATIC_FILE_SERVICE_DYNAMODB_TABLE_NAME must be provided")?
-            .to_string();
-
-        let storage_bucket_name = StaticStorageBucket::new()
-            .context("STATIC_STORAGE_BUCKET must be provided")?
-            .to_string();
-
-        let service_url = StaticFileServiceUrl::new()?.to_string();
-
-        let s3_event_queue_url = StaticFileServiceS3EventQueueUrl::new()
-            .context("S3_EVENT_QUEUE_URL must be provided")?
-            .to_string();
-
-        let internal_api_secret_key = InternalApiSecretKey::new()
-            .context("INTERNAL_API_SECRET_KEY must be provided")?
-            .to_string();
-
-        Ok(Config {
-            environment,
-            port,
-            dynamodb_table,
-            storage_bucket_name,
-            service_url,
-            s3_event_queue_url,
-            internal_api_secret_key,
-        })
+        macro_config::ConfigLoader::load::<Config>()
+            .context("failed to load static file service config")
     }
 }
