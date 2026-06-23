4fb0bba1603040c4c9be45c91e4828c9dc329d6f
Sean Aye <hello@seanaye.ca>
2026-06-23 11:35:29 -0400

    Use direct cloud-storage source in flake

--- flake.nix
+++ flake.nix
@@ -9,8 +9,6 @@
     fenix.inputs.nixpkgs.follows = "nixpkgs";
     crane.url = "github:ipetkov/crane";
     crane-tauri.url = "github:JPHutchins/crane-tauri";
-    rs-libreoffice-bindings.url = "github:macro-inc/rs-libreoffice-bindings/dev";
-    rs-libreoffice-bindings.flake = false;
   };
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./nix);
