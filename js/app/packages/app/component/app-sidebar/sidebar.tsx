fb4ba514ee97afc085bb5b58eaa2e2fb86eeeb07
Rahul <43100342+dev-rb@users.noreply.github.com>
2026-07-07 16:31:11 -0400

    Add suspense around favorites section

--- js/app/packages/app/component/app-sidebar/sidebar.tsx
+++ js/app/packages/app/component/app-sidebar/sidebar.tsx
@@ -90,6 +90,7 @@ import {
   type JSX,
   onCleanup,
   Show,
+  Suspense,
 } from 'solid-js';
 import { Dynamic } from 'solid-js/web';
 
@@ -859,7 +860,9 @@ export const AppSidebar = (props: AppSidebarProps) => {
         <hr class="border-transparent my-2" />
       </div>
 
-      <FavoritesSection sidebarState={props.sidebarState ?? 'expanded'} />
+      <Suspense>
+        <FavoritesSection sidebarState={props.sidebarState ?? 'expanded'} />
+      </Suspense>
 
       <div class="min-h-0 flex-1 overflow-hidden">
         <ChannelsUnreadWidget sidebarState={props.sidebarState ?? 'expanded'} />
