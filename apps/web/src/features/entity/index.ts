55dfcf897716ddc2b82e830c75d54706f66e0ca4
Peter Chinman <peter.chinman@gmail.com>
2026-07-17 17:54:53 -0400

    oops

--- apps/web/src/features/entity/index.ts
+++ apps/web/src/features/entity/index.ts
@@ -30,7 +30,7 @@ export {
 } from './queries/dss';
 export { createEmailsInfiniteQuery } from './queries/email';
 export { createUpdateFileTypeMutation } from './queries/file-type';
-export { queryKeys } from './queries/key';
+export { emailQueryKeyExcludesDone, queryKeys } from './queries/key';
 export {
   createBulkRenameDssEntityMutation,
   createRenameDssEntityMutation,
