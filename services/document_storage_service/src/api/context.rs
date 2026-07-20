8cb83af8d444eb6fcfedb892bf58333b58da5c9e
Hutch <will@thehutchery.com>
2026-07-20 15:11:45 -0400

    fix(search): use MacroAuthorizationExtractor

--- services/document_storage_service/src/api/context.rs
+++ services/document_storage_service/src/api/context.rs
@@ -475,6 +475,7 @@ impl From<&ApiContext> for SearchHandlerState {
             db: ctx.readonly_db.clone(),
             opensearch_client: ctx.opensearch_client.clone(),
             entity_access_service: ctx.entity_access_service.clone(),
+            authorization_state: ctx.authorization_state.clone(),
         }
     }
 }
