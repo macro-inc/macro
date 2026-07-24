fa41c479ea0ae3a7ec33ea67d6e72ce160e33d8d
Hutch <will@thehutchery.com>
2026-07-24 09:03:20 -0400

    T01: Declare the macro.chats Kafka topic

--- crates/macro_event_topics/src/test.rs
+++ crates/macro_event_topics/src/test.rs
@@ -26,4 +26,5 @@ fn all_topic_names_includes_declared_topics() {
     assert!(all_topic_names().contains(&MacroBotsTopic::TOPIC_STR));
     assert!(all_topic_names().contains(&MacroWebhooksTopic::TOPIC_STR));
     assert!(all_topic_names().contains(&MacroMentionsTopic::TOPIC_STR));
+    assert!(all_topic_names().contains(&MacroChatsTopic::TOPIC_STR));
 }
