969e16055f886a6665caa46593d2112e168683fa
seamus <seamus@macro.com>
2026-09-15 11:27:11 -0400

    chg to ask macro

--- apps/web/src/lib/core/component/DocumentPreview.tsx
+++ apps/web/src/lib/core/component/DocumentPreview.tsx
@@ -732,7 +732,7 @@ export function DocumentPreviewContent(props: DocumentPreviewContentProps) {
             <Show when={canOpenInChat()}>
               <Dropdown.Item onSelect={handleOpenInChat}>
                 <SparkleIcon class="size-4" />
-                Open Document in AI Chat
+                Ask Macro
               </Dropdown.Item>
             </Show>
             <Dropdown.Item onSelect={handleCopy}>
